40b3f7ddc597b5338e4962910b2b76b0e0c20686
Sergey Morozov <sergey.morozov@ins.world>
2018-07-19 18:29:06 +0300

    Some fixes

--- vm/wasm/modulereader/modulereader.go
+++ vm/wasm/modulereader/modulereader.go
@@ -1,18 +1,19 @@
 /*
-*    Copyright 2018 INS Ecosystem
-*
-*    Licensed under the Apache License, Version 2.0 (the "License");
-*    you may not use this file except in compliance with the License.
-*    You may obtain a copy of the License at
-*
-*        http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS,
-*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*    See the License for the specific language governing permissions and
-*    limitations under the License.
-*/
+ *    Copyright 2018 INS Ecosystem
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 // Package modulereader implements binary wasm reader statemachine
 // that simple reads binary code into internal representation
 package modulereader
@@ -21,9 +22,10 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
-	"gitlab.com/insecosystem/vm/iwasm/types"
 	"io"
 	"io/ioutil"
+
+	"github.com/insolar/insolar/vm/wasm/types"
 )
 
 // Reader is generalised module reader context and tokenizer
