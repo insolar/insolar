6ed042d1be899deb716067500fe24b4b0154acb5
Denis Linnik <denis.linnik@ins.world>
2018-07-19 20:37:43 +0300

    genesis: reference domain test

---
+++ genesis/public/core/reference_test.go
@@ -0,0 +1,84 @@
+/*
+ *    Copyright 2018 INS Ecosystem
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package core
+
+import (
+	"testing"
+
+	"github.com/platform/mock/storage"
+	"github.com/platform/model/domain"
+	"github.com/platform/model/object"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockParent struct {
+	Reference      *object.Reference
+	ContextStorage storage.Storage
+	parent         object.Parent
+}
+
+func (p *mockParent) GetParent() object.Parent {
+	return p.parent
+}
+
+func (p *mockParent) GetClassID() string {
+	return "mockParent"
+}
+
+func (p *mockParent) GetReference() *object.Reference {
+	return p.Reference
+}
+
+func (p *mockParent) GetChildStorage() storage.Storage {
+	return nil
+}
+
+func (p *mockParent) AddChild(child object.Child) (string, error) {
+	return "", nil
+}
+
+func (p *mockParent) GetChild(key string) (object.Child, error) {
+	return nil, nil
+}
+
+func (p *mockParent) GetContext() []string {
+	return []string{}
+}
+
+func (p *mockParent) GetContextStorage() storage.Storage {
+	return p.ContextStorage
+}
+
+func TestNewReferenceDomain(t *testing.T) {
+	parent := &mockParent{}
+	refDomain := newReferenceDomain(parent)
+
+	assert.Equal(t, &referenceDomain{
+		BaseDomain: *domain.NewBaseDomain(parent, ReferenceDomainName),
+	}, refDomain)
+}
+
+func TestNewReferenceDomain_WithNoParent(t *testing.T) {
+	refDomain := newReferenceDomain(nil)
+	expected := &referenceDomain{
+		BaseDomain: *domain.NewBaseDomain(nil, ReferenceDomainName),
+	}
+	expected.Parent = refDomain
+
+	assert.Equal(t, expected, refDomain)
+}
