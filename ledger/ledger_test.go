e15c3f56250cc31971121312185caa2a44db442a
Sergey Morozov <sergey.morozov@ins.world>
2018-07-24 18:18:58 +0300

    Comments added

--- ledger/ledger_test.go
+++ ledger/ledger_test.go
@@ -17,20 +17,8 @@
 package ledger
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/davecgh/go-spew/spew"
-	"github.com/syndtr/goleveldb/leveldb"
 )
 
-func TestNewLedger(t *testing.T) {
-	ledger, err := newLedger()
-	if err != nil {
-		fmt.Println("error:", err.Error())
-	}
-
-	s := &leveldb.DBStats{}
-	ledger.db.Stats(s)
-	spew.Dump(s)
+func TestStub(t *testing.T) {
 }
--- ledger/record/result_record.go
+++ ledger/record/result_record.go
@@ -16,14 +16,19 @@
 
 package record
 
+// ReasonCode is an error reason code.
 type ReasonCode uint
 
+// ResultRecord is a common type for all results.
 type ResultRecord struct {
 	AppDataRecord
 
 	RequestRecord Reference
 }
 
+// WipeOutRecord is a special record that takes place of another record
+// when we need to completely wipe out some information from storage
+// (think GDPR).
 type WipeOutRecord struct {
 	ResultRecord
 
@@ -31,28 +36,44 @@ type WipeOutRecord struct {
 	WipedHash   Hash
 }
 
+// StatelessResult is a result type that does not need to be stored.
 type StatelessResult struct {
 	ResultRecord
 }
 
+// ReadRecordResult just contains necessary record from storage.
 type ReadRecordResult struct {
 	StatelessResult
 
 	RecordBody []byte
 }
 
+// StatelessCallResult is a contract call result that didn't produce new state.
 type StatelessCallResult struct {
 	StatelessResult
 
 	resultMemory Memory
 }
 
+// Write allows to write to Request's paramMemory.
+func (r *StatelessCallResult) Write(p []byte) (n int, err error) {
+	r.resultMemory = make([]byte, len(p))
+	return copy(r.resultMemory, p), nil
+}
+
+// Read allows to read Result's resultMemory.
+func (r *StatelessCallResult) Read(p []byte) (n int, err error) {
+	return copy(p, r.resultMemory), nil
+}
+
+// StatelessExceptionResult is an exception result that does not need to be stored.
 type StatelessExceptionResult struct {
 	StatelessCallResult
 
 	ExceptionType Reference
 }
 
+// ReadObjectResult contains necessary object's memory.
 type ReadObjectResult struct {
 	StatelessResult
 
@@ -60,30 +81,36 @@ type ReadObjectResult struct {
 	MomoryProjection Memory
 }
 
+// SpecialResult is a result type for special situations.
 type SpecialResult struct {
 	ResultRecord
 
 	ReasonCode ReasonCode
 }
 
+// LockUnlockResult is a result of lock/unlock attempts.
 type LockUnlockResult struct {
 	SpecialResult
 }
 
+// RejectionResult is a result type for failed attempts.
 type RejectionResult struct {
 	SpecialResult
 }
 
+// StatefulResult is a result type which contents need to be persistently stored.
 type StatefulResult struct {
 	ResultRecord
 }
 
+// ActivationRecord is an activation record.
 type ActivationRecord struct {
 	StatefulResult
 
 	GoverningDomain Reference
 }
 
+// ClassActivateRecord is produced when we "activate" new contract class.
 type ClassActivateRecord struct {
 	ActivationRecord
 
@@ -91,6 +118,7 @@ type ClassActivateRecord struct {
 	DefaultMemory Memory
 }
 
+// ObjectActivateRecord is produced when we instantiate new object from an available class.
 type ObjectActivateRecord struct {
 	ActivationRecord
 
@@ -98,10 +126,12 @@ type ObjectActivateRecord struct {
 	Memory              Memory
 }
 
+// StorageRecord is produced when we store something in ledger. Code, data etc.
 type StorageRecord struct {
 	StatefulResult
 }
 
+// CodeRecord is a code storage record.
 type CodeRecord struct {
 	StorageRecord
 
@@ -110,6 +140,7 @@ type CodeRecord struct {
 	SourceCode   string   // ObjectSourceCode
 }
 
+// AmendRecord is produced when we modify another record in ledger.
 type AmendRecord struct {
 	StatefulResult
 
@@ -117,16 +148,19 @@ type AmendRecord struct {
 	AmendedRecord Reference
 }
 
+// ClassAmendRecord is an amendment record for classes.
 type ClassAmendRecord struct {
 	AmendRecord
 
 	NewCode []byte // ObjectBinaryCode
 }
 
+// MigrationCodes returns a list of data migration procedures for a given code change.
 func (r *ClassAmendRecord) MigrationCodes() []*MemoryMigrationCode {
 	panic("not implemented")
 }
 
+// MemoryMigrationCode is a data migration procedure.
 type MemoryMigrationCode struct {
 	ClassAmendRecord
 
@@ -134,32 +168,38 @@ type MemoryMigrationCode struct {
 	MigrationCodeRecord    Reference
 }
 
+// DeactivationRecord marks targeted object as disabled.
 type DeactivationRecord struct {
 	AmendRecord
 }
 
+// ObjectAmendRecord is an amendment record for objects.
 type ObjectAmendRecord struct {
 	AmendRecord
 
 	NewMemory Memory
 }
 
+// StatefulCallResult is a contract call result that produces new state.
 type StatefulCallResult struct {
 	ObjectAmendRecord
 
 	ResultMemory Memory
 }
 
+// StatefulExceptionResult is an exception result that needs to be stored.
 type StatefulExceptionResult struct {
 	StatefulCallResult
 
 	ExceptionType Reference
 }
 
+// EnforcedObjectAmendRecord is an enforced amendment record for objects.
 type EnforcedObjectAmendRecord struct {
 	ObjectAmendRecord
 }
 
+// ObjectAppendRecord is an "append state" record for objects. It does not contain full actual state.
 type ObjectAppendRecord struct {
 	AmendRecord
 
