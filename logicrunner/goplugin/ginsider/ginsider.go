69d68e8b70bb8a8b5817b8cc9d8805ff99a9b8ed
vany <ivan.serezhkin@ins.world>
2018-07-26 17:18:51 +0300

    INS-27: quick and dirty start of development of golang contract runner

--- logicrunner/goplugin/ginsider/ginsider.go
+++ logicrunner/goplugin/ginsider/ginsider.go
@@ -6,16 +6,13 @@ import (
 	"net/http"
 	"net/rpc"
 
-	"github.com/insolar/insolar/logicrunner"
 	"github.com/insolar/insolar/logicrunner/goplugin"
 )
 
-type GoInsider int
-
-func (t *GoInsider) Exec(object logicrunner.Object, method string, args logicrunner.Arguments) (ret logicrunner.Arguments, err error) {
+type GoInsider struct {
 }
 
-func (t *GoInsider) Call(args *goplugin.CallReq, reply *goplugin.CallResp) error {
+func (t *GoInsider) Call(args goplugin.CallReq, reply *goplugin.CallResp) error {
 	*reply = goplugin.CallResp{
 		Ret: []byte{1, 2, 3, 4, 5},
 		Err: nil,
@@ -24,12 +21,14 @@ func (t *GoInsider) Call(args *goplugin.CallReq, reply *goplugin.CallResp) error
 }
 
 func main() {
+	log.Print("ginsider launched")
 	insider := new(GoInsider)
 	rpc.Register(insider)
 	rpc.HandleHTTP()
-	l, e := net.Listen("tcp", ":1234")
+	l, e := net.Listen("tcp", ":7777")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
 	go http.Serve(l, nil)
+	<-make(chan byte)
 }
