85ff6caa58b7dbe090dd938be89828e54edd1be4
Ruslan Zakirov <ruslan.zakirov@gmail.com>
2018-08-02 11:21:17 +0300

    INS-22: we have only one test plugin now, make test pass

--- logicrunner/goplugin/goplugin_test.go
+++ logicrunner/goplugin/goplugin_test.go
@@ -11,8 +11,6 @@ import (
 
 	"bytes"
 
-	"os"
-
 	"github.com/2tvenom/cbor"
 )
 
@@ -47,30 +45,17 @@ func TestHelloWorld(t *testing.T) {
 	t.Log(ret)
 }
 
-var PATH = "/Users/vany/go/src/github.com/insolar/insolar/logicrunner/goplugin/ginsider/plugins"
+var PATH = "./testplugins"
 
 func TestConfigLoad(t *testing.T) {
-	t.Fatal(os.Getwd())
-	pl, err := plugin.Open(PATH + "/main.so")
+	plugin, err := plugin.Open(PATH + "/secondary.so")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	pl2, err := plugin.Open(PATH + "/secondary/main.so")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	hw, err := pl.Lookup("EXP")
+	hw, err := plugin.Lookup("EXP")
 	r := reflect.ValueOf(hw)
 	m := r.MethodByName("Hello")
 	ret := m.Call([]reflect.Value{})
 	t.Logf("%+v", ret)
-
-	hw2, err := pl2.Lookup("EXP")
-	r2 := reflect.ValueOf(hw2)
-	m2 := r2.MethodByName("Hello")
-	ret2 := m2.Call([]reflect.Value{})
-	t.Logf("%+v", ret2)
-
 }
