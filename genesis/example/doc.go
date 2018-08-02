743f896749ad498cf6976d9518f5dd8da4bc19c2
Maria Zubareva <maria.zubareva@insolar.io>
2018-08-02 11:53:07 +0300

    INS-59: add docs

--- genesis/example/doc.go
+++ genesis/example/doc.go
@@ -1,1 +1,33 @@
+/*
+ *    Copyright 2018 INS Ecosystem
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
+Package example provides smart contracts for building example of system work.
+
+Entities:
+
+	MemberDomain - domain that allows to add new members to system.
+
+	Usage:
+
+		factory := NewMemberDomainFactory()
+		mDomain, error := factory.Create(parent)
+
+		record, err := mDomain.CreateMember(someMemberFactory)
+		memberProxy, err := mDomain.GetMember(record)
+
+*/
 package example
