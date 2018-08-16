97004049e8af636200215cb20ed6a36f822af6f3
yakov.zverev <yakov.zverev@ins.world>
2018-08-16 19:18:13 +0300

    added network analyze tests

--- network/host/dht_test.go
+++ network/host/dht_test.go
@@ -1292,3 +1292,55 @@ func TestDHT_ObtainIP(t *testing.T) {
 	}
 	<-done
 }
+
+func TestDHT_AnalyzeNetwork(t *testing.T) {
+	count := 2
+	port := 8000
+	var dhts []*DHT
+	done := make(chan bool)
+	var ids []string
+
+	for i := 0; i < count; i++ {
+		id, _ := node.NewIDs(1)
+		ids = append(ids, id[0].String())
+		st, s, tp, r, _ := realDhtParams(id, "127.0.0.1:"+strconv.Itoa(port))
+		address, _ := node.NewAddress("127.0.0.1:" + strconv.Itoa(port-1))
+		bootstrapNode := node.NewNode(address)
+		dht, err := NewDHT(st, s, tp, r, &Options{
+			BootstrapNodes: []*node.Node{
+				bootstrapNode,
+			},
+		},
+			relay.NewProxy())
+		port++
+		dhts = append(dhts, dht)
+		assert.NoError(t, err)
+	}
+
+	for _, dht := range dhts {
+		ctx, _ := NewContextBuilder(dht).SetDefaultNode().Build()
+		assert.Equal(t, 0, dht.NumNodes(ctx))
+		go func(dht *DHT) {
+			err := dht.Listen()
+			assert.Equal(t, "closed", err.Error())
+			done <- true
+		}(dht)
+	}
+
+	for _, dht := range dhts {
+		dht.Bootstrap()
+	}
+
+	ctx, _ := NewContextBuilder(dhts[0]).SetDefaultNode().Build()
+
+	err := dhts[0].ObtainIP(ctx)
+	assert.NoError(t, err)
+
+	err = dhts[0].AnalyzeNetwork(ctx)
+	assert.NoError(t, err)
+
+	for _, dht := range dhts {
+		dht.Disconnect()
+	}
+	<-done
+}
