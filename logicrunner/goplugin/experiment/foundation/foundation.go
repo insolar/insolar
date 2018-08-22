d3c04357df7f5ad9002fabe609f8a040dfbb75bd
vany <ivan.serezhkin@ins.world>
2018-08-22 13:36:40 +0300

    INS-195: delete contract in foundation

--- logicrunner/goplugin/experiment/foundation/foundation.go
+++ logicrunner/goplugin/experiment/foundation/foundation.go
@@ -10,12 +10,17 @@ type CallContext struct {
 	Me     *Reference
 	Caller *Reference
 	Parent *Reference
+	Type   *Reference
 	Time   time.Time
 	Pulse  uint64
 }
 
 type BaseContract struct {
 	context *CallContext
+}
+
+type BaseContractInterface interface {
+	GetContext() *CallContext
 }
 
 func (bc BaseContract) GetContext() *CallContext {
@@ -33,12 +38,27 @@ var FakeLedger = make(map[*Reference]interface{})
 var FakeDelegates = make(map[*Reference]map[*Reference]interface{})
 var FakeChildren = make(map[*Reference]map[*Reference][]interface{})
 
-func (bc BaseContract) GetImplementationFor(r *Reference) interface{} {
+func (bc *BaseContract) GetImplementationFor(r *Reference) interface{} {
 	return FakeDelegates[bc.context.Me][r]
 }
 
-func (bc BaseContract) GetChildrenTyped(r *Reference) []interface{} {
+func (bc *BaseContract) GetChildrenTyped(r *Reference) []interface{} {
 	return FakeChildren[bc.context.Me][r]
 }
 
-func (bc BaseContract) SelfDestructRequest() {}
+func (bc *BaseContract) SelfDestructRequest() {
+	me := bc.context.Me
+	delete(FakeLedger, me)
+	for _, v := range FakeDelegates {
+		delete(v, me)
+	}
+	for _, c := range FakeChildren {
+		arr := []interface{}{}
+		for _, v := range c[bc.context.Type] {
+			if v.(BaseContractInterface).GetContext().Me != me {
+				arr = append(arr, v)
+			}
+		}
+		c[bc.context.Type] = arr
+	}
+}
