d632219d7a2adffdb2ebd94428237665b5b3b31f
Andrey Bronin <andrey.bronin@insolar.io>
2018-08-22 18:32:27 +0300

    fix golint in dht.go

--- network/host/dht.go
+++ network/host/dht.go
@@ -40,7 +40,10 @@ import (
 	"github.com/jbenet/go-base58"
 )
 
+// RemoteProcedure is remote procedure call function.
 type RemoteProcedure func(args [][]byte) ([]byte, error)
+
+// RPC is remote procedure call interface
 type RPC interface {
 	RemoteProcedureCall(ctx Context, target string, method string, args [][]byte) (result []byte, err error)
 	RemoteProcedureRegister(name string, method RemoteProcedure)
@@ -1423,7 +1426,7 @@ func (dht *DHT) RemoteProcedureCall(ctx Context, target string, method string, a
 	}
 }
 
-// Register procedure for remote call on this node
+// RemoteProcedureRegister registers procedure for remote call on this node
 func (dht *DHT) RemoteProcedureRegister(name string, method RemoteProcedure) {
 	rp := func(sender *node.Node, args [][]byte) ([]byte, error) {
 		return method(args)
