bc368b3820a95957b546f3212a4407892dabf79b
Maria Zubareva <maria.zubareva@insolar.io>
2018-08-23 14:13:17 +0300

    INS-192: fixes in test

--- genesis/experiment/first_test.go
+++ genesis/experiment/first_test.go
@@ -26,35 +26,52 @@ import (
 )
 
 func TestFirst(t *testing.T) {
+	// Create member which balance will increase
 	toMember, toMemberRef := member.NewMember("Vasya")
 	toMember.SetContext(&foundation.CallContext{
 		Me: toMemberRef,
 	})
 
+	// Create member which balance will decrease
 	fromMember, fromMemberRef := member.NewMember("Petya")
 	fromMember.SetContext(&foundation.CallContext{
 		Me: fromMemberRef,
 	})
 
+	// Create wallet for toMember
 	toWallet, toWalletRef := wallet.NewWallet(1000)
 	toWallet.SetContext(&foundation.CallContext{
 		Me: toWalletRef,
 	})
 
+	// Create wallet for fromMember
 	fromWallet, fromWalletRef := wallet.NewWallet(2000)
 	fromWallet.SetContext(&foundation.CallContext{
 		Me: fromWalletRef,
 	})
 
+	// Make fromWallet delegate of fromMember
 	fromMember.TakeDelegate(fromWallet, &wallet.TypeReference)
+	// Make toWallet delegate of toMember
+	toMember.TakeDelegate(toWallet, &wallet.TypeReference)
 
-	_, ok := fromMember.GetImplementationFor(&wallet.TypeReference).(*wallet.Wallet)
+	// Get fromMember as wallet instance
+	fromMemberAsWallet, ok := fromMember.GetImplementationFor(&wallet.TypeReference).(*wallet.Wallet)
 	assert.True(t, ok)
+	assert.NotNil(t, fromMemberAsWallet)
 
+	// Get toMember as wallet instance
+	toMemberAsWallet, ok := toMember.GetImplementationFor(&wallet.TypeReference).(*wallet.Wallet)
+	assert.True(t, ok)
+	assert.NotNil(t, toMemberAsWallet)
+
+	// Inject fake context of Caller for test
 	foundation.InjectFakeContext(3, &foundation.CallContext{Caller: toWalletRef})
 
-	toWallet.Receive(500, fromMemberRef)
+	// Call to get money from one member to another
+	toMemberAsWallet.Receive(500, fromMemberRef)
 
+	// Check balance
 	assert.Equal(t, 1500, int(fromWallet.GetTotalBalance()))
 	assert.Equal(t, 1500, int(toWallet.GetTotalBalance()))
 }
