4fc212757c04240a94fe92114e07a8c61514b2d3
Ruslan Zakirov <ruslan.zakirov@gmail.com>
2018-09-04 13:18:38 +0300

    INS-252: remove testplugins
    tests we wrote since then cover this functionality

--- logicrunner/goplugin/goplugin_test.go
+++ logicrunner/goplugin/goplugin_test.go
@@ -40,46 +40,6 @@ func init() {
 	log.SetLevel(log.DebugLevel)
 }
 
-type HelloWorlder struct {
-	Greeted int
-}
-
-func (r *HelloWorlder) ProxyEcho(gp *GoPlugin, s string) string {
-	ch := new(codec.CborHandle)
-	var data []byte
-	err := codec.NewEncoderBytes(&data, ch).Encode(*r)
-	if err != nil {
-		panic(err)
-	}
-
-	var args [1]interface{}
-	args[0] = s
-
-	var argsSerialized []byte
-	err = codec.NewEncoderBytes(&argsSerialized, ch).Encode(args)
-	if err != nil {
-		panic(err)
-	}
-
-	data, res, err := gp.CallMethod(core.String2Ref("secondary"), data, "Echo", argsSerialized)
-	if err != nil {
-		panic(err)
-	}
-
-	err = codec.NewDecoderBytes(data, ch).Decode(r)
-	if err != nil {
-		panic(err)
-	}
-
-	var resParsed []interface{}
-	err = codec.NewDecoderBytes(res, ch).Decode(&resParsed)
-	if err != nil {
-		panic(err)
-	}
-
-	return resParsed[0].(string)
-}
-
 func buildCLI(name string) error {
 	out, err := exec.Command("go", "build", "-o", "./"+name+"/"+name, "./"+name+"/").CombinedOutput()
 	if err != nil {
@@ -94,65 +54,6 @@ func buildInciderCLI() error {
 
 func buildPreprocessor() error {
 	return buildCLI("preprocessor")
-}
-
-func compileBinaries() error {
-	err := buildInciderCLI()
-	if err != nil {
-		return errors.Wrap(err, "can't build ginsider")
-	}
-
-	d, _ := os.Getwd()
-
-	err = os.Chdir(d + "/testplugins")
-	if err != nil {
-		return errors.Wrap(err, "couldn't chdir")
-	}
-
-	defer os.Chdir(d) // nolint: errcheck
-
-	out, err := exec.Command("make", "secondary.so").CombinedOutput()
-	if err != nil {
-		return errors.Wrap(err, "can't build pluigins: "+string(out))
-	}
-	return nil
-}
-
-func TestHelloWorld(t *testing.T) {
-	if err := compileBinaries(); err != nil {
-		t.Fatal("Can't compile binaries", err)
-	}
-	dir, err := ioutil.TempDir("", "test-")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir) // nolint: errcheck
-
-	am := &testArtifactManager{codePath: "./testplugins/"}
-
-	gp, err := NewGoPlugin(
-		configuration.Goplugin{
-			MainListen:     "127.0.0.1:7778",
-			RunnerListen:   "127.0.0.1:7777",
-			RunnerCodePath: dir,
-		},
-		nil,
-		am,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer gp.Stop()
-
-	hw := &HelloWorlder{77}
-	res := hw.ProxyEcho(gp, "hi there here we are")
-	if hw.Greeted != 78 {
-		t.Fatalf("Got unexpected value: %d, 78 is expected", hw.Greeted)
-	}
-
-	if res != "hi there here we are" {
-		t.Fatalf("Got unexpected value: %s, 'hi there here we are' is expected", res)
-	}
 }
 
 const contractOneCode = `
