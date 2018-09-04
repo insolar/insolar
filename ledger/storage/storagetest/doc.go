deb47c16f552cfaabf18d16ff614d7cdfaf2df29
Andrey Romancev <andrey@romancev.com>
2018-09-04 18:06:37 +0300

    INS-126: Update godoc and license.

--- ledger/storage/storagetest/doc.go
+++ ledger/storage/storagetest/doc.go
@@ -14,25 +14,5 @@
  *    limitations under the License.
  */
 
-package helloworld
-
-import "github.com/insolar/insolar/core"
-
-type HelloWorld struct {
-	Greeted int
-}
-
-func CodeRef() core.RecordRef {
-	var ref core.RecordRef
-	ref[core.RecordRefSize-1] = 1
-	return ref
-}
-
-func NewHelloWorld() *HelloWorld {
-	return &HelloWorld{}
-}
-
-func (hw *HelloWorld) Greet(name string) string {
-	hw.Greeted++
-	return "Hello " + name + "'s world"
-}
+// Package storagetest contains high level API tests and test utils for other modules.
+package storagetest
