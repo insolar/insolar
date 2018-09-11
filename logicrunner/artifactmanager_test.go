2a8972f03f2381cbcefed7850878a7bbf3b999ca
Alexander Orlovsky <alexander.orlovsky@ins.world>
2018-09-11 18:50:03 +0300

    NOISSUE: reorder imports and fix copyright text

--- logicrunner/artifactmanager_test.go
+++ logicrunner/artifactmanager_test.go
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2018 INS Ecosystem
+ *    Copyright 2018 Insolar
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -25,9 +25,9 @@ import (
 
 	"github.com/insolar/insolar/configuration"
 	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/logicrunner/goplugin"
 
 	"github.com/insolar/insolar/ledger/ledgertestutil"
-	"github.com/insolar/insolar/logicrunner/goplugin"
 	"github.com/insolar/insolar/logicrunner/goplugin/testutil"
 )
 
