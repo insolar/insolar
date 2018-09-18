0fe9f92c2d76e21668a7b3aff495f684dc044e81
Ilya Petrusenko <ilyapt4@gmail.com>
2018-09-18 16:19:47 +0300

    INS-301: spf23/cobra arrived

--- cmd/insgocc/insgocc.go
+++ cmd/insgocc/insgocc.go
@@ -17,7 +17,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -26,17 +25,8 @@ import (
 
 	"github.com/insolar/insolar/logicrunner/goplugin/preprocessor"
 	"github.com/pkg/errors"
-	flag "github.com/spf13/pflag"
+	"github.com/spf13/cobra"
 )
-
-func printUsage() {
-	fmt.Println("usage: insgocc <command> [<args>]")
-	fmt.Println("Commands: ")
-	fmt.Println(" wrapper   generate contract's wrapper")
-	fmt.Println(" proxy     generate contract's proxy")
-	fmt.Println(" compile   compile contract")
-	fmt.Println(" imports   rewrite imports")
-}
 
 type outputFlag struct {
 	path   string
@@ -50,6 +40,7 @@ func newOutputFlag() *outputFlag {
 func (r *outputFlag) String() string {
 	return r.path
 }
+
 func (r *outputFlag) Set(arg string) error {
 	var res io.Writer
 	if arg == "-" {
@@ -65,29 +56,45 @@ func (r *outputFlag) Set(arg string) error {
 	r.writer = res
 	return nil
 }
+
 func (r *outputFlag) Type() string {
 	return "file"
 }
 
 func main() {
 
-	if len(os.Args) == 1 {
-		printUsage()
-		return
+	var reference, outdir string
+	output := newOutputFlag()
+
+	var cmdProxy = &cobra.Command{
+		Use:   "proxy [flags] <file name to process>",
+		Short: "Generate contract's proxy",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				panic(errors.New("proxy command should be followed by exactly one file name to process"))
+			}
+			parsed, err := preprocessor.ParseFile(args[0])
+			if err != nil {
+				panic(errors.Wrap(err, "couldn't parse"))
+			}
+
+			err = preprocessor.GenerateContractProxy(parsed, reference, output.writer)
+			if err != nil {
+				panic(err)
+			}
+		},
 	}
+	cmdProxy.Flags().StringVarP(&reference, "code-reference", "r", "", "reference to code of")
+	cmdProxy.Flags().VarP(output, "output", "o", "output file (use - for STDOUT)")
 
-	switch os.Args[1] {
-	case "wrapper":
-		fs := flag.NewFlagSet("wrapper", flag.ExitOnError)
-		output := newOutputFlag()
-		fs.VarP(output, "output", "o", "output file (use - for STDOUT)")
-		err := fs.Parse(os.Args[2:])
-		if err != nil {
-			panic(err)
-		}
-
-		for _, fn := range fs.Args() {
-			parsed, err := preprocessor.ParseFile(fn)
+	var cmdWrapper = &cobra.Command{
+		Use:   "wrapper [flags] <file name to process>",
+		Short: "Generate contract's wrapper",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				panic(errors.New("wrapper command should be followed by exactly one file name to process"))
+			}
+			parsed, err := preprocessor.ParseFile(args[0])
 			if err != nil {
 				panic(errors.Wrap(err, "couldn't parse"))
 			}
@@ -96,113 +103,88 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-		}
-	case "proxy":
-		fs := flag.NewFlagSet("proxy", flag.ExitOnError)
-		output := newOutputFlag()
-		fs.VarP(output, "output", "o", "output file (use - for STDOUT)")
-		var reference string
-		fs.StringVarP(&reference, "code-reference", "r", "testRef", "reference to code of")
-		err := fs.Parse(os.Args[2:])
-		if err != nil {
-			panic(err)
-		}
-
-		if fs.NArg() != 1 {
-			panic(errors.New("proxy command should be followed by exactly one file name to process"))
-		}
-
-		parsed, err := preprocessor.ParseFile(fs.Arg(0))
-		if err != nil {
-			panic(errors.Wrap(err, "couldn't parse"))
-		}
-
-		err = preprocessor.GenerateContractProxy(parsed, reference, output.writer)
-		if err != nil {
-			panic(err)
-		}
-	case "imports":
-		fs := flag.NewFlagSet("imports", flag.ExitOnError)
-		output := newOutputFlag()
-		fs.VarP(output, "output", "o", "output file (use - for STDOUT)")
-		err := fs.Parse(os.Args[2:])
-		if err != nil {
-			panic(err)
-		}
-
-		if fs.NArg() != 1 {
-			panic(errors.New("imports command should be followed by exactly one file name to process"))
-		}
-
-		parsed, err := preprocessor.ParseFile(fs.Arg(0))
-		if err != nil {
-			panic(errors.Wrap(err, "couldn't parse"))
-		}
-
-		err = preprocessor.CmdRewriteImports(parsed, output.writer)
-		if err != nil {
-			panic(err)
-		}
-	// use compile [-o <output-directory>] <smart-contract-file>
-	case "compile":
-		fs := flag.NewFlagSet("compile", flag.ExitOnError)
-		dir, err := os.Getwd()
-		if err != nil {
-			panic(err)
-		}
-		var output string
-		fs.StringVarP(&output, "output", "o", dir, "output directory (default .)")
-		err = fs.Parse(os.Args[2:])
-		if err != nil {
-			panic(err)
-		}
-
-		parsed, err := preprocessor.ParseFile(fs.Arg(0))
-		if err != nil {
-			panic(err)
-		}
-
-		// make temporary dir
-		tmpDir, err := ioutil.TempDir("", "test-")
-		if err != nil {
-			panic(err)
-		}
-		defer os.RemoveAll(tmpDir) // nolint: errcheck
-
-		name := preprocessor.GetContractName(parsed)
-
-		contract, err := os.Create(tmpDir + "/" + name + ".go")
-		if err != nil {
-			panic(err)
-		}
-		defer contract.Close()
-
-		preprocessor.RewriteContractPackage(parsed, contract)
-
-		wrapper, err := os.Create(tmpDir + "/" + name + ".wrapper.go")
-		if err != nil {
-			panic(err)
-		}
-		defer wrapper.Close()
-
-		err = preprocessor.GenerateContractWrapper(parsed, wrapper)
-		if err != nil {
-			panic(err)
-		}
-
-		err = os.Chdir(tmpDir)
-		if err != nil {
-			panic(err)
-		}
-
-		out, err := exec.Command("go", "build", "-buildmode=plugin", "-o", path.Join(dir, output, name+".so")).CombinedOutput()
-		if err != nil {
-			panic(errors.Wrap(err, "can't build contract: "+string(out)))
-		}
-
-	default:
-		printUsage()
-		fmt.Printf("\n\n%q is not valid command.\n", os.Args[1])
-		os.Exit(2)
+		},
 	}
+	cmdWrapper.Flags().VarP(output, "output", "o", "output file (use - for STDOUT)")
+
+	var cmdImports = &cobra.Command{
+		Use:   "imports [flags] <file name to process>",
+		Short: "Rewrite imports in contract file",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				panic(errors.New("imports command should be followed by exactly one file name to process"))
+			}
+			parsed, err := preprocessor.ParseFile(args[0])
+			if err != nil {
+				panic(errors.Wrap(err, "couldn't parse"))
+			}
+
+			err = preprocessor.CmdRewriteImports(parsed, output.writer)
+			if err != nil {
+				panic(err)
+			}
+		},
+	}
+	cmdImports.Flags().VarP(output, "output", "o", "output file (use - for STDOUT)")
+
+	var cmdCompile = &cobra.Command{
+		Use:   "compile [flags] <file name to compile>",
+		Short: "Compile contract",
+		Run: func(cmd *cobra.Command, args []string) {
+			dir, err := os.Getwd()
+			if err != nil {
+				panic(err)
+			}
+			if len(args) != 1 {
+				panic(errors.New("compile command should be followed by exactly one file name to compile"))
+			}
+			parsed, err := preprocessor.ParseFile(args[0])
+			if err != nil {
+				panic(err)
+			}
+
+			// make temporary dir
+			tmpDir, err := ioutil.TempDir("", "test-")
+			if err != nil {
+				panic(err)
+			}
+			defer os.RemoveAll(tmpDir) // nolint: errcheck
+
+			name := preprocessor.GetContractName(parsed)
+
+			contract, err := os.Create(tmpDir + "/" + name + ".go")
+			if err != nil {
+				panic(err)
+			}
+			defer contract.Close()
+
+			preprocessor.RewriteContractPackage(parsed, contract)
+
+			wrapper, err := os.Create(tmpDir + "/" + name + ".wrapper.go")
+			if err != nil {
+				panic(err)
+			}
+			defer wrapper.Close()
+
+			err = preprocessor.GenerateContractWrapper(parsed, wrapper)
+			if err != nil {
+				panic(err)
+			}
+
+			err = os.Chdir(tmpDir)
+			if err != nil {
+				panic(err)
+			}
+
+			out, err := exec.Command("go", "build", "-buildmode=plugin", "-o", path.Join(dir, outdir, name+".so")).CombinedOutput()
+			if err != nil {
+				panic(errors.Wrap(err, "can't build contract: "+string(out)))
+			}
+		},
+	}
+	cmdCompile.Flags().StringVarP(&outdir, "output-dir", "o", ".", "output dir (default .)")
+
+	var rootCmd = &cobra.Command{Use: "insgocc"}
+	rootCmd.AddCommand(cmdProxy, cmdWrapper, cmdImports, cmdCompile)
+	rootCmd.Execute()
 }
