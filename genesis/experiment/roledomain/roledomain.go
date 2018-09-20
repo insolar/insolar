e3e20a72267b078fac0fa5c550faa953bb3e6ac3
Ivan Shibitov <ivan.shibitov@insolar.io>
2018-09-20 15:34:50 +0300

    INS-482: add RoleDomain and RoleRecord SCs

---
+++ genesis/experiment/roledomain/roledomain.go
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2018 Insolar
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package roledomain
+
+import (
+	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/genesis/experiment/roledomain/rolerecord"
+	"github.com/insolar/insolar/toolkit/go/foundation"
+)
+
+type RoleDomain struct {
+	foundation.BaseContract
+}
+
+func (rd *RoleDomain) AddNode(pk string, role core.JetRole, node core.RecordRef) core.RecordRef {
+	newRecord := rolerecord.New(pk, role, node)
+	recordHolder := rd.AsChild(newRecord.GetReference())
+	return recordHolder.GetReference()
+}
+
+func (rd *RoleDomain) RemoveNode(nodeRef core.RecordRef) {
+	node := foundation.GetObject(nodeRef)
+	node.SelfDestructRequest()
+}
