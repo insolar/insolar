837b42ac0845412b2672c1f034fe93e76d83d6b1
Andrey Bronin <andrey.bronin@insolar.io>
2018-09-20 13:14:31 +0300

    INS-447: transport tests with two IPs

--- network/hostnetwork/transport/transport_test.go
+++ network/hostnetwork/transport/transport_test.go
@@ -27,36 +27,57 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type transportSuite struct {
-	suite.Suite
-	Config    configuration.Transport
+type node struct {
+	config    configuration.Transport
 	transport Transport
 	host      *host.Host
+	address   *host.Address
 }
 
-func NewSuite(cfg configuration.Transport) *transportSuite {
-	return &transportSuite{Suite: suite.Suite{}, Config: cfg}
+type transportSuite struct {
+	suite.Suite
+	node1 node
+	node2 node
+}
+
+func NewSuite(cfg1 configuration.Transport, cfg2 configuration.Transport) *transportSuite {
+	return &transportSuite{
+		Suite: suite.Suite{},
+		node1: node{config: cfg1},
+		node2: node{config: cfg2},
+	}
+}
+
+func setupNode(t *transportSuite, n *node) {
+	var err error
+	n.address, err = host.NewAddress(n.config.Address)
+	t.Assert().NoError(err)
+
+	n.host = host.NewHost(n.address)
+
+	n.transport, err = NewTransport(n.config, relay.NewProxy())
+	t.Assert().NoError(err)
+	t.Assert().Implements((*Transport)(nil), n.transport)
 }
 
 func (t *transportSuite) SetupTest() {
-	address, err := host.NewAddress(t.Config.Address)
-	t.Assert().NoError(err)
-
-	t.host = host.NewHost(address)
-
-	t.transport, err = NewTransport(t.Config, relay.NewProxy())
-	t.Assert().NoError(err)
-	t.Assert().Implements((*Transport)(nil), t.transport)
+	setupNode(t, &t.node1)
+	setupNode(t, &t.node2)
 }
 
 func (t *transportSuite) BeforeTest(suiteName, testName string) {
-	go t.transport.Start()
+	go t.node1.transport.Start()
+	go t.node2.transport.Start()
 }
 
 func (t *transportSuite) AfterTest(suiteName, testName string) {
-	go t.transport.Stop()
-	<-t.transport.Stopped()
-	t.transport.Close()
+	go t.node1.transport.Stop()
+	<-t.node1.transport.Stopped()
+	t.node1.transport.Close()
+
+	go t.node2.transport.Stop()
+	<-t.node2.transport.Stopped()
+	t.node2.transport.Close()
 }
 
 func generateRandomBytes(n int) ([]byte, error) {
@@ -69,17 +90,17 @@ func generateRandomBytes(n int) ([]byte, error) {
 }
 
 func (t *transportSuite) TestPingPong() {
-	future, err := t.transport.SendRequest(packet.NewPingPacket(t.host, t.host))
+	future, err := t.node1.transport.SendRequest(packet.NewPingPacket(t.node1.host, t.node2.host))
 	t.Assert().NoError(err)
 
-	requestMsg := <-t.transport.Packets()
+	requestMsg := <-t.node2.transport.Packets()
 	t.Assert().True(requestMsg.IsValid())
 	t.Assert().Equal(packet.TypePing, requestMsg.Type)
-	t.Assert().Equal(t.host, future.Actor())
+	t.Assert().Equal(t.node2.host, future.Actor())
 	t.Assert().False(requestMsg.IsResponse)
 
-	builder := packet.NewBuilder().Sender(t.host).Receiver(requestMsg.Sender).Type(packet.TypePing)
-	err = t.transport.SendResponse(requestMsg.RequestID, builder.Response(nil).Build())
+	builder := packet.NewBuilder().Sender(t.node2.host).Receiver(requestMsg.Sender).Type(packet.TypePing)
+	err = t.node2.transport.SendResponse(requestMsg.RequestID, builder.Response(nil).Build())
 	t.Assert().NoError(err)
 
 	responseMsg := <-future.Result()
@@ -90,14 +111,14 @@ func (t *transportSuite) TestPingPong() {
 
 func (t *transportSuite) TestSendBigPacket() {
 	data, _ := generateRandomBytes(1024 * 1024 * 2)
-	builder := packet.NewBuilder().Sender(t.host).Receiver(t.host).Type(packet.TypeStore)
+	builder := packet.NewBuilder().Sender(t.node1.host).Receiver(t.node2.host).Type(packet.TypeStore)
 	requestMsg := builder.Request(&packet.RequestDataStore{data, true}).Build()
 	t.Assert().True(requestMsg.IsValid())
 
-	_, err := t.transport.SendRequest(requestMsg)
+	_, err := t.node1.transport.SendRequest(requestMsg)
 	t.Assert().NoError(err)
 
-	msg := <-t.transport.Packets()
+	msg := <-t.node2.transport.Packets()
 	t.Assert().True(requestMsg.IsValid())
 	t.Assert().Equal(packet.TypeStore, requestMsg.Type)
 	receivedData := msg.Data.(*packet.RequestDataStore).Data
@@ -105,28 +126,28 @@ func (t *transportSuite) TestSendBigPacket() {
 }
 
 func (t *transportSuite) TestSendInvalidPacket() {
-	builder := packet.NewBuilder().Sender(t.host).Receiver(t.host).Type(packet.TypeRPC)
+	builder := packet.NewBuilder().Sender(t.node1.host).Receiver(t.node2.host).Type(packet.TypeRPC)
 	msg := builder.Build()
 	t.Assert().False(msg.IsValid())
 
-	future, err := t.transport.SendRequest(msg)
+	future, err := t.node1.transport.SendRequest(msg)
 	t.Assert().Error(err)
 	t.Assert().Nil(future)
 }
 
 func TestUTPTransport(t *testing.T) {
-	cfg := configuration.NewHostNetwork().Transport
-	cfg.Address = "127.0.0.1:17010"
-	cfg.Protocol = "UTP"
-	cfg.BehindNAT = false
-	suite.Run(t, NewSuite(cfg))
+	//t.Skip("skip unstable tests for UTP")
+
+	cfg1 := configuration.Transport{Protocol: "UTP", Address: "127.0.0.1:17010", BehindNAT: false}
+	cfg2 := configuration.Transport{Protocol: "UTP", Address: "127.0.0.1:17011", BehindNAT: false}
+
+	suite.Run(t, NewSuite(cfg1, cfg2))
 }
 
 func TestKCPTransport(t *testing.T) {
-	t.Skip("skip unstable tests for KCP")
-	cfg := configuration.NewHostNetwork().Transport
-	cfg.Address = "127.0.0.1:17012"
-	cfg.Protocol = "KCP"
-	cfg.BehindNAT = false
-	suite.Run(t, NewSuite(cfg))
+	//t.Skip("skip unstable tests for KCP")
+	cfg1 := configuration.Transport{Protocol: "KCP", Address: "127.0.0.1:17012", BehindNAT: false}
+	cfg2 := configuration.Transport{Protocol: "KCP", Address: "127.0.0.1:17013", BehindNAT: false}
+
+	suite.Run(t, NewSuite(cfg1, cfg2))
 }
