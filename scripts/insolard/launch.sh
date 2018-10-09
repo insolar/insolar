30e198f7818906a75cda283fd294f614836a0d9d
Ivan Shibitov <ivan.shibitov@insolar.io>
2018-10-09 17:28:00 +0300

    INS-595: fix launch script

--- scripts/insolard/launch.sh
+++ scripts/insolard/launch.sh
@@ -7,10 +7,12 @@ INSOLARD=$BIN_DIR/insolard
 INSGORUND=$BIN_DIR/insgorund
 CONTRACT_STORAGE=contractstorage
 LEDGER_DIR=data
+INSGORUND_LISTEN_PORT=18181
+INSGORUND_RPS_PORT=18182
 
 stop_listening()
 {
-    ports="19191 18181 18182 8090"
+    ports="19191 $INSGORUND_LISTEN_PORT $INSGORUND_RPS_PORT 18182 8090"
     echo "Stop listening..."
     for port in $ports
     do
@@ -61,7 +63,7 @@ check_working_dir()
 
 usage()
 {
-    echo "usage: $0 <clear|rebuild>"
+    echo "usage: $0 <clear|rebuild|gorund_only>"
 }
 
 process_input_params()
@@ -79,6 +81,12 @@ process_input_params()
         exit 0
     fi
 
+    if [ "$param" == "gorund_only" ]
+    then
+        gorund_only=1
+        return 0
+    fi
+
     if [ "$param" == "help" ] || [ "$param" == "-h" ] || [ "$param" == "--help" ]
     then
         usage
@@ -86,6 +94,15 @@ process_input_params()
     fi
 }
 
+run_insgorund()
+{
+    host=127.0.0.1
+    $INSGORUND -l $host:$INSGORUND_LISTEN_PORT --rpc $host:$INSGORUND_RPS_PORT
+}
+
+trap stop_listening EXIT
+
+gorund_only=0
 param=$1
 check_working_dir
 process_input_params $param
@@ -93,5 +110,10 @@ process_input_params $param
 prepare
 build_binaries
 
-$INSGORUND -l 127.0.0.1:18181 --rpc 127.0.0.1:18182 &
-$INSOLARD --config scripts/insolard/insolar.yaml
+if [ "$gorund_only" == "1" ]
+then
+    run_insgorund
+else
+    run_insgorund &
+    $INSOLARD --config scripts/insolard/insolar.yaml
+fi
