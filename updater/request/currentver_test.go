2132a5c780263ac7473dd1dacfc00297b7ad5cc0
Andrey Pavlushev <andrey.pavlushev@insolar.io>
2018-10-09 13:30:12 +0300

    INS-580: Test files

--- updater/request/currentver_test.go
+++ updater/request/currentver_test.go
@@ -1,1 +1,8 @@
 package request
+
+import "testing"
+
+// Just to make Goland happy
+func TestStubCurrentVer(t *testing.T) {
+
+}
--- updater/request/utils_test.go
+++ updater/request/utils_test.go
@@ -1,1 +1,8 @@
 package request
+
+import "testing"
+
+// Just to make Goland happy
+func TestStub(t *testing.T) {
+
+}
