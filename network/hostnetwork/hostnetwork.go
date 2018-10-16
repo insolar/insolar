c6c8d08810d9f47d61190ab6b622f8b0f433587c
Andrey Triumfov <triumfov@yahoo.com>
2018-10-16 19:37:20 +0300

    Revert "INS-521: fixed starting at 0.0.0.0"
    This reverts commit e2eaa26

--- network/hostnetwork/hostnetwork.go
+++ network/hostnetwork/hostnetwork.go
@@ -18,6 +18,7 @@ package hostnetwork
 
 import (
 	"crypto/ecdsa"
+	"strings"
 
 	"github.com/insolar/insolar/configuration"
 	"github.com/insolar/insolar/log"
@@ -45,9 +46,9 @@ func NewHostNetwork(
 	keeper nodekeeper.NodeKeeper,
 ) (*DHT, error) {
 
-	// if strings.Contains(cfg.Transport.Address, "0.0.0.0") && !cfg.Transport.BehindNAT {
-	// 	return nil, errors.New("couldn't start at 0.0.0.0")
-	// }
+	if strings.Contains(cfg.Transport.Address, "0.0.0.0") && !cfg.Transport.BehindNAT {
+		return nil, errors.New("couldn't start at 0.0.0.0")
+	}
 
 	proxy := relay.NewProxy()
 
