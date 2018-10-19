65eaa75028c259094c0ac79245bfeb16f72a2ca4
Ivan Shibitov <ivan.shibitov@insolar.io>
2018-10-19 12:08:47 +0300

    INS-656: fix requueters tests

--- api/requesters/requesters_test.go
+++ api/requesters/requesters_test.go
@@ -34,8 +34,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const TESTSEED = "VGVzdA=="
 const TESTREFERENCE = "222222"
+const TESTSEED = "VGVzdA=="
 
 func writeReponse(response http.ResponseWriter, answer map[string]interface{}) {
 	serJSON, err := json.MarshalIndent(answer, "", "    ")
@@ -95,21 +95,29 @@ func waitForStart() error {
 	return nil
 }
 
+func startServer() error {
+	server := &http.Server{}
+	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 12221})
+	if err != nil {
+		return errors.Wrap(err, "error creating listener")
+	}
+	go server.Serve(listener)
+
+	return nil
+}
+
 func setup() error {
 	fh := FakeHandler
 	http.HandleFunc(LOCATION, fh)
 	log.Info("Starting Test api server ...")
 
-	started := make(chan bool)
-	go func() {
-		started <- true
-		if err := server.ListenAndServe(); err != nil {
-			log.Error("Test Httpserver: ListenAndServe() error: ", err)
-		}
-	}()
+	err := startServer()
+	if err != nil {
+		log.Error("Problem with starting test server: ", err)
+		return errors.Wrap(err, "[ setup ]")
+	}
 
-	<-started
-	err := waitForStart()
+	err = waitForStart()
 	if err != nil {
 		log.Error("Can't start api: ", err)
 		return errors.Wrap(err, "[ setup ]")
