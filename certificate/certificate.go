26aff27838043d6783d18048ae8fc56e28dd507a
Ivan Shibitov <ivan.shibitov@insolar.io>
2018-10-29 17:41:07 +0300

    purify error

--- certificate/certificate.go
+++ certificate/certificate.go
@@ -79,7 +79,7 @@ func NewCertificate(keysPath string, certPath string) (*Certificate, error) {
 
 	err = AreKeysTheSame(private, cert.PublicKey)
 	if err != nil {
-		return nil, errors.Wrap(err, "[ NewCertificate ]")
+		return nil, errors.Wrap(err, "[ NewCertificate ] Different keys")
 	}
 
 	cert.privateKey = private
