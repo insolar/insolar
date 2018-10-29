0845cbe2d1b3b699c1abc3bc28e8f4d81783f77b
Alexander Orlovsky <nordicdyno@gmail.com>
2018-10-29 11:31:13 +0300

    INS-693: store log trace ID with context and add getter for it

--- instrumentation/inslogger/logger.go
+++ instrumentation/inslogger/logger.go
@@ -43,6 +43,7 @@ func WithField(ctx context.Context, key string, value string) (context.Context, 
 
 // WithTraceField returns context with logger initialized with provided traceid value and logger itself.
 func WithTraceField(ctx context.Context, traceid string) (context.Context, core.Logger) {
+	ctx = setTraceID(ctx, traceid)
 	return WithField(ctx, "traceid", traceid)
 }
 
@@ -58,4 +59,19 @@ func getLogger(ctx context.Context) core.Logger {
 		return logger.GlobalLogger
 	}
 	return l.(core.Logger)
+}
+
+type traceIDKey struct{}
+
+// TraceID returns traceid provided by WithTraceField and ContextWithTrace helpers.
+func TraceID(ctx context.Context) string {
+	val := ctx.Value(traceIDKey{})
+	if val == nil {
+		return ""
+	}
+	return val.(string)
+}
+
+func setTraceID(ctx context.Context, traceid string) context.Context {
+	return context.WithValue(ctx, traceIDKey{}, traceid)
 }
