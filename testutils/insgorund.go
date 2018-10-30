b839ebdfe4c075ccd7d23ab5e0064ab6d564069e
Ruslan Zakirov <ruslan.zakirov@gmail.com>
2018-10-30 13:36:35 +0300

    improve greatly handling of insgorund in tests
    * start with log message and random identifier
    * report random identifier when stopping
    * log a error if insgorund is running for a minute with stack trace,
      helps find runaway daemons
    * run Wait() so we don't leave zombies
    * first .Kill(), then Wait() 3 seconds, then SIGTERM
    
    should help us find hang ingorund processes that we either forgot to
    stop or that are in deadlock

--- testutils/insgorund.go
+++ testutils/insgorund.go
@@ -3,14 +3,29 @@ package testutils
 import (
 	"os"
 	"os/exec"
+	"runtime/debug"
 	"syscall"
 	"time"
 
+	"github.com/insolar/insolar/log"
 	"github.com/pkg/errors"
 )
 
 // StartInsgorund starts `insgorund` process
 func StartInsgorund(cmdPath, lProto, listen, upstreamProto, upstreamAddr string) (func(), error) {
+	id := RandomString()
+	log.Debug("Starting 'insgorund' ", id)
+
+	stackTrace := (string)(debug.Stack())
+	cancelWarning := make(chan error, 1)
+	go func() {
+		select {
+		case <-time.After(60 * time.Second):
+			log.Error("'insgorund' is running for a minute, was started by:\n", stackTrace)
+		case <-cancelWarning:
+		}
+	}()
+
 	var args []string
 	if listen != "" {
 		args = append(args, "-l", listen)
@@ -45,11 +60,34 @@ func StartInsgorund(cmdPath, lProto, listen, upstreamProto, upstreamAddr string)
 	time.Sleep(200 * time.Millisecond)
 
 	return func() {
-		err := runner.Process.Signal(syscall.SIGINT)
+		log.Debug("stopping 'insgorund' ", id)
+
+		close(cancelWarning)
+
+		p := runner.Process
+		err := p.Kill()
 		if err != nil {
-			return
+			log.Error("couldn't kill process: ", err)
 		}
-		// XXX: dirty hack
-		time.Sleep(200 * time.Millisecond)
+
+		// Wait for the process to finish or kill it after a timeout:
+		done := make(chan error, 1)
+		go func() {
+			done <- runner.Wait()
+		}()
+
+		select {
+		case <-time.After(3 * time.Second):
+			log.Debug("waited for insgorund to finish and got tired")
+			if err := p.Signal(syscall.SIGTERM); err != nil {
+				log.Fatal("failed to terminate process: ", err)
+			}
+		case err := <-done:
+			if err != nil {
+				log.Debug("process finished, error: ", err)
+			} else {
+				log.Debug("process finished successfully")
+			}
+		}
 	}, nil
 }
