eb92aa81903e9f5aa8d779ffbe480e45c087c975
Andrey Triumfov <triumfov@yahoo.com>
2018-11-01 20:56:28 +0300

    NET-5: new network.Controller skeleton

---
+++ network/controller/bootstrap_controller.go
@@ -0,0 +1,139 @@
+/*
+ *    Copyright 2018 Insolar
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package controller
+
+import (
+	"encoding/gob"
+	"time"
+
+	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/log"
+	"github.com/insolar/insolar/network"
+	"github.com/insolar/insolar/network/transport/host"
+	"github.com/insolar/insolar/network/transport/packet/types"
+	"github.com/pkg/errors"
+)
+
+type BootstrapController struct {
+	options *Options
+	network network.HostNetwork
+	pinger  *Pinger
+}
+
+type BootstrapRequest struct {
+	// pass node certificate, i guess
+}
+
+type BootstrapResponse struct {
+	Code         BootstrapCode
+	RedirectHost string
+	RejectReason string
+}
+
+type BootstrapCode uint8
+
+const (
+	BootstrapAccepted = BootstrapCode(iota + 1)
+	BootstrapRejected
+	BootstrapRedirected
+)
+
+func init() {
+	gob.Register(&BootstrapRequest{})
+	gob.Register(&BootstrapResponse{})
+}
+
+func (bc *BootstrapController) Bootstrap() error {
+	bootstrapCount := len(bc.options.BootstrapHosts)
+	bootstrapHosts := make(chan *host.Host, bootstrapCount)
+	for _, bootstrapAddress := range bc.options.BootstrapHosts {
+		go func(address string, ch chan<- *host.Host) {
+			log.Infof("Starting bootstrap to address %s", address)
+			ref, err := bc.bootstrap(address)
+			if err != nil {
+				log.Errorf("Error bootstrapping to address %s: %s", address, err.Error())
+				return
+			}
+			addr, err := host.NewAddress(address)
+			if err != nil {
+				log.Errorf("Failed to resolve address: %s", address)
+			}
+			bootstrapHost := &host.Host{NodeID: ref, Address: addr}
+			bootstrapHosts <- bootstrapHost
+		}(bootstrapAddress, bootstrapHosts)
+	}
+
+	counter := 0
+	result := make([]*host.Host, 0)
+Loop:
+	for {
+		select {
+		case bootstrapHost := <-bootstrapHosts:
+			result = append(result, bootstrapHost)
+			counter++
+			if counter == bootstrapCount {
+				break Loop
+			}
+		case <-time.After(bc.options.BootstrapTimeout):
+			log.Warnf("Bootstrap timeout, successful bootstraps: %d/%d", counter, bootstrapCount)
+			break Loop
+		}
+	}
+
+	if counter == 0 {
+		return errors.New("Failed to bootstrap to any of discovery nodes")
+	}
+	return nil
+}
+
+func (bc *BootstrapController) bootstrap(address string) (core.RecordRef, error) {
+	// TODO: add infinite bootstrap
+	ref, err := bc.pinger.Ping(address, bc.options.PingTimeout)
+	if err != nil {
+		return core.RecordRef{}, errors.Wrapf(err, "Failed to ping address %s", address)
+	}
+	request := bc.network.NewRequestBuilder().Type(types.Bootstrap).Data(&BootstrapRequest{}).Build()
+	future, err := bc.network.SendRequest(request, ref)
+	if err != nil {
+		return core.RecordRef{}, errors.Wrapf(err, "Failed to ping address %s", address)
+	}
+	response, err := future.GetResponse(bc.options.BootstrapTimeout)
+	if err != nil {
+		return core.RecordRef{}, errors.Wrap(err, "Failed to get response to bootstrap request")
+	}
+	data := response.GetData().(*BootstrapResponse)
+	if data.Code == BootstrapRejected {
+		return core.RecordRef{}, errors.New("Rejected: " + data.RejectReason)
+	}
+	if data.Code == BootstrapRedirected {
+		return bc.bootstrap(data.RedirectHost)
+	}
+	return response.GetSender(), nil
+}
+
+func (bc *BootstrapController) processBootstrap(request network.Request) (network.Response, error) {
+	// TODO: check certificate and redirect logic
+	return bc.network.BuildResponse(request, &BootstrapResponse{Code: BootstrapAccepted}), nil
+}
+
+func (bc *BootstrapController) Start(components core.Components) {
+	bc.network.RegisterRequestHandler(types.Bootstrap, bc.processBootstrap)
+}
+
+func NewBootstrapController(options *Options, network network.HostNetwork, pinger *Pinger) *BootstrapController {
+	return &BootstrapController{options: options, network: network, pinger: pinger}
+}
---
+++ network/controller/controller.go
@@ -0,0 +1,96 @@
+/*
+ *    Copyright 2018 Insolar
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package controller
+
+import (
+	"github.com/insolar/insolar/configuration"
+	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/log"
+	"github.com/insolar/insolar/network"
+	"github.com/insolar/insolar/network/hostnetwork"
+	"github.com/insolar/insolar/network/transport/packet/types"
+)
+
+// Controller contains network logic.
+type Controller struct {
+	options Options
+	network network.HostNetwork
+
+	pinger              *Pinger
+	bootstrapController *BootstrapController
+}
+
+// SendMessage send message to nodeID.
+func (c *Controller) SendMessage(nodeID core.RecordRef, name string, msg core.SignedMessage) ([]byte, error) {
+	return nil, nil
+}
+
+// RemoteProcedureRegister register remote procedure that will be executed when message is received.
+func (c *Controller) RemoteProcedureRegister(name string, method core.RemoteProcedure) {
+
+}
+
+// SendCascadeMessage sends a message from MessageBus to a cascade of nodes.
+func (c *Controller) SendCascadeMessage(data core.Cascade, method string, msg core.SignedMessage) error {
+	return nil
+}
+
+// Bootstrap init bootstrap process: 1. Connect to discovery node; 2. Reconnect to new discovery node if redirected.
+func (c *Controller) Bootstrap() error {
+	return c.bootstrapController.Bootstrap()
+}
+
+// AnalyzeNetwork legacy method for old DHT network (should be removed in new network).
+func (c *Controller) AnalyzeNetwork() error {
+	log.Warn("this method was created for compatibility with old network, should be deleted")
+	return nil
+}
+
+// Authorize start authorization process on discovery node.
+func (c *Controller) Authorize() error {
+	return nil
+}
+
+// ResendPulseToKnownHosts resend pulse when we receive pulse from pulsar daemon.
+func (c *Controller) ResendPulseToKnownHosts(pulse core.Pulse) {
+
+}
+
+// GetNodeID get self node id (should be removed in far future).
+func (c *Controller) GetNodeID() core.RecordRef {
+	return core.RecordRef{}
+}
+
+// Inject inject components.
+func (c *Controller) Inject(components core.Components) {
+	c.network.RegisterRequestHandler(types.Ping, func(request network.Request) (network.Response, error) {
+		return c.network.BuildResponse(request, nil), nil
+	})
+}
+
+// NewNetworkController create new network controller.
+func NewNetworkController(
+	configuration configuration.Configuration,
+	network network.HostNetwork,
+	transport hostnetwork.InternalTransport) network.Controller {
+
+	c := Controller{}
+	c.network = network
+	c.bootstrapController = NewBootstrapController(&c.options, network, NewPinger(transport))
+
+	return &c
+}
