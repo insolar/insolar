20a9a8c05202b457db9d0ae301a0d08b436c9be0
egorikas <egorgrishechko@gmail.com>
2018-11-06 18:01:44 +0300

    INS-731
    CallConstructor

--- core/message/messagehelpers.go
+++ core/message/messagehelpers.go
@@ -6,6 +6,11 @@ func Extract(msg core.Message) core.RecordRef {
 	switch m := msg.(type) {
 	case *BootstrapRequest:
 		return core.NewRefFromBase58(m.Name)
+	case *CallConstructor:
+		if m.SaveAs == Delegate {
+			return m.ParentRef
+		}
+		return *core.GenRequest(m.PulseNum, MustSerializeBytes(m))
 	default:
 		panic("unknow message type")
 	}
@@ -14,6 +19,8 @@ func ExtractRole(msg core.Message) core.JetRole {
 	switch _ := msg.(type) {
 	case *BootstrapRequest:
 		return core.RoleLightExecutor
+	case *CallConstructor:
+		return core.RoleVirtualExecutor
 	default:
 		panic("unknow message type")
 	}
