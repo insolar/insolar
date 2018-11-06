46fefdc68d4aed6b2022ce77506df4fac393827d
egorikas <egorgrishechko@gmail.com>
2018-11-06 17:55:29 +0300

    INS-731
    type switch with default panic

--- core/message/messagehelpers.go
+++ core/message/messagehelpers.go
@@ -1,5 +1,13 @@
 package message
 func Extract(msg core.Message) core.RecordRef {
+	switch m := msg.(type) {
+	default:
+		panic("unknow message type")
+	}
 }
 func ExtractRole(msg core.Message) core.JetRole {
+	switch _ := msg.(type) {
+	default:
+		panic("unknow message type")
+	}
 }
