b7616ea5a802d7e1ca6ca9b53b824375a072122a
egorikas <egorgrishechko@gmail.com>
2018-11-06 15:38:10 +0300

    INS-731
    Fix TestDHT_MessageSign

--- network/dhtnetwork/dht_test.go
+++ network/dhtnetwork/dht_test.go
@@ -1011,7 +1011,7 @@ func TestDHT_MessageSign(t *testing.T) {
 	ref := testutils.RandomRef()
 
 	tmp := core.Message(&message.BootstrapRequest{})
-	msg, err := message.NewSignedMessage(context.TODO(), tmp, ref, key, 0)
+	msg, err := message.NewSignedMessage(context.TODO(), tmp, ref, key, 0, core.Token{})
 	assert.NoError(t, err)
 	assert.True(t, msg.IsValid(&key.PublicKey))
 	assert.False(t, msg.IsValid(&key2.PublicKey))
