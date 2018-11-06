a5dbc20bc8afd063ae9e27e92b660500da6984e6
egorikas <egorgrishechko@gmail.com>
2018-11-06 18:01:11 +0300

    INS-731
    BootstrapRequest

--- core/message/messagehelpers.go
+++ core/message/messagehelpers.go
@@ -1,12 +1,19 @@
 package message
+import (
+	"github.com/insolar/insolar/core"
+)
 func Extract(msg core.Message) core.RecordRef {
 	switch m := msg.(type) {
+	case *BootstrapRequest:
+		return core.NewRefFromBase58(m.Name)
 	default:
 		panic("unknow message type")
 	}
 }
 func ExtractRole(msg core.Message) core.JetRole {
 	switch _ := msg.(type) {
+	case *BootstrapRequest:
+		return core.RoleLightExecutor
 	default:
 		panic("unknow message type")
 	}
