a1beb04b9526ee7e764da7899a9c47e119ece871
egorikas <egorgrishechko@gmail.com>
2018-11-06 15:30:13 +0300

    INS-731
    Adding new token to message bus

--- messagebus/messagebus.go
+++ messagebus/messagebus.go
@@ -91,6 +91,8 @@ func (mb *MessageBus) Send(ctx context.Context, msg core.Message) (core.Reply, e
 		return nil, err
 	}
 
+	to := mb.Service.GetNodeID()
+	token := core.NewToken(msg.Target(), &to, pulse.PulseNumber, mb.Service.GetPrivateKey())
 	signedMsg, err := message.NewSignedMessage(
 		ctx, msg, mb.Service.GetNodeID(), mb.Service.GetPrivateKey(), pulse.PulseNumber, *token,
 	)
