1710f20b349286639847c663301744ed02c000e0
Andrey Bronin <andrey.bronin@insolar.io>
2018-11-06 18:23:26 +0300

    NET-42: initial naive communicator impl

--- consensus/phases/communicator.go
+++ consensus/phases/communicator.go
@@ -19,24 +19,71 @@ package phases
 import (
 	"context"
 
-	"github.com/insolar/insolar/consensus/packets"
 	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/log"
 	"github.com/insolar/insolar/network"
+	"github.com/insolar/insolar/network/transport/packet/types"
+	"github.com/pkg/errors"
 )
 
 // Communicator interface provides methods to exchange data between nodes
 //go:generate minimock -i github.com/insolar/insolar/consensus/phases.Communicator -o ../../testutils/network -s _mock.go
 type Communicator interface {
 	// ExchangeData used in first consensus step to exchange data between participants
-	ExchangeData(ctx context.Context, participants []core.Node, pulseData packets.PulseData, packet packets.Phase1Packet) (map[core.RecordRef]packets.Phase1Packet, error)
+	ExchangeData(ctx context.Context, participants []core.Node, pulseData PulseData, packet Phase1Packet) (map[core.RecordRef]Phase1Packet, error)
 }
 
 // NaiveCommunicator is simple Communicator implementation which communicates with each participants
 type NaiveCommunicator struct {
-	HostNetwork network.HostNetwork `inject:""`
+	HostNetwork  network.HostNetwork `inject:""`
+	phase1packet *Phase1Packet
+}
+
+// NewNaiveCommunicator constructor creates new NaiveCommunicator
+func NewNaiveCommunicator() *NaiveCommunicator {
+	return &NaiveCommunicator{}
+}
+
+// Start method implements Starter interface
+func (nc *NaiveCommunicator) Start(ctx context.Context) error {
+	nc.HostNetwork.RegisterRequestHandler(types.ConsensusPhase1Exchange, nc.exchangeDataHandler)
+	return nil
 }
 
 // ExchangeData used in first consensus phase to exchange data between participants
-func (nc *NaiveCommunicator) ExchangeData(ctx context.Context, participants []core.Node, pulseData packets.PulseData, packet packets.Phase1Packet) (map[core.RecordRef]packets.Phase1Packet, error) {
+func (nc *NaiveCommunicator) ExchangeData(ctx context.Context, participants []core.Node, pulseData PulseData, packet Phase1Packet) (map[core.RecordRef]Phase1Packet, error) {
+	futures := make([]network.Future, len(participants))
+
+	packetBuffer, err := packet.Serialize()
+	if err != nil {
+		return nil, errors.Wrap(err, "[ExchangeData] Failed to serialize Phase1Packet.")
+	}
+
+	requestBuilder := nc.HostNetwork.NewRequestBuilder()
+	request := requestBuilder.Type(types.ConsensusPhase1Exchange).Data(packetBuffer).Build()
+
+	for _, node := range participants {
+		future, err := nc.HostNetwork.SendRequest(request, node.ID())
+		if err != nil {
+			// TODO: mark participant as unreachable
+			log.Errorln(err.Error())
+		} else {
+			futures = append(futures, future)
+		}
+	}
+
+	//TODO: get futures results
+	/*
+		for _, f := range futures {
+			f.GetResponse(time.Second)
+		}
+	*/
+
+	return nil, nil
+}
+
+func (nc *NaiveCommunicator) exchangeDataHandler(network.Request) (network.Response, error) {
+	//TODO: check pulse?
+	//TODO: return serialized nc.phase1packet
 	panic("implement me")
 }
