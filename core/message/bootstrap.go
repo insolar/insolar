3430e6622225681141f9655d26a65a16462a922c
egorikas <egorgrishechko@gmail.com>
2018-11-07 13:11:57 +0300

    INS-731
    Remove useless methods from bootstrap

--- core/message/bootstrap.go
+++ core/message/bootstrap.go
@@ -31,17 +31,6 @@ func (*BootstrapRequest) Type() core.MessageType {
 	return core.TypeBootstrapRequest
 }
 
-// Target implementation for bootstrap request.
-func (m *BootstrapRequest) Target() *core.RecordRef {
-	ref := core.NewRefFromBase58(m.Name)
-	return &ref
-}
-
-// TargetRole implementation for bootstrap request.
-func (*BootstrapRequest) TargetRole() core.JetRole {
-	return core.RoleLightExecutor
-}
-
 // GetCaller implementation for bootstrap request.
 func (*BootstrapRequest) GetCaller() *core.RecordRef {
 	return nil
