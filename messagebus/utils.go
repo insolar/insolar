9661d8b12e94a39958c0467ef3ddc7fd4f5cbacd
Andrey Romancev <andrey@romancev.com>
2018-11-09 13:01:26 +0300

    INS-741: Add message bus utils docs.

--- messagebus/utils.go
+++ messagebus/utils.go
@@ -1,11 +1,13 @@
 package messagebus
 
 import (
+	"github.com/insolar/insolar/cryptohelpers/hash"
+
 	"github.com/insolar/insolar/core"
 	"github.com/insolar/insolar/core/message"
-	"github.com/insolar/insolar/cryptohelpers/hash"
 )
 
+// GetMessageHash calculates message hash.
 func GetMessageHash(msg core.SignedMessage) []byte {
 	return hash.SHA3Bytes256(message.SignedToBytes(msg))
 }
