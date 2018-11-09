c5ed7a072cad0fcbf264e644c5ce05d1040c45f8
Andrey Romancev <andrey@romancev.com>
2018-11-09 12:28:28 +0300

    INS-741: Add tape factory methods.

--- messagebus/tape.go
+++ messagebus/tape.go
@@ -31,3 +31,46 @@ type couple struct {
 	Key   []byte
 	Value []byte
 }
+
+// NewTape creates new storagetape with random id.
+func NewTape(ls core.LocalStorage, pulse core.PulseNumber) (*storagetape, error) {
+	id, err := uuid.NewV4()
+	if err != nil {
+		return nil, err
+	}
+	return &storagetape{ls: ls, pulse: pulse, id: id}, nil
+}
+
+// NewTapeFromReader creates and fills a new storagetape from a stream.
+//
+// This is a very long operation, as it saves replies in storage until the stream is exhausted.
+func NewTapeFromReader(ctx context.Context, ls core.LocalStorage, reader io.Reader) (*storagetape, error) {
+	var err error
+	tape := storagetape{ls: ls}
+
+	decoder := gob.NewDecoder(reader)
+	err = decoder.Decode(&tape.pulse)
+	if err != nil {
+		return nil, err
+	}
+	err = decoder.Decode(&tape.id)
+	if err != nil {
+		return nil, err
+	}
+	for {
+		var rep couple
+		err = decoder.Decode(&rep)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		err = tape.setReplyBinary(ctx, rep.Key, rep.Value)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &tape, nil
+}
