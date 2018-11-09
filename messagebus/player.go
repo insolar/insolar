53e6ae8f5d3c8183dc5e0943db87dd3a65cb6932
Andrey Romancev <andrey@romancev.com>
2018-11-09 13:24:08 +0300

    INS-741: Add player docs.

--- messagebus/player.go
+++ messagebus/player.go
@@ -8,20 +8,26 @@ import (
 	"github.com/insolar/insolar/ledger/localstorage"
 )
 
+// Player is a MessageBus wrapper that replays replies from provided tape. The tape can be created and by Recorder
+// and transferred to player.
 type player struct {
 	sender
 	tape tape
 	pm   core.PulseManager
 }
 
+// NewPlayer creates player instance. It will replay replies from provided tape.
 func NewPlayer(s sender, tape tape, pm core.PulseManager) (*player, error) {
 	return &player{sender: s, tape: tape, pm: pm}, nil
 }
 
+// WriteTape for player is not available.
 func (r *player) WriteTape(ctx context.Context, w io.Writer) error {
 	panic("can't write the tape from player")
 }
 
+// Send wraps MessageBus Send to reply replies from the tape. If reply for this message is not on the tape, an error
+// will be returned.
 func (r *player) Send(ctx context.Context, msg core.Message) (core.Reply, error) {
 	var (
 		rep core.Reply
