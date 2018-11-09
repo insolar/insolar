1969bce378d756ffc0829aa417be1ace3a08b8a3
Ivan Shibitov <ivan.shibitov@insolar.io>
2018-11-09 16:33:34 +0300

    NET-61: fix tests

--- consensus/phases/communicator.go
+++ consensus/phases/communicator.go
@@ -27,7 +27,7 @@ import (
 // Communicator interface provides methods to exchange data between nodes
 type Communicator interface {
 	// ExchangeData used in first consensus step to exchange data between participants
-	ExchangeData(ctx context.Context, participants []core.Node, pulseData packets.PulseData, packet packets.Phase1Packet) (map[core.RecordRef]data.Phase1Packet, error)
+	ExchangeData(ctx context.Context, participants []core.Node, pulseData packets.PulseData, packet packets.Phase1Packet) (map[core.RecordRef]packets.Phase1Packet, error)
 }
 
 // NaiveCommunicator is simple Communicator implementation which communicates with each participants
@@ -36,6 +36,6 @@ type NaiveCommunicator struct {
 }
 
 // ExchangeData used in first consensus phase to exchange data between participants
-func (nc *NaiveCommunicator) ExchangeData(ctx context.Context, participants []core.Node, pulseData packets.PulseData, packet data.Phase1Packet) (map[core.RecordRef]data.Phase1Packet, error) {
+func (nc *NaiveCommunicator) ExchangeData(ctx context.Context, participants []core.Node, pulseData packets.PulseData, packet packets.Phase1Packet) (map[core.RecordRef]packets.Phase1Packet, error) {
 	panic("implement me")
 }
