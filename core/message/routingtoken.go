1bc031401fdf0b255ef0c7543aa2adf5be670de9
Dmitry Panchenko <dmitry.panchenko@ins.world>
2018-11-12 15:17:17 +0300

    NET-105: remove new routing token

--- core/message/routingtoken.go
+++ core/message/routingtoken.go
@@ -1,14 +1,9 @@
 package message
 
 import (
-	"bytes"
-	"crypto/ecdsa"
-	"encoding/gob"
 	"crypto"
 
 	"github.com/insolar/insolar/core"
-	crypto_helper "github.com/insolar/insolar/cryptohelpers/ecdsa"
-	"github.com/pkg/errors"
 )
 
 type RoutingTokenFactory interface {
@@ -42,65 +37,4 @@ func (t *RoutingToken) GetPulse() core.PulseNumber {
 // GetSign returns token's sign
 func (t *RoutingToken) GetSign() []byte {
 	return t.Sign
-}
-
-// NewRoutingToken creates new token with sign of its fields
-func NewRoutingToken(to *core.RecordRef, from *core.RecordRef, pulseNumber core.PulseNumber, msgHash []byte, key *ecdsa.PrivateKey) *RoutingToken {
-	token := &RoutingToken{
-		To:    to,
-		From:  from,
-		Pulse: pulseNumber,
-	}
-
-	var tokenBuffer bytes.Buffer
-	enc := gob.NewEncoder(&tokenBuffer)
-	err := enc.Encode(to)
-	if err != nil {
-		panic(err)
-	}
-	err = enc.Encode(from)
-	if err != nil {
-		panic(err)
-	}
-	err = enc.Encode(pulseNumber)
-	if err != nil {
-		panic(err)
-	}
-	tokenBuffer.Write(msgHash)
-
-	sign, err := crypto_helper.Sign(tokenBuffer.Bytes(), key)
-	if err != nil {
-		panic(err)
-	}
-	token.Sign = sign
-	return token
-}
-
-// ValidateRoutingToken checks that a routing token is valid
-func ValidateRoutingToken(pubKey *ecdsa.PublicKey, token core.RoutingToken, msgHash []byte) error {
-	var tokenBuffer bytes.Buffer
-	enc := gob.NewEncoder(&tokenBuffer)
-	err := enc.Encode(token.GetTo())
-	if err != nil {
-		panic(err)
-	}
-	err = enc.Encode(token.GetFrom())
-	if err != nil {
-		panic(err)
-	}
-	err = enc.Encode(token.GetPulse())
-	if err != nil {
-		panic(err)
-	}
-	tokenBuffer.Write(msgHash)
-
-	ok, err := crypto_helper.VerifyWithFullKey(tokenBuffer.Bytes(), token.GetSign(), pubKey)
-	if err != nil {
-		return err
-	}
-	if !ok {
-		return errors.New("token isn't valid")
-	}
-
-	return nil
 }
