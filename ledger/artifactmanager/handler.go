a5af520898eac29b89e6f06ddfc550ce994e4537
egorikas <egorgrishechko@gmail.com>
2018-11-14 16:50:24 +0300

    INS-731 remove go

--- ledger/artifactmanager/handler.go
+++ ledger/artifactmanager/handler.go
@@ -209,7 +209,7 @@ func (h *MessageHandler) handleGetChildren(ctx context.Context, pulseNumber core
 	if err != nil {
 		return nil, err
 	}
-	go h.recentObjects.AddID(msg.Parent.Record())
+	h.recentObjects.AddID(msg.Parent.Record())
 
 	var (
 		refs         []core.RecordRef
@@ -275,7 +275,7 @@ func (h *MessageHandler) handleUpdateObject(ctx context.Context, pulseNumber cor
 		if idx.LatestState != nil && !state.PrevStateID().Equal(idx.LatestState) {
 			return errors.New("invalid state record")
 		}
-		go h.recentObjects.AddID(msg.Object.Record())
+		h.recentObjects.AddID(msg.Object.Record())
 
 		id, err := tx.SetRecord(ctx, pulseNumber, rec)
 		if err != nil {
@@ -294,7 +294,7 @@ func (h *MessageHandler) handleUpdateObject(ctx context.Context, pulseNumber cor
 		}
 		return nil, err
 	}
-	go h.recentObjects.AddID(msg.Object.Record())
+	h.recentObjects.AddID(msg.Object.Record())
 
 	rep := reply.Object{
 		Head:         msg.Object,
@@ -322,7 +322,7 @@ func (h *MessageHandler) handleRegisterChild(ctx context.Context, pulseNumber co
 		if err != nil {
 			return err
 		}
-		go h.recentObjects.AddID(msg.Parent.Record())
+		h.recentObjects.AddID(msg.Parent.Record())
 
 		// Children exist and pointer does not match (preserving chain consistency).
 		if idx.ChildPointer != nil && !childRec.PrevChild.Equal(idx.ChildPointer) {
@@ -348,7 +348,7 @@ func (h *MessageHandler) handleRegisterChild(ctx context.Context, pulseNumber co
 	if err != nil {
 		return nil, err
 	}
-	go h.recentObjects.AddID(msg.Parent.Record())
+	h.recentObjects.AddID(msg.Parent.Record())
 
 	return &reply.ID{ID: *child}, nil
 }
@@ -429,7 +429,7 @@ func (h *MessageHandler) handleValidateRecord(ctx context.Context, pulseNumber c
 	if err != nil {
 		return nil, err
 	}
-	go h.recentObjects.AddID(msg.Object.Record())
+	h.recentObjects.AddID(msg.Object.Record())
 
 	return &reply.OK{}, nil
 }
