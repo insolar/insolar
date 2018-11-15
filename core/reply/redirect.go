aee9db45489dae47c82657fe713c42a92fdbbef0
egorikas <egorgrishechko@gmail.com>
2018-11-15 19:35:14 +0300

    INS-770 adding prepareRedirect method

--- core/reply/redirect.go
+++ core/reply/redirect.go
@@ -1,1 +1,24 @@
 package reply
+
+import (
+	"github.com/insolar/insolar/core"
+)
+
+type Redirect struct {
+	To   core.RecordRef
+	Sign core.Signature
+}
+
+func (r *Redirect) Type() core.ReplyType {
+	return TypeRedirect
+}
+
+
+type DefinedStateRedirect struct {
+	Redirect
+	StateID core.RecordID
+}
+
+func (r *DefinedStateRedirect) Type() core.ReplyType {
+	return TypeDefinedStateRedirect
+}
