675d5715eb3ccfb2f651d9f3f05e1bfaff13e23b
egorikas <egorgrishechko@gmail.com>
2018-11-16 15:51:57 +0300

    INS-770 redirect struct

--- core/reply/redirect.go
+++ core/reply/redirect.go
@@ -4,18 +4,31 @@ import (
 	"github.com/insolar/insolar/core"
 )
 
-type Redirect struct {
-	To   core.RecordRef
-	Sign core.Signature
+type Redirect interface {
+	core.Reply
+	GetTo() *core.RecordRef
+	GetSign() *core.Signature
 }
 
-func (r *Redirect) Type() core.ReplyType {
+type GenericRedirect struct {
+	To   *core.RecordRef
+	Sign *core.Signature
+}
+
+func (r *GenericRedirect) GetTo() *core.RecordRef {
+	return r.To
+}
+
+func (r *GenericRedirect) GetSign() *core.Signature {
+	return r.Sign
+}
+
+func (r *GenericRedirect) Type() core.ReplyType {
 	return TypeRedirect
 }
 
-
 type DefinedStateRedirect struct {
-	Redirect
+	GenericRedirect
 	StateID core.RecordID
 }
 
