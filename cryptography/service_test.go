4cfc1c21a408c6c3c27e184c9bf83d880664ac70
Ivan Shibitov <ivan.shibitov@insolar.io>
2018-11-16 19:34:49 +0300

    NET-117-move-root-domain-reference-to-certificate: assert -> require: cryptography

--- cryptography/service_test.go
+++ cryptography/service_test.go
@@ -19,12 +19,12 @@ package cryptography
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const TestBadCert = "testdata/bad_keys.json"
 
 func TestReadPrivateKey_BadPrivateKey(t *testing.T) {
 	_, err := NewStorageBoundCryptographyService(TestBadCert)
-	assert.Contains(t, err.Error(), "Failed to create KeyStore")
+	require.Contains(t, err.Error(), "Failed to create KeyStore")
 }
