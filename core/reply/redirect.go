d9dbf8477f34ae89ee318cd092bbbc9da5f6340f
egorikas <egorgrishechko@gmail.com>
2018-11-19 13:50:49 +0300

    Refactoring of redirect removing interface

--- core/reply/redirect.go
+++ core/reply/redirect.go
@@ -5,51 +5,22 @@ import (
 	"github.com/insolar/insolar/core/message"
 )
 
-type Redirect interface {
-	core.Reply
-	GetTo() *core.RecordRef
-	GetSign() *core.Signature
-	SetSign(sign *core.Signature)
-	RecreateMessage(genericMessage core.Message) core.Message
-}
-
-type GenericRedirect struct {
-	To   *core.RecordRef
-	Sign *core.Signature
-}
-
-func (r *GenericRedirect) GetTo() *core.RecordRef {
-	return r.To
-}
-
-func (r *GenericRedirect) GetSign() *core.Signature {
-	return r.Sign
-}
-
 type ObjectRedirect struct {
-	GenericRedirect
-	StateID core.RecordID
-}
+	core.Reply
 
-func (r *ObjectRedirect) SetSign(sign *core.Signature) {
-	r.Sign = sign
+	To *core.RecordRef
+	StateID *core.RecordID
 }
 
 func (r *ObjectRedirect) RecreateMessage(genericMessage core.Message) core.Message {
 	getObjectRequest := genericMessage.(*message.GetObject)
-	getObjectRequest.State = &r.StateID
+	getObjectRequest.State = r.StateID
 	return getObjectRequest
-}
-
-func (r *ObjectRedirect) Type() core.ReplyType {
-	return TypeGetObjectRedirect
 }
 
 func NewObjectRedirect(to *core.RecordRef, state *core.RecordID) *ObjectRedirect {
 	return &ObjectRedirect{
-		GenericRedirect: GenericRedirect{
-			To: to,
-		},
-		StateID: *state,
+		To: to,
+		StateID: state,
 	}
 }
