1dc9ba5e485d0e2d420a887b7a0d8121b62586a2
egorikas <egorgrishechko@gmail.com>
2018-11-21 16:02:06 +0300

    INS-770 factory remove useless code

--- core/delegationtoken/factory.go
+++ core/delegationtoken/factory.go
@@ -72,23 +72,6 @@ func (f *delegationTokenFactory) Verify(parcel core.Parcel) (bool, error) {
 	return parcel.DelegationToken().Verify(parcel)
 }
 
-// func (f *delegationTokenFactory) newFromBytes(data []byte) (core.DelegationToken, error) {
-// 	if len(data) == 0 {
-// 		return nil, nil
-// 	}
-//
-// 	res, err := empty(core.DelegationTokenType(data[0]))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	err = gob.NewDecoder(bytes.NewReader(data[1:])).Decode(res)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return res, nil
-// }
-
 func empty(t core.DelegationTokenType) (core.DelegationToken, error) {
 	switch t {
 
