4c5c9902e4ce10a82529df0b864cd98014072092
egorikas <egorgrishechko@gmail.com>
2018-11-21 14:15:52 +0300

    INS-770 remove comments

--- core/reply/redirect.go
+++ core/reply/redirect.go
@@ -7,16 +7,15 @@ import (
 type GetObjectRedirectReply struct {
 	core.Reply
 
-	To *core.RecordRef
+	To      *core.RecordRef
 	StateID *core.RecordID
 
 	Token core.DelegationToken
 }
 
-
 func NewGetObjectRedirectReply(to *core.RecordRef, state *core.RecordID) *GetObjectRedirectReply {
 	return &GetObjectRedirectReply{
-		To: to,
+		To:      to,
 		StateID: state,
 	}
 }
@@ -25,16 +24,3 @@ func NewGetObjectRedirectReply(to *core.RecordRef, state *core.RecordID) *GetObj
 func (r *GetObjectRedirectReply) Type() core.ReplyType {
 	return TypeGetObjectRedirect
 }
-
-// func (r *GetObjectRedirectReply) RecreateMessage(genericMessage core.Message) core.Message {
-// 	getObjectRequest := genericMessage.(*message.GetObject)
-// 	getObjectRequest.State = r.StateID
-// 	return getObjectRequest
-// }
-//
-// func (r *GetObjectRedirectReply) CreateToken(genericMessage core.Parcel) []byte {
-// 	newMessage := r.RecreateMessage(genericMessage.Message())
-// 	dataForSign := append(genericMessage.GetSender().Bytes(), message.ToBytes(newMessage)...)
-// 	return dataForSign
-// }
-
