0ac547aaa950d1c4646f2d3bfec5c278fc18a17b
Andrey Romancev <andrey@romancev.com>
2018-11-21 11:59:23 +0300

    INS-784: Update copyright.

--- ledger/storage/logger.go
+++ ledger/storage/logger.go
@@ -1,5 +1,3 @@
-// +build !debug
-
 /*
  *    Copyright 2018 Insolar
  *
@@ -15,6 +13,8 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
+// +build !debug
 
 package storage
 
