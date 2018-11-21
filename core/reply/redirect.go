6371b67e8fe6c2c45f3e63de50ab17d8d7aa5b4d
egorikas <egorgrishechko@gmail.com>
2018-11-21 16:21:26 +0300

    INS-770 create new helper method for redirect

--- core/reply/redirect.go
+++ core/reply/redirect.go
@@ -2,8 +2,10 @@ package reply
 
 import (
 	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/core/message"
 )
 
+// GetObjectRedirectReply is a redirect-reply for get object
 type GetObjectRedirectReply struct {
 	core.Reply
 
@@ -13,6 +15,7 @@ type GetObjectRedirectReply struct {
 	Token core.DelegationToken
 }
 
+// NewGetObjectRedirectReply return new GetObjectRedirectReply
 func NewGetObjectRedirectReply(to *core.RecordRef, state *core.RecordID) *GetObjectRedirectReply {
 	return &GetObjectRedirectReply{
 		To:      to,
@@ -23,4 +26,13 @@ func NewGetObjectRedirectReply(to *core.RecordRef, state *core.RecordID) *GetObj
 // Type returns type of the reply
 func (r *GetObjectRedirectReply) Type() core.ReplyType {
 	return TypeGetObjectRedirect
+}
+
+// RecreateMessage recreates the message on the base of token
+func (r *GetObjectRedirectReply) RecreateMessage(msg message.GetObject) *message.GetObject {
+	return &message.GetObject{
+		State:    r.StateID,
+		Head:     msg.Head,
+		Approved: msg.Approved,
+	}
 }
