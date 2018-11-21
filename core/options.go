9b0215fcd97c89e78a6bc2f61ae2a899d62e6259
egorikas <egorgrishechko@gmail.com>
2018-11-21 10:15:23 +0300

    INS-770 add options builder to mb

--- core/options.go
+++ core/options.go
@@ -1,1 +1,20 @@
 package core
+
+type Options struct {
+	Receiver *RecordRef
+	Token    DelegationToken
+}
+
+type Option func(*Options)
+
+func Receiver(receiver *RecordRef) Option {
+	return func(args *Options) {
+		args.Receiver = receiver
+	}
+}
+
+func Token(token DelegationToken) Option {
+	return func(args *Options) {
+		args.Token = token
+	}
+}
