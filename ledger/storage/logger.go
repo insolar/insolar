b7cf7eab2201665342ad509d3ec861905eceda7d
Andrey Romancev <andrey@romancev.com>
2018-11-21 12:28:29 +0300

    INS-784: Fix build debug.

--- ledger/storage/logger.go
+++ ledger/storage/logger.go
@@ -1,3 +1,5 @@
+// +build !debug
+
 /*
  *    Copyright 2018 Insolar
  *
@@ -13,8 +15,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
-// +build !debug
 
 package storage
 
