a9957041380a806cb5d1c54195b34c499915e4c7
Andrey Romancev <andrey@romancev.com>
2018-11-21 11:50:54 +0300

    INS-784: Add export method to exporter.

--- ledger/exporter/exporter.go
+++ ledger/exporter/exporter.go
@@ -38,3 +38,86 @@ type pulseData struct {
 	Records recordsData
 	Pulse   core.Pulse
 }
+
+func (e *Exporter) Export(ctx context.Context, fromPulse core.PulseNumber, size int) (*core.ExportResult, error) {
+	result := core.ExportResult{Data: map[string]interface{}{}}
+
+	counter := 0
+	currentPN := core.PulseNumber(math.Max(float64(fromPulse), float64(core.GenesisPulse.PulseNumber)))
+	current := &currentPN
+	for current != nil && counter < size {
+		pulse, err := e.db.GetPulse(ctx, *current)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to fetch pulse data 22222")
+		}
+		data, err := e.exportPulse(ctx, &pulse.Pulse)
+		if err != nil {
+			return nil, err
+		}
+		result.Data[strconv.FormatUint(uint64(pulse.Pulse.PulseNumber), 10)] = *data
+
+		current = pulse.Next
+		counter++
+	}
+
+	result.Size = counter
+	result.NextFrom = current
+
+	return &result, nil
+}
+
+func (e *Exporter) exportPulse(ctx context.Context, pulse *core.Pulse) (*pulseData, error) {
+	records := recordsData{}
+	err := e.db.IterateRecords(ctx, pulse.PulseNumber, func(id core.RecordID, rec record.Record) error {
+		pl, err := e.getPayload(ctx, rec)
+		if err != nil {
+			return err
+		}
+		records[string(base58.Encode(id[:]))] = recordData{
+			Type:    strings.Title(rec.Type().String()),
+			Data:    rec,
+			Payload: pl,
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	data := pulseData{
+		Records: records,
+		Pulse:   *pulse,
+	}
+
+	return &data, nil
+}
+
+func (e *Exporter) getPayload(ctx context.Context, rec record.Record) (payload, error) {
+	switch r := rec.(type) {
+	case record.ObjectState:
+		if r.GetMemory() == nil {
+			break
+		}
+		blob, err := e.db.GetBlob(ctx, r.GetMemory())
+		if err != nil {
+			return nil, err
+		}
+		memory := payload{}
+		err = codec.NewDecoderBytes(blob, &codec.CborHandle{}).Decode(&memory)
+		if err != nil {
+			return payload{"MemoryBinary": blob}, nil
+		}
+		return payload{"Memory": memory}, nil
+	case record.Request:
+		if r.GetPayload() == nil {
+			break
+		}
+		parcel, err := message.DeserializeParcel(bytes.NewBuffer(r.GetPayload()))
+		if err != nil {
+			return payload{"PayloadBinary": r.GetPayload()}, nil
+		}
+		return payload{"Payload": parcel}, nil
+	}
+
+	return nil, nil
+}
