b0c2c8c65ca4897ab595485f8e373491604a1772
vany <ivan.serezhkin@insolar.io>
2018-11-22 13:32:57 +0300

    INS-819: More proper error check

--- logicrunner/logicrunner.go
+++ logicrunner/logicrunner.go
@@ -467,7 +467,7 @@ func (lr *LogicRunner) executeMethodCall(es *ExecutionState, m *message.CallMeth
 					ctx, Ref{}, *es.request, es.objectbody.objDescriptor, newData,
 				)
 				err = e
-				if od != nil {
+				if od != nil && e == nil {
 					es.objectbody.objDescriptor = od
 				}
 			}
