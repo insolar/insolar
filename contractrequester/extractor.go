9066667afdd8c1a4e0a2b415f922a4e99099d3f6
Maria Zubareva <maria.zubareva@insolar.io>
2018-11-22 14:10:01 +0300

    NET-171: add some extractors

--- contractrequester/extractor.go
+++ contractrequester/extractor.go
@@ -52,3 +52,76 @@ func ExtractInfoResponse(data []byte) (*InfoResponse, error) {
 
 	return &info, nil
 }
+
+func ExtractAuthorizeResponse(data []byte) (string, core.NodeRole, error) {
+	var pubKey string
+	var role core.NodeRole
+	var fErr *foundation.Error
+
+	_, err := core.UnMarshalResponse(data, []interface{}{&pubKey, &role, &fErr})
+	if err != nil {
+		return "", core.RoleUnknown, errors.Wrap(err, "[ ExtractAuthorizeResponse ] Can't unmarshal")
+	}
+
+	if fErr != nil {
+		return "", core.RoleUnknown, errors.Wrap(fErr, "[ ExtractAuthorizeResponse ] Has error")
+	}
+
+	return pubKey, role, nil
+}
+
+// ExtractRegisterNodeResponse extracts response of RegisterNode
+func ExtractRegisterNodeResponse(data []byte) ([]byte, error) {
+	var holderData []byte
+	holderError := &foundation.Error{}
+	raw, err := core.UnMarshalResponse(data, []interface{}{holderData, holderError})
+	if err != nil {
+		return nil, errors.Wrap(err, "[ ExtractRegisterNodeResponse ] Can't unmarshal")
+	}
+	if len(raw) != 2 {
+		return nil, errors.New("[ ExtractRegisterNodeResponse ] No enough values")
+	}
+
+	if raw[1] != nil {
+		return nil, errors.Wrap(raw[1].(error), "[ ExtractRegisterNodeResponse ] Has error")
+	}
+
+	if raw[0] == nil {
+		return nil, errors.New("[ ExtractRegisterNodeResponse ] Empty data")
+	}
+
+	rawJSON, ok := raw[0].([]byte)
+	if !ok {
+		return nil, errors.New("[ ExtractRegisterNodeResponse ] Bad data type")
+	}
+
+	return rawJSON, nil
+}
+
+// ExtractNodeRef extract reference from json response
+func ExtractNodeRef(rawJSON []byte) (string, error) {
+	type NodeRef struct {
+		Ref string `json:"reference"`
+	}
+
+	nRef := NodeRef{}
+	err := json.Unmarshal(rawJSON, &nRef)
+	if err != nil {
+		return "", errors.Wrap(err, "[ ExtractNodeRef ]  Can't extract reference")
+	}
+
+	return nRef.Ref, nil
+}
+
+func ExtractReferenceResponse(data []byte) (*core.RecordRef, error) {
+	var ref *core.RecordRef
+	var ferr *foundation.Error
+	_, err := core.UnMarshalResponse(data, []interface{}{&ref, &ferr})
+	if err != nil {
+		return nil, errors.Wrap(err, "[ ExtractReferenceResponse ] Can't unmarshal response ")
+	}
+	if ferr != nil {
+		return nil, errors.Wrap(ferr, "[ ExtractReferenceResponse ] Has error in response")
+	}
+	return ref, nil
+}
