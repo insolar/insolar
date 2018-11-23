e018a35341c95396d6a43bca0bca38f6e9a98787
Maria Zubareva <maria.zubareva@insolar.io>
2018-11-23 17:39:56 +0300

    NET-136: update api tests

--- api/main_test.go
+++ api/main_test.go
@@ -17,9 +17,7 @@
 package api
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -35,7 +33,7 @@ import (
 )
 
 const HOST = "http://localhost:19191"
-const TestUrl = HOST + "/api/v1?query_type=LOL"
+const TestUrl = HOST + "/api/v1/call"
 
 func TestMain(m *testing.M) {
 	ctx, _ := inslogger.WithTraceField(context.Background(), "APItests")
@@ -53,22 +51,12 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func TestWrongQueryParam(t *testing.T) {
-	postParams := map[string]string{"query_type": "TEST", "reference": "test"}
-	jsonValue, _ := json.Marshal(postParams)
-	postResp, err := http.Post(TestUrl, "application/json", bytes.NewBuffer(jsonValue))
-	require.NoError(t, err)
-	body, err := ioutil.ReadAll(postResp.Body)
-	require.NoError(t, err)
-	require.Contains(t, string(body[:]), `"message": "Wrong query parameter 'query_type' = 'TEST'"`)
-}
-
 func TestBadRequest(t *testing.T) {
 	resp, err := http.Get(TestUrl)
 	require.NoError(t, err)
 	body, err := ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
-	require.Contains(t, string(body[:]), `"message": "Bad request"`)
+	require.Contains(t, string(body[:]), `"[ UnmarshalRequest ] Empty body"`)
 }
 
 func TestSerialization(t *testing.T) {
@@ -106,9 +94,13 @@ func TestNewApiRunnerNoRequiredParams(t *testing.T) {
 
 	cfg.Address = "address:100"
 	_, err = NewRunner(&cfg)
-	require.EqualError(t, err, "[ NewAPIRunner ] Location must exist")
+	require.EqualError(t, err, "[ NewAPIRunner ] Call must exist")
 
-	cfg.Location = "test"
+	cfg.Call = "test"
+	_, err = NewRunner(&cfg)
+	require.EqualError(t, err, "[ NewAPIRunner ] RPC must exist")
+
+	cfg.RPC = "test"
 	_, err = NewRunner(&cfg)
 	require.NoError(t, err)
 }
