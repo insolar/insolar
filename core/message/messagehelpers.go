c195250f4016e47c27e1f749ebccb42d16452be7
Sergey Morozov <sergey.morozov@insolar.io>
2018-11-23 18:52:35 +0300

    INS-785: check msg sender roles

--- core/message/messagehelpers.go
+++ core/message/messagehelpers.go
@@ -98,7 +98,7 @@ func ExtractRole(msg core.Message) core.JetRole {
 }
 
 // ExtractAllowedSenderObjectAndRole extracts information from message
-// verify senderrequired to 's "caller" for sender
+// verify sender required to 's "caller" for sender
 // verification purpose. If nil then check of sender's role is not
 // provided by the message bus
 func ExtractAllowedSenderObjectAndRole(msg core.Message) (*core.RecordRef, core.JetRole) {
@@ -120,32 +120,33 @@ func ExtractAllowedSenderObjectAndRole(msg core.Message) (*core.RecordRef, core.
 	case *ExecutorResults:
 		return nil, 0
 	case *GetChildren:
-		return nil, 0
+		return &t.Parent, core.RoleVirtualExecutor
 	case *GetCode:
-		return nil, 0
+		return &t.Code, core.RoleVirtualExecutor
 	case *GetDelegate:
-		return nil, 0
+		return &t.Head, core.RoleVirtualExecutor
 	case *GetObject:
-		return nil, 0
+		return &t.Head, core.RoleVirtualExecutor
 	case *JetDrop:
+		// This check is not needed, because JetDrop sender is explicitly checked in handler.
 		return nil, 0
 	case *RegisterChild:
-		return nil, 0
+		return &t.Child, core.RoleVirtualExecutor
 	case *SetBlob:
-		return nil, 0
+		return &t.TargetRef, core.RoleVirtualExecutor
 	case *SetRecord:
-		return nil, 0
+		return &t.TargetRef, core.RoleVirtualExecutor
 	case *UpdateObject:
-		return nil, 0
+		return &t.Object, core.RoleVirtualExecutor
 	case *ValidateCaseBind:
 		return &t.RecordRef, core.RoleVirtualExecutor
 	case *ValidateRecord:
-		return nil, 0
+		return &t.Object, core.RoleVirtualExecutor
 	case *ValidationResults:
 		return &t.RecordRef, core.RoleVirtualValidator
 	case *Parcel:
 		return ExtractAllowedSenderObjectAndRole(t.Msg)
 	default:
-		panic(fmt.Sprintf("unknow message type - %v", t))
+		panic(fmt.Sprintf("unknown message type - %v", t))
 	}
 }
