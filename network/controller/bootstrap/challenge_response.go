e606e117e1abcd43d0ee2b7e50ac210b5087640b
Andrey Triumfov <triumfov@yahoo.com>
2018-11-26 20:17:20 +0300

    NET-85: challenge response controller skeleton

---
+++ network/controller/bootstrap/challenge_response.go
@@ -0,0 +1,74 @@
+/*
+ *    Copyright 2018 Insolar
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package bootstrap
+
+import (
+	"github.com/insolar/insolar/core"
+	"github.com/pkg/errors"
+)
+
+type ChallengeResponseController struct {
+}
+
+type Nonce []byte
+type SignedNonce []byte
+type SessionID uint64
+
+// Node                           Discovery Node
+//  | ------ ChallengeRequest -----> |
+//  | <-- SignedChallengeResponse -- |
+//  | --- SignedChallengeRequest --> |
+//  | <----- ChallengeResponse ----- |
+// ------------------------------------
+
+type SessionHeader struct {
+	Success   bool
+	SessionID SessionID
+	Error     string
+}
+
+type ChallengeRequest struct {
+	Certificate core.Certificate
+	Nonce       Nonce
+}
+
+type SignedChallengeResponse struct {
+	SessionHeader SessionHeader
+
+	SignedNonce       SignedNonce
+	XorDiscoveryNonce Nonce
+	DiscoveryNonce    Nonce
+}
+
+type SignedChallengeRequest struct {
+	SessionHeader SessionHeader
+
+	SignedDiscoveryNonce SignedNonce
+	XorNonce             Nonce
+}
+
+type ChallengeResponse struct {
+	SessionHeader SessionHeader
+
+	CurrentPulse core.Pulse
+	State        core.NetworkState
+}
+
+func (crc *ChallengeResponseController) SendRequest(request *ChallengeRequest) (*ChallengeResponse, error) {
+	// TODO: double challenge response
+	return nil, errors.New("not implemented")
+}
