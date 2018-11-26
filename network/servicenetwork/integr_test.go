520f4e17735db8012794e9b9caeadd7bc65a3583
Andrey Bronin <andrey.bronin@insolar.io>
2018-11-26 17:19:44 +0300

    NET-165: servicenetwork basic integration test

---
+++ network/servicenetwork/integr_test.go
@@ -0,0 +1,79 @@
+/*
+ *    Copyright 2018 Insolar
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package servicenetwork
+
+import (
+	"context"
+	"testing"
+
+	"github.com/insolar/insolar/certificate"
+	"github.com/insolar/insolar/component"
+	"github.com/insolar/insolar/configuration"
+	"github.com/insolar/insolar/cryptography"
+	"github.com/insolar/insolar/network/nodenetwork"
+	"github.com/insolar/insolar/platformpolicy"
+	"github.com/insolar/insolar/testutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewServiceNetwork2(t *testing.T) {
+
+	address := "127.0.0.1:3007"
+	consensusAddr := "127.0.0.1:3008"
+
+	origin := nodenetwork.NewNode(testutils.RandomRef(), nil, nil, 0, address, "")
+	keeper := nodenetwork.NewNodeKeeper(origin)
+
+	key, _ := platformpolicy.NewKeyProcessor().GeneratePrivateKey()
+	require.NotNil(t, key)
+	cs := cryptography.NewKeyBoundCryptographyService(key)
+	kp := platformpolicy.NewKeyProcessor()
+	pk, _ := cs.GetPublicKey()
+	cert, err := certificate.NewCertificatesWithKeys(pk, kp)
+	require.NoError(t, err)
+
+	cfg := configuration.NewConfiguration()
+	cfg.Node.Node.ID = origin.ID().String()
+	cfg.Host.Transport.Address = address
+	cfg.Host.ConsensusTransport.Address = consensusAddr
+
+	scheme := platformpolicy.NewPlatformCryptographyScheme()
+	serviceNetwork, err := NewServiceNetwork(cfg, scheme)
+	assert.NoError(t, err)
+
+	pulseManagerMock := testutils.NewPulseManagerMock(t)
+	netCoordinator := testutils.NewNetworkCoordinatorMock(t)
+	amMock := testutils.NewArtifactManagerMock(t)
+
+	cm := component.Manager{}
+	cm.Register(keeper, cert, pulseManagerMock, cs, netCoordinator, amMock)
+	cm.Inject(serviceNetwork)
+
+	serviceNetwork.NodeKeeper = keeper
+
+	ctx := context.Background()
+
+	err = cm.Init(ctx)
+	assert.NoError(t, err)
+
+	err = cm.Start(ctx)
+	assert.NoError(t, err)
+
+	err = cm.Stop(ctx)
+	assert.NoError(t, err)
+}
