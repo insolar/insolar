f462c789a46d24cf49f6e93f5e0ca4f17364e739
Andrey Romancev <andrey@romancev.com>
2018-11-27 15:09:56 +0300

    INS-714: Add children redirection docs.

--- ledger/artifactmanager/descriptors.go
+++ ledger/artifactmanager/descriptors.go
@@ -135,9 +135,25 @@ func (d *ObjectDescriptor) Parent() *core.RecordRef {
 	return &d.parent
 }
 
-// ChildIterator is used to iterate over objects children.
+// ChildIterator is used to iterate over objects children. During iteration children refs will be fetched from remote
+// source (parent object).
 //
-// During iteration children refs will be fetched from remote source (parent object).
+// Data can be fetched only from Active Executor (AE), although children references can be stored on other nodes.
+// To cope with this, we have a token system. Every time AE doesn't have data and asked for it, it will issue a token
+// that will allow requester to fetch data from a different node. This node will return all children references it has,
+// after which the requester has to go to AE again to fetch a new token. It will then be redirected to another node.
+// E.i. children fetching happens like this:
+// [R = requester, AE = active executor, LE = any light executor that has data, H = heavy executor]
+// 1. R (get children 0 ... ) -> AE
+// 2. AE (children 0 ... 3) -> R
+// 3. R (get children 4 ...) -> AE
+// 4. AE (redirect to LE1) -> R
+// 5. R (get children 4 ...) -> LE
+// 6. LE (children 4 ... 5) -> R
+// 7. R (get children 6 ...) -> AE
+// 8. AE (redirect to H) -> R
+// 9. R (get children 6 ...) -> H
+// 10. H (children 6 ... 15 EOF) -> R
 type ChildIterator struct {
 	ctx        context.Context
 	messageBus core.MessageBus
