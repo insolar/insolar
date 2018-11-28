5e9128abaf7114da160b3b4c60458ec027212a88
Andrey Romancev <andrey@romancev.com>
2018-11-28 16:04:28 +0300

    INS-826: Add update object index fetching test.

--- ledger/artifactmanager/handler_test.go
+++ ledger/artifactmanager/handler_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/insolar/insolar/core/reply"
 	"github.com/insolar/insolar/instrumentation/inslogger"
 	"github.com/insolar/insolar/ledger/index"
+	"github.com/insolar/insolar/ledger/record"
 	"github.com/insolar/insolar/ledger/storage"
 	"github.com/insolar/insolar/ledger/storage/storagetest"
 	"github.com/insolar/insolar/testutils"
@@ -276,11 +277,71 @@ func TestMessageHandler_HandleGetDelegate_FetchesIndexFromHeavy(t *testing.T) {
 		Msg: &msg,
 	})
 	require.NoError(t, err)
-	relegateRep, ok := rep.(*reply.Delegate)
+	delegateRep, ok := rep.(*reply.Delegate)
 	require.True(t, ok)
-	assert.Equal(t, delegate, relegateRep.Head)
+	assert.Equal(t, delegate, delegateRep.Head)
 
 	idx, err := db.GetObjectIndex(ctx, msg.Head.Record(), false)
 	require.NoError(t, err)
 	assert.Equal(t, objIndex.Delegates, idx.Delegates)
+}
+
+func TestMessageHandler_HandleUpdateObject_FetchesIndexFromHeavy(t *testing.T) {
+	t.Parallel()
+	ctx := inslogger.TestContext(t)
+	mc := minimock.NewController(t)
+	db, cleaner := storagetest.TmpDB(ctx, t)
+	defer cleaner()
+	defer mc.Finish()
+
+	mb := testutils.NewMessageBusMock(mc)
+	jc := testutils.NewJetCoordinatorMock(mc)
+	h := NewMessageHandler(db, storage.NewRecentStorage(0), &configuration.ArtifactManager{
+		LightChainLimit: 3,
+	})
+
+	objIndex := index.ObjectLifeline{LatestState: genRandomID(0), State: record.StateActivation}
+	amendRecord := record.ObjectAmendRecord{
+		PrevState: *objIndex.LatestState,
+	}
+	amendHash := db.PlatformCryptographyScheme.ReferenceHasher()
+	_, err := amendRecord.WriteHashData(amendHash)
+	require.NoError(t, err)
+	amendID := core.NewRecordID(0, amendHash.Sum(nil))
+
+	msg := message.UpdateObject{
+		Record: record.SerializeRecord(&amendRecord),
+		Object: *genRandomRef(0),
+	}
+
+	mb.SendFunc = func(c context.Context, gm core.Message, o *core.MessageSendOptions) (r core.Reply, r1 error) {
+		if m, ok := gm.(*message.GetObjectIndex); ok {
+			assert.Equal(t, msg.Object, m.Object)
+			buf, err := index.EncodeObjectLifeline(&objIndex)
+			require.NoError(t, err)
+			return &reply.ObjectIndex{Index: buf}, nil
+		}
+
+		panic("unexpected call")
+	}
+
+	h.JetCoordinator = jc
+	h.Bus = mb
+	heavyRef := genRandomRef(0)
+	jc.QueryRoleMock.Expect(
+		ctx, core.RoleHeavyExecutor, &msg.Object, 0,
+	).Return(
+		[]core.RecordRef{*heavyRef}, nil,
+	)
+	rep, err := h.handleUpdateObject(ctx, 0, &message.Parcel{
+		Msg: &msg,
+	})
+	require.NoError(t, err)
+	objRep, ok := rep.(*reply.Object)
+	require.True(t, ok)
+	assert.Equal(t, *amendID, objRep.State)
+
+	idx, err := db.GetObjectIndex(ctx, msg.Object.Record(), false)
+	require.NoError(t, err)
+	assert.Equal(t, amendID, idx.LatestState)
 }
