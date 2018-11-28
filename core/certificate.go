60abda071f3a7cf378f8b4effd2331300576e147
Andrey Triumfov <triumfov@yahoo.com>
2018-11-28 12:51:22 +0300

    NET-85: changes in certificate needed for bootstrap

--- core/certificate.go
+++ core/certificate.go
@@ -20,20 +20,24 @@ import (
 	"crypto"
 )
 
+type NetworkNode interface {
+	GetRef() *RecordRef
+	GetPublicKey() crypto.PublicKey
+}
+
 // Certificate interface provides methods to manage keys
 type Certificate interface {
+	NetworkNode
+
 	GetRole() NodeRole
 	GetRootDomainReference() *RecordRef
 	SetRootDomainReference(ref *RecordRef)
 	GetBootstrapNodes() []BootstrapNode
-	GetPublicKey() crypto.PublicKey
 	IsDiscoveryNode() bool
 }
 
-// BootstrapNode holds info about bootstrap nodes
-type BootstrapNode struct {
-	PublicKey   string `json:"public_key"`
-	Host        string `json:"host"`
-	NetworkSign []byte `json:"network_sign"`
-	NodeSign    []byte `json:"node_sign"`
+type BootstrapNode interface {
+	NetworkNode
+
+	GetHost() string
 }
