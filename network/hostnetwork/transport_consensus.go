da8556ffe4fe97bcccf647e82914e513b1fad160
Andrey Bronin <andrey.bronin@insolar.io>
2018-11-30 13:44:07 +0300

    NET-8: workaround for increment port in consensus transport

--- network/hostnetwork/transport_consensus.go
+++ network/hostnetwork/transport_consensus.go
@@ -18,6 +18,8 @@ package hostnetwork
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/insolar/insolar/configuration"
 	"github.com/insolar/insolar/core"
@@ -73,6 +75,19 @@ func (tc *transportConsensus) processMessage(msg *packet.Packet) {
 	handler((*packetWrapper)(msg))
 }
 
+func incrementPort(address string) (string, error) {
+	parts := strings.Split(address, ":")
+	if len(parts) != 2 {
+		return address, errors.New("failed to get port from address.")
+	}
+	port, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return address, err
+	}
+
+	return fmt.Sprintf("%s:%d", parts[0], port+1), nil
+}
+
 func NewConsensusNetwork(address, nodeID string, shortID core.ShortNodeID,
 	resolver network.RoutingTable) (network.ConsensusNetwork, error) {
 
@@ -80,6 +95,13 @@ func NewConsensusNetwork(address, nodeID string, shortID core.ShortNodeID,
 	conf.Address = address
 	conf.Protocol = "PURE_UDP"
 	conf.BehindNAT = false
+
+	// workaround for Consensus transport, port+=1 of default transport
+	var err error
+	conf.Address, err = incrementPort(conf.Address)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to increment port.")
+	}
 
 	tp, err := transport.NewTransport(conf, relay.NewProxy())
 	if err != nil {
@@ -94,6 +116,7 @@ func NewConsensusNetwork(address, nodeID string, shortID core.ShortNodeID,
 	}
 	origin.ShortID = shortID
 	result := &transportConsensus{handlers: make(map[types.PacketType]network.ConsensusRequestHandler)}
+
 	result.transport = tp
 	result.resolver = resolver
 	result.origin = origin
