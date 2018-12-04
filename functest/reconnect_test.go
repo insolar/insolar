7dc97d7eb3787b495724a7c6d546a29041c8eb5c
Ivan Shibitov <ivan.shibitov@insolar.io>
2018-12-04 15:41:53 +0300

    NET-90: fix reconnect

--- functest/reconnect_test.go
+++ functest/reconnect_test.go
@@ -5,8 +5,28 @@ package functest
 import (
 	"testing"
 
+	"github.com/insolar/insolar/logicrunner/goplugin/goplugintestutils"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
+
+var insgorundCleaner func()
+
+func startInsgorund() (err error) {
+	// It starts on ports of "virtual" node
+	insgorundCleaner, err = goplugintestutils.StartInsgorund(insgorundPath, "tcp", "127.0.0.1:38181", "tcp", "127.0.0.1:38182")
+	if err != nil {
+		return errors.Wrap(err, "[ startInsgorund ] could't wait for insolard to start completely: ")
+	}
+	return nil
+}
+
+func stopInsgorund() error {
+	if insgorundCleaner != nil {
+		insgorundCleaner()
+	}
+	return nil
+}
 
 func TestInsgorundReload(t *testing.T) {
 	_, err := signedRequest(&root, "DumpAllUsers")
