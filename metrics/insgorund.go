22ac6fa8f1adcf58f7ebf120d1122ce2ae171d82
anton.alekseev <anton.alexeev@insolar.io>
2018-12-04 20:19:37 +0300

    INS-54 new metric

--- metrics/insgorund.go
+++ metrics/insgorund.go
@@ -13,10 +13,18 @@ var InsgorundCallsTotal = prometheus.NewCounter(prometheus.CounterOpts{
 	Namespace: insgorundNamespace,
 })
 
+var InsgorundContractExecutionTime = prometheus.NewSummary(prometheus.SummaryOpts{
+	Name:       "contract_execution_time",
+	Help:       "Time spent on execution contract",
+	Namespace:  insgorundNamespace,
+	Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.05, 0.95: 0.05, 0.99: 0.05},
+})
+
 func GetInsgorundRegistry() *prometheus.Registry {
 	registry := prometheus.NewRegistry()
 
 	registry.MustRegister(InsgorundCallsTotal)
+	registry.MustRegister(InsgorundContractExecutionTime)
 	// default system collectors
 	registry.MustRegister(prometheus.NewProcessCollector(os.Getpid(), insgorundNamespace))
 	registry.MustRegister(prometheus.NewGoCollector())
