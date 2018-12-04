3943c62e545f3726c5fa43d0588450f5178f067d
anton.alekseev <anton.alexeev@insolar.io>
2018-12-04 20:19:24 +0300

    INS-54 new metric

--- metrics/goplugin.go
+++ metrics/goplugin.go
@@ -2,8 +2,8 @@ package metrics
 
 import "github.com/prometheus/client_golang/prometheus"
 
-var InsgorundContractExecutionTime = prometheus.NewSummary(prometheus.SummaryOpts{
-	Name:       "contract_execution_time",
+var GopluginContractExecutionTime = prometheus.NewSummary(prometheus.SummaryOpts{
+	Name:       "goplugin_contract_execution_time",
 	Help:       "Time spent on execution contract, measured in goplugin",
 	Namespace:  insgorundNamespace,
 	Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.05, 0.95: 0.05, 0.99: 0.05},
