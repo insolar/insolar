44e53d423404a69b6cbbfae46a85df22ef6734cc
Denis Linnik <denis.linnik@ins.world>
2018-12-10 11:29:30 +0300

    INS-875: comments

--- networkcoordinator/interface.go
+++ networkcoordinator/interface.go
@@ -33,6 +33,6 @@ type Coordinator interface {
 	// SetPulse uses PulseManager component for saving pulse info
 	SetPulse(ctx context.Context, pulse core.Pulse) error
 
-	// signCert is used by MsgBus handler for signing certificate
+	// signCertHandler is used by MsgBus handler for signing certificate
 	signCertHandler(ctx context.Context, p core.Parcel) (core.Reply, error)
 }
