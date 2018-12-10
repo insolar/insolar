7f6ab2c161211ae92a5845963ee1cfe0f6f0d194
YakovZverev <yakov.zverev@insolar.io>
2018-12-10 17:06:08 +0300

    NET-11: fixed nodeKeeper wrapper

--- network/servicenetwork/integr_test.go
+++ network/servicenetwork/integr_test.go
@@ -29,7 +29,6 @@ import (
 	"github.com/insolar/insolar/certificate"
 	"github.com/insolar/insolar/component"
 	"github.com/insolar/insolar/configuration"
-	"github.com/insolar/insolar/consensus/packets"
 	"github.com/insolar/insolar/consensus/phases"
 	"github.com/insolar/insolar/core"
 	"github.com/insolar/insolar/cryptography"
@@ -43,90 +42,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
-
-type nodeKeeperWrapperPartial struct {
-	realKeeper network.NodeKeeper
-}
-
-func (nkw *nodeKeeperWrapperPartial) SetCloudHash(hash []byte) {
-	nkw.realKeeper.SetCloudHash(hash)
-}
-func (nkw *nodeKeeperWrapperPartial) AddActiveNodes(nodes []core.Node) {
-	nkw.realKeeper.AddActiveNodes(nodes)
-}
-func (nkw *nodeKeeperWrapperPartial) GetActiveNodeByShortID(shortID core.ShortNodeID) core.Node {
-	return nkw.realKeeper.GetActiveNodeByShortID(shortID)
-}
-
-func (nkw *nodeKeeperWrapperPartial) SetState(state network.NodeKeeperState) {
-	nkw.realKeeper.SetState(state)
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetState() network.NodeKeeperState {
-	return nkw.realKeeper.GetState()
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetOriginClaim() (*packets.NodeJoinClaim, error) {
-	return nkw.realKeeper.GetOriginClaim()
-}
-
-func (nkw *nodeKeeperWrapperPartial) NodesJoinedDuringPreviousPulse() bool {
-	return nkw.realKeeper.NodesJoinedDuringPreviousPulse()
-}
-
-func (nkw *nodeKeeperWrapperPartial) AddPendingClaim(claim packets.ReferendumClaim) bool {
-	return nkw.realKeeper.AddPendingClaim(claim)
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetClaimQueue() network.ClaimQueue {
-	return nkw.realKeeper.GetClaimQueue()
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetUnsyncList() network.UnsyncList {
-	return nkw.realKeeper.GetUnsyncList()
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetSparseUnsyncList(length int) network.UnsyncList {
-	return nkw.realKeeper.GetSparseUnsyncList(length)
-}
-
-func (nkw *nodeKeeperWrapperPartial) Sync(list network.UnsyncList) {
-	nkw.realKeeper.Sync(list)
-}
-
-func (nkw *nodeKeeperWrapperPartial) MoveSyncToActive() {
-	nkw.realKeeper.MoveSyncToActive()
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetActiveNodes() []core.Node {
-	tmp := nkw.realKeeper.GetActiveNodes()
-	tmp = tmp[:len(tmp)-2]
-	return tmp
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetOrigin() core.Node {
-	return nkw.realKeeper.GetOrigin()
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetActiveNode(ref core.RecordRef) core.Node {
-	return nkw.realKeeper.GetActiveNode(ref)
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetActiveNodesByRole(role core.DynamicRole) []core.RecordRef {
-	return nkw.realKeeper.GetActiveNodesByRole(role)
-}
-
-func (nkw *nodeKeeperWrapperPartial) GetCloudHash() []byte {
-	return nkw.realKeeper.GetCloudHash()
-}
-
-func (nkw *nodeKeeperWrapperPartial) IsBootstrapped() bool {
-	return nkw.realKeeper.IsBootstrapped()
-}
-
-func (nkw *nodeKeeperWrapperPartial) SetIsBootstrapped(isBootstrap bool) {
-	nkw.realKeeper.SetIsBootstrapped(isBootstrap)
-}
 
 type testSuite struct {
 	suite.Suite
@@ -300,7 +215,7 @@ func (s *testSuite) createNetworkNode(t *testing.T, timeOut PhaseTimeOut) networ
 		phaseManager = &FullTimeoutPhaseManager{}
 	case Partitial:
 		phaseManager = &PartialTimeoutPhaseManager{}
-		keeper = &nodeKeeperWrapperPartial{realKeeper}
+		keeper = &nodeKeeperWrapper{realKeeper}
 	}
 
 	cm := &component.Manager{}
@@ -312,7 +227,7 @@ func (s *testSuite) createNetworkNode(t *testing.T, timeOut PhaseTimeOut) networ
 }
 
 func (s *testSuite) TestNodeConnect() {
-	//s.T().Skip("will be available after phase result fix !")
+	// s.T().Skip("will be available after phase result fix !")
 	phasesResult := make(chan error)
 	bootstrapNode1 := s.createNetworkNode(s.T(), Disable)
 	s.bootstrapNodes = append(s.bootstrapNodes, bootstrapNode1)
