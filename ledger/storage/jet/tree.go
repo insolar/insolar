a1a70bb54c3ae4032ffe2c995b5733951520608a
Andrey Romancev <andrey@romancev.com>
2018-12-11 18:40:46 +0300

    INS-910: Make tree find result a reference.

--- ledger/storage/jet/tree.go
+++ ledger/storage/jet/tree.go
@@ -75,9 +75,9 @@ func NewTree() *Tree {
 }
 
 // Find returns jet for provided reference.
-func (t *Tree) Find(val []byte) core.RecordID {
+func (t *Tree) Find(val []byte) *core.RecordID {
 	depth := t.Head.Find(val, 0)
-	return *core.NewJetID(uint8(depth), resetBits(val, depth))
+	return core.NewJetID(uint8(depth), resetBits(val, depth))
 }
 
 // Update add missing tree branches for provided prefix.
