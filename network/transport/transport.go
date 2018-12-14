2344bfc6adc9bd286d1e932bc1517a30d2c90737
Maria Zubareva <maria.zubareva@insolar.io>
2018-12-14 19:18:26 +0300

    INS-969: delete some transport

--- network/transport/transport.go
+++ network/transport/transport.go
@@ -72,10 +72,6 @@ func NewTransport(cfg configuration.Transport, proxy relay.Proxy) (Transport, er
 		// TODO: little hack: It's better to change interface for NewConnection
 		conn.Close()
 		return newTCPTransport(conn.LocalAddr().String(), proxy, publicAddress)
-	case "UTP":
-		return newUTPTransport(conn, proxy, publicAddress)
-	case "KCP":
-		return newKCPTransport(conn, proxy, publicAddress)
 	case "PURE_UDP":
 		return newUDPTransport(conn, proxy, publicAddress)
 	case "QUIC":
