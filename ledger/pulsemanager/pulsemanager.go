9a7c6281edc739cf9bd60d71c105d39ce1ad8132
Maria Zubareva <maria.zubareva@insolar.io>
2018-12-14 18:07:59 +0300

    INS-931: releases in pulsemanager

--- ledger/pulsemanager/pulsemanager.go
+++ ledger/pulsemanager/pulsemanager.go
@@ -307,8 +307,7 @@ func (m *PulseManager) Set(ctx context.Context, pulse core.Pulse, persist bool) 
 	}
 
 	// swap active nodes
-	// TODO: fix network consensus and uncomment this (after NETD18-74)
-	// m.ActiveListSwapper.MoveSyncToActive()
+	m.ActiveListSwapper.MoveSyncToActive()
 	if persist {
 		if err := m.db.AddPulse(ctx, pulse); err != nil {
 			m.GIL.Release(ctx)
