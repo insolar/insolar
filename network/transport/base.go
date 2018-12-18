ee8ada4cce9adf6b1d4d605d47031c4c0ef38b4d
Maria Zubareva <maria.zubareva@insolar.io>
2018-12-18 21:35:16 +0300

    INS-969: add locks

--- network/transport/base.go
+++ network/transport/base.go
@@ -119,10 +119,16 @@ func (t *baseTransport) Packets() <-chan *packet.Packet {
 
 // Stopped checks if networking is stopped already.
 func (t *baseTransport) Stopped() <-chan bool {
+	t.mutex.RLock()
+	defer t.mutex.RUnlock()
+
 	return t.disconnectStarted
 }
 
 func (t *baseTransport) prepareListen() {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+
 	t.disconnectStarted = make(chan bool, 1)
 }
 
