15c9e2858f6ae701f0ba34fb9f481ccdd11f4fc7
ooki2day <ooki2day@gmail.com>
2018-12-21 03:01:43 +0300

    NET-83: added fakepulsar types

---
+++ network/fakepulsar/fakepulsardata.go
@@ -0,0 +1,37 @@
+/*
+ *    Copyright 2018 Insolar
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package fakepulsar
+
+import (
+	"github.com/insolar/insolar/network/transport/packet/types"
+)
+
+type FakePulsarRequest struct {
+}
+
+func (fpr *FakePulsarRequest) Type() types.PacketType {
+	return types.FakePulsarRequest
+}
+
+type FakePulsarResponse struct {
+	pulseNum       int64
+	firstPulseTime int64
+}
+
+func (fpr *FakePulsarResponse) Type() types.PacketType {
+	return types.FakePulsarResponse
+}
