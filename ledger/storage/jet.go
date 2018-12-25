887555a1a1e170597ecda612cd9ff00f7fecf9a4
egorikas <egorgrishechko@gmail.com>
2018-12-25 15:54:07 +0300

    revert useless code about creating a new tree

--- ledger/storage/jet.go
+++ ledger/storage/jet.go
@@ -175,12 +175,7 @@ func (db *DB) getJetTree(ctx context.Context, pulse core.PulseNumber) (*jet.Tree
 	k := prefixkey(scopeIDSystem, []byte{sysJetTree}, pulse.Bytes())
 	buff, err := db.get(ctx, k)
 	if err == ErrNotFound {
-		tree := jet.NewTree()
-		err := db.set(ctx, k, tree.Bytes())
-		if err != nil {
-			return nil, err
-		}
-		return tree, nil
+		return jet.NewTree(), nil
 	}
 	if err != nil {
 		return nil, err
