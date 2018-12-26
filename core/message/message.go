ac764d9c8fc47fe44fed58f40c9754aa4007bbd7
Maria Zubareva <maria.zubareva@insolar.io>
2018-12-26 11:11:56 +0300

    INS-991: add HotData to gob

--- core/message/message.go
+++ core/message/message.go
@@ -213,6 +213,7 @@ func init() {
 	gob.Register(&GetPendingRequests{})
 	gob.Register(&GetJet{})
 	gob.Register(&AbandonedRequestsNotification{})
+	gob.Register(&HotData{})
 
 	// heavy
 	gob.Register(&HeavyStartStop{})
