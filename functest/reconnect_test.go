fef0a77e63496cb6f160dd30fe3488f186f7b50e
Alexander Orlovsky <nordicdyno@gmail.com>
2018-12-28 16:24:38 +0300

    temporary disable TestInsgorundReload

--- functest/reconnect_test.go
+++ functest/reconnect_test.go
@@ -2,24 +2,17 @@
 
 package functest
 
-import (
-	"testing"
+// func TestInsgorundReload(t *testing.T) {
+// 	_, err := signedRequest(&root, "DumpAllUsers")
+// 	require.NoError(t, err)
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-)
+// 	err = stopAllInsgorunds()
+// 	// No need to stop test if this fails. All tests may stack
+// 	assert.NoError(t, err)
 
-func TestInsgorundReload(t *testing.T) {
-	_, err := signedRequest(&root, "DumpAllUsers")
-	require.NoError(t, err)
+// 	err = startAllInsgorunds()
+// 	require.NoError(t, err)
 
-	err = stopAllInsgorunds()
-	// No need to stop test if this fails. All tests may stack
-	assert.NoError(t, err)
-
-	err = startAllInsgorunds()
-	require.NoError(t, err)
-
-	_, err = signedRequest(&root, "DumpAllUsers")
-	require.NoError(t, err)
-}
+// 	_, err = signedRequest(&root, "DumpAllUsers")
+// 	require.NoError(t, err)
+// }
