18f1b5e9216e97b6bb7c1a20fd0784d3abc04d48
Andrey Triumfov <triumfov@yahoo.com>
2019-01-11 18:32:08 +0300

    INS-1114: + implementation of keys generation and node registering

--- cmd/certgen/main.go
+++ cmd/certgen/main.go
@@ -17,26 +17,120 @@
 package main
 
 import (
+	"context"
+	"crypto"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/insolar/insolar/api/requester"
+	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/instrumentation/inslogger"
+	"github.com/insolar/insolar/platformpolicy"
+	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 )
 
-const defaultURL = "http://localhost:19191/"
+const defaultURL = "http://localhost:19191/api"
+
+var ks = platformpolicy.NewKeyProcessor()
 
 var (
 	role        string
-	apiHost     string
+	api         string
 	keysFileOut string
 	certFileOut string
+	rootConfig  string
+	verbose     bool
 )
 
 func parseInputParams() {
 	pflag.StringVarP(&role, "role", "r", "virtual", "The role of the new node")
-	pflag.StringVarP(&apiHost, "api_host", "h", defaultURL, "HTTP base host that serves insolar API requests")
-	pflag.StringVarP(&keysFileOut, "keys_file", "k", "keys.json", "The OUT file for public/private keys of the node")
-	pflag.StringVarP(&certFileOut, "cert_file", "c", "cert.json", "The OUT file the node certificate")
+	pflag.StringVarP(&api, "url", "h", defaultURL, "Insolar API URL")
+	pflag.BoolVarP(&verbose, "verbose", "v", false, "Be verbose (default false)")
+	pflag.StringVarP(&keysFileOut, "keys-file", "k", "keys.json", "The OUT file for public/private keys of the node")
+	pflag.StringVarP(&certFileOut, "cert-file", "c", "cert.json", "The OUT file the node certificate")
+	pflag.StringVar(&rootConfig, "root-conf", "", "Config that contains public/private keys of root member")
 	pflag.Parse()
+}
+
+func generateKeys() (crypto.PublicKey, crypto.PrivateKey) {
+	privKey, err := ks.GeneratePrivateKey()
+	checkError("Failed to generate private key:", err)
+	pubKey := ks.ExtractPublicKey(privKey)
+	return pubKey, privKey
+}
+
+type RegisterResult struct {
+	Result  string `json:"result"`
+	TraceID string `json:"traceID"`
+}
+
+func registerNode(key crypto.PublicKey, staticRole core.StaticRole) core.RecordRef {
+	requester.SetVerbose(verbose)
+	userCfg, err := requester.ReadUserConfigFromFile(rootConfig)
+	checkError("Failed to read root config:", err)
+	info, err := requester.Info(api)
+	checkError("Failed to execute info request to API:", err)
+	userCfg.Caller = info.RootMember
+
+	keySerialized, err := ks.ExportPublicKeyPEM(key)
+	checkError("Failed to export public key:", err)
+	request := requester.RequestConfigJSON{
+		Method: "RegisterNode",
+		Params: []interface{}{keySerialized, staticRole.String()},
+	}
+
+	ctx := inslogger.ContextWithTrace(context.Background(), "insolarUtility")
+	response, err := requester.Send(ctx, api, userCfg, &request)
+	checkError("Failed to execute register node request", err)
+
+	r := RegisterResult{}
+	err = json.Unmarshal(response, &r)
+	checkError("Failed to parse response from register node request", err)
+	ref, err := core.NewRefFromBase58(r.Result)
+	checkError("Failed to construct ref from register node response", err)
+	return *ref
+}
+
+func fetchCertificate(ref core.RecordRef) ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
+
+func writeKeys(crypto.PublicKey, crypto.PrivateKey) error {
+	return errors.New("not implemented")
+}
+
+func writeCertificate(cert []byte) error {
+	return errors.New("not implemented")
+}
+
+func checkError(msg string, err error) {
+	if err != nil {
+		fmt.Println(msg, err)
+		os.Exit(1)
+	}
 }
 
 func main() {
 	parseInputParams()
+	staticRole := core.GetStaticRoleFromString(role)
+	if staticRole == core.StaticRoleUnknown {
+		fmt.Println("Invalid role:", role)
+		os.Exit(1)
+	}
+
+	pub, priv := generateKeys()
+	ref := registerNode(pub, staticRole)
+
+	cert, err := fetchCertificate(ref)
+	checkError("Failed to get certificate of the registered node:", err)
+
+	err = writeKeys(pub, priv)
+	checkError("Failed to write file with public/private keys:", err)
+
+	err = writeCertificate(cert)
+	checkError("Failed to write file with node certificate:", err)
+
+	fmt.Println("Successfully generated files with keys and certificate")
 }
