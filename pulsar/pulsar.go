bb3649046139c1212fba5750865d8f2b53834b6b
egorikas <egorgrishechko@gmail.com>
2019-01-12 12:14:49 +0300

    add stacktrace

--- pulsar/pulsar.go
+++ pulsar/pulsar.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"net"
 	"net/rpc"
+	"runtime/debug"
 	"sync"
 
 	"github.com/insolar/insolar/certificate"
@@ -288,6 +289,7 @@ func (currentPulsar *Pulsar) StartConsensusProcess(ctx context.Context, pulseNum
 	logger := inslogger.FromContext(ctx)
 	logger.Debugf("[StartConsensusProcess] pulse number - %v, host - %v", pulseNumber, currentPulsar.Config.MainListenerAddress)
 	logger.Debugf("[Before StartProcessLock]")
+	debug.PrintStack()
 	currentPulsar.StartProcessLock.Lock()
 	logger.Debugf("[After StartProcessLock]")
 
