d33dff9abbaaee040bb0303ced650f4306bb4c23
Ruslan Zakirov <ruslan.zakirov@gmail.com>
2019-01-16 10:34:04 +0300

    ask for GetJet from all active light material nodes
    I know this is not effective, but at this moment we don't have algorithm
    to do it more reliably, even this method may fail

--- ledger/artifactmanager/middleware.go
+++ ledger/artifactmanager/middleware.go
@@ -38,6 +38,11 @@ type middleware struct {
 	messageBus                         core.MessageBus
 	earlyRequestCircuitBreakerProvider *earlyRequestCircuitBreakerProvider
 	conf                               *configuration.Ledger
+	seqMutex                           sync.Mutex
+	sequencer                          map[core.RecordID]*struct {
+		sync.Mutex
+		done bool
+	}
 }
 
 func newMiddleware(
@@ -52,6 +57,10 @@ func newMiddleware(
 		messageBus:                         messageBus,
 		earlyRequestCircuitBreakerProvider: &earlyRequestCircuitBreakerProvider{breakers: map[core.RecordID]*requestCircuitBreakerProvider{}},
 		conf:                               conf,
+		sequencer:                          map[core.RecordID]*struct {
+			sync.Mutex
+			done bool
+		}{},
 	}
 }
 
@@ -189,49 +198,109 @@ func (m *middleware) fetchJet(
 		return jetID, actual, nil
 	}
 
+	m.seqMutex.Lock()
+	if _, ok := m.sequencer[*jetID]; !ok {
+		m.sequencer[*jetID] = &struct {
+			sync.Mutex
+			done bool
+		}{}
+	}
+	mu := m.sequencer[*jetID]
+	m.seqMutex.Unlock()
+
+	mu.Lock()
+	if mu.done {
+		mu.Unlock()
+		return m.fetchJet(ctx, target, pulse, retries-1)
+	}
+	defer func() {
+		mu.done = true
+		mu.Unlock()
+
+		m.seqMutex.Lock()
+		delete(m.sequencer, *jetID)
+		m.seqMutex.Unlock()
+	}()
+
 	// Couldn't find the actual jet locally. Ask for the jet from the previous executor.
 	prevPulse, err := m.db.GetPreviousPulse(ctx, pulse)
 	if err != nil {
 		return nil, false, err
 	}
 
-	prevExecutor, err := m.jetCoordinator.LightExecutorForJet(ctx, *jetID, prevPulse.Pulse.PulseNumber)
+	nodes, err := m.db.GetActiveNodesByRole(prevPulse.Pulse.PulseNumber, core.StaticRoleLightMaterial)
 	if err != nil {
 		return nil, false, err
 	}
-	if *prevExecutor == m.jetCoordinator.Me() {
+
+	me := m.jetCoordinator.Me()
+	for i, _ := range nodes {
+		if nodes[i].ID() == me {
+			nodes = append(nodes[:i], nodes[i+1:]...)
+			break
+		}
+	}
+
+	num := len(nodes)
+	if num == 0 {
 		inslogger.FromContext(ctx).Error(
-			"This shouldn't happen. If we are prev executor then tree should be actual",
+			"This shouldn't happen. We're solo active light material and have no active jet tree",
 		)
 
 		return nil, false, errors.New("impossible situation")
 	}
 
-	rep, err := m.messageBus.Send(
-		ctx,
-		&message.GetJet{Object: target},
-		&core.MessageSendOptions{Receiver: prevExecutor},
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+
+	res := make([]*reply.Jet, num)
+	for i, node := range nodes {
+		go func(i int, node core.Node) {
+			defer wg.Done()
+
+			nodeID := node.ID()
+			rep, err := m.messageBus.Send(
+				ctx,
+				&message.GetJet{Object: target},
+				&core.MessageSendOptions{Receiver: &nodeID},
+			)
+			if err != nil {
+				inslogger.FromContext(ctx).Error(
+					errors.Wrap(err, "couldn't get jet"),
+				)
+				return
+			}
+
+			r, ok := rep.(*reply.Jet)
+			if !ok {
+				inslogger.FromContext(ctx).Error(
+					errors.Wrap(ErrUnexpectedReply, "couldn't get jet"),
+				)
+				return
+			}
+			res[i] = r
+		}(i, node)
+	}
+	wg.Wait()
+
+	for _, r := range res {
+		if r == nil {
+			continue
+		}
+		if !r.Actual {
+			continue
+		}
+
+		err = m.db.UpdateJetTree(ctx, pulse, r.Actual, r.ID)
+		if err != nil {
+			return nil, false, err
+		}
+
+		return &r.ID, true, nil
+	}
+
+	inslogger.FromContext(ctx).Error(
+		"All active light material nodes have no actual jet tree",
 	)
-	if err != nil {
-		return nil, false, err
-	}
-	r, ok := rep.(*reply.Jet)
-	if !ok {
-		return nil, false, ErrUnexpectedReply
-	}
-
-	// TODO: check if the same executor again or the same jet again. INS-1041
-
-	// Update local tree.
-	err = m.db.UpdateJetTree(ctx, pulse, r.Actual, r.ID)
-	if err != nil {
-		return nil, false, err
-	}
-	if !r.Actual {
-		inslogger.FromContext(ctx).Error(
-			"Previous executor said that jet is not actual",
-		)
-		return nil, false, errors.New("impossible situation")
-	}
-	return &r.ID, r.Actual, nil
+	return nil, false, errors.New("impossible situation")
 }
