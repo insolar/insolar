d39697bc273822b14f95b29128482914a34d09e7
Ilya Markin <amferiuss@gmail.com>
2019-01-19 11:08:02 +0300

    INS-1032: fix from fresh master

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -103,7 +103,6 @@ func GetLedgerComponents(conf configuration.Ledger) []interface{} {
 		artifactmanager.NewMessageHandler(db, &conf),
 		localstorage.NewLocalStorage(db),
 		heavyserver.NewSync(db),
-		heavyserver.NewHeavyJetSync(db),
 		exporter.NewExporter(db, ps),
 	}
 }
