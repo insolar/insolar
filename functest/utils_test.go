6682928aa337dfaaaaa5f1454ee683670c297783
Maria Zubareva <maria.zubareva@insolar.io>
2019-01-22 13:11:39 +0300

    NOISSUE: test

--- functest/utils_test.go
+++ functest/utils_test.go
@@ -235,7 +235,7 @@ func signedRequest(user *user, method string, params ...interface{}) (interface{
 	return resp.Result, errors.New(resp.Error)
 }
 
-// test 4
+// test 5
 func newUserWithKeys() (*user, error) {
 	ks := platformpolicy.NewKeyProcessor()
 
