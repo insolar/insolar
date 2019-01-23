f3b1b5cca3828aeebbba5b741614f8fc72521b25
Ilya Markin <amferiuss@gmail.com>
2019-01-23 11:18:30 +0300

    NOISSUE: refactor main chain file; change println to debugf

--- ledger/artifactmanager/handlerschain.go
+++ ledger/artifactmanager/handlerschain.go
@@ -20,29 +20,32 @@ import (
 	"github.com/insolar/insolar/core"
 )
 
+type Handler func(core.MessageHandler) core.MessageHandler
+
 type HandlersChain struct {
 	handlers []Handler
 }
 
-type Handler func(core.MessageHandler) core.MessageHandler
-
-func New(handlers ...Handler) HandlersChain {
-	return HandlersChain{append(([]Handler)(nil), handlers...)}
+func NewChain(handlers ...Handler) *HandlersChain {
+	return &HandlersChain{handlers: append([]Handler{}, handlers...)}
 }
 
-// func (hc *HandlersChain) Extend(chain HandlersChain) HandlersChain {
-// 	return hc.Append(chain.handlers...)
-// }
+func (hc *HandlersChain) Extend(chain *HandlersChain) *HandlersChain {
+	return hc.Append(chain.handlers...)
+}
 
-func (hc HandlersChain) Append(handlers ...Handler) HandlersChain {
+func (hc *HandlersChain) Append(handlers ...Handler) *HandlersChain {
 	newCons := make([]Handler, 0, len(hc.handlers)+len(handlers))
 	newCons = append(newCons, hc.handlers...)
 	newCons = append(newCons, handlers...)
 
-	return HandlersChain{newCons}
+	return &HandlersChain{handlers: newCons}
 }
 
-func (hc HandlersChain) Then(mh core.MessageHandler) core.MessageHandler {
+func (hc *HandlersChain) Then(mh core.MessageHandler) core.MessageHandler {
+	if mh == nil {
+		panic("MessageHandler in Then function-call can't be nil")
+	}
 
 	for i := range hc.handlers {
 		mh = hc.handlers[len(hc.handlers)-1-i](mh)
