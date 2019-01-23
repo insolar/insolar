7225c756dfe2331277e16408114b7aecfbf5a982
Sergey Morozov <sergey.morozov@insolar.io>
2019-01-23 20:12:04 +0300

    INS-1215: update license

--- ledger/storage/logger.go
+++ ledger/storage/logger.go
@@ -1,7 +1,5 @@
-// +build !debug
-
 /*
- *    Copyright 2018 Insolar
+ *    Copyright 2019 Insolar Technologies
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,6 +13,8 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
+// +build !debug
 
 package storage
 
