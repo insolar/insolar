4c316905a2fd69832df937e19863128ceaa11e9d
YakovZverev <yakov.zverev@insolar.io>
2019-01-23 18:28:22 +0300

    added jaeger to claim queue

--- network/nodenetwork/claimqueue.go
+++ network/nodenetwork/claimqueue.go
@@ -17,9 +17,11 @@
 package nodenetwork
 
 import (
+	"context"
 	"sync"
 
 	"github.com/insolar/insolar/consensus/packets"
+	"github.com/insolar/insolar/instrumentation/instracer"
 )
 
 type claimQueue struct {
@@ -32,7 +34,11 @@ func newClaimQueue() *claimQueue {
 }
 
 func (cq *claimQueue) Pop() packets.ReferendumClaim {
+	ctx, span := instracer.StartSpan(context.Background(), "claimQueue.Pop wait lock")
 	cq.lock.Lock()
+	span.End()
+	ctx, span = instracer.StartSpan(ctx, "claimQueue.Pop lock")
+	defer span.End()
 	defer cq.lock.Unlock()
 
 	if len(cq.data) == 0 {
@@ -44,7 +50,11 @@ func (cq *claimQueue) Pop() packets.ReferendumClaim {
 }
 
 func (cq *claimQueue) Front() packets.ReferendumClaim {
+	ctx, span := instracer.StartSpan(context.Background(), "claimQueue.Front wait lock")
 	cq.lock.RLock()
+	span.End()
+	ctx, span = instracer.StartSpan(ctx, "claimQueue.Front lock")
+	defer span.End()
 	defer cq.lock.RUnlock()
 
 	if len(cq.data) == 0 {
@@ -54,14 +64,22 @@ func (cq *claimQueue) Front() packets.ReferendumClaim {
 }
 
 func (cq *claimQueue) Length() int {
+	ctx, span := instracer.StartSpan(context.Background(), "claimQueue.Length wait lock")
 	cq.lock.RLock()
+	span.End()
+	ctx, span = instracer.StartSpan(ctx, "claimQueue.Length lock")
+	defer span.End()
 	defer cq.lock.RUnlock()
 
 	return len(cq.data)
 }
 
 func (cq *claimQueue) Push(claim packets.ReferendumClaim) {
+	ctx, span := instracer.StartSpan(context.Background(), "claimQueue.Push wait lock")
 	cq.lock.Lock()
+	span.End()
+	ctx, span = instracer.StartSpan(ctx, "claimQueue.Push lock")
+	defer span.End()
 	defer cq.lock.Unlock()
 
 	cq.data = append(cq.data, claim)
