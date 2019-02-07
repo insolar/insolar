9a244acb68e49b885d256368c81387ec8d669040
Sergey Morozov <sergey.morozov@insolar.io>
2019-02-07 16:05:09 +0300

    INS-1370: renamed Name field to Method in CallConstructor

--- core/message/logicrunner.go
+++ core/message/logicrunner.go
@@ -202,7 +202,7 @@ type CallConstructor struct {
 	ParentRef    core.RecordRef
 	SaveAs       SaveAs
 	PrototypeRef core.RecordRef
-	Name         string
+	Method       string
 	Arguments    core.Arguments
 	PulseNum     core.PulseNumber
 }
@@ -223,7 +223,7 @@ func (cc *CallConstructor) ToMap() (map[string]interface{}, error) {
 	msg["ParentRef"] = cc.ParentRef.String()
 	msg["SaveAs"] = cc.SaveAs
 	msg["PrototypeRef"] = cc.PrototypeRef.String()
-	msg["Name"] = cc.Name
+	msg["Method"] = cc.Method
 	msg["PulseNum"] = cc.PulseNum
 	args, err := cc.Arguments.MarshalJSON()
 	if err != nil {
