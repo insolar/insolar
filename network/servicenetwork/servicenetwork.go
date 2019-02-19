d632061e2f14795867e108cc81d427e9f656059b
anton.alekseev <anton.alexeev@insolar.io>
2019-02-19 21:17:57 +0300

    INS-1494 remove outdated comment

--- network/servicenetwork/servicenetwork.go
+++ network/servicenetwork/servicenetwork.go
@@ -194,7 +194,6 @@ func (n *ServiceNetwork) Start(ctx context.Context) error {
 	return nil
 }
 
-// TODO add ETA as param
 func (n *ServiceNetwork) Leave(ctx context.Context, ETA core.PulseNumber) {
 	logger := inslogger.FromContext(ctx)
 	logger.Info("Gracefully stopping service network")
