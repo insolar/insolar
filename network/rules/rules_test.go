3eb7783c54421cad9f5848584df652d8315f5f16
Andrey Bronin <andrey.bronin@insolar.io>
2019-02-21 16:15:01 +0300

    INS-1517: rules test skipped

--- network/rules/rules_test.go
+++ network/rules/rules_test.go
@@ -1,1 +1,59 @@
 package rules
+
+import (
+	"testing"
+
+	"github.com/insolar/insolar/certificate"
+	"github.com/insolar/insolar/component"
+	"github.com/insolar/insolar/core"
+	"github.com/insolar/insolar/network/nodenetwork"
+	"github.com/insolar/insolar/testutils"
+	"github.com/insolar/insolar/testutils/network"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRules_CheckMajorityRule(t *testing.T) {
+	t.Skip("tmp")
+	cm := component.Manager{}
+	r := NewRules()
+	certManager := testutils.NewCertificateManagerMock(t)
+	cert := testutils.NewCertificateMock(t)
+
+	certManager.GetCertificateMock.Set(func() (r core.Certificate) {
+		return cert
+	})
+
+	cert.GetDiscoveryNodesMock.Set(func() (r []core.DiscoveryNode) {
+		_, nodes := getDiscoveryNodes(5)
+		return nodes
+	})
+
+	cert.GetMajorityRuleMock.Set(func() (r int) {
+		return 4
+	})
+
+	nodeKeeper := network.NewNodeKeeperMock(t)
+	nodeKeeper.GetActiveNodesMock.Set(func() (r []core.Node) {
+		nodes, _ := getDiscoveryNodes(5)
+		return nodes
+	})
+
+	cm.Inject(r, certManager, nodeKeeper)
+
+	result, count := r.CheckMajorityRule()
+	assert.True(t, result)
+	assert.Equal(t, 0, count)
+}
+
+func getDiscoveryNodes(count int) ([]core.Node, []core.DiscoveryNode) {
+	result1 := make([]core.Node, count)
+	result2 := make([]core.DiscoveryNode, count)
+
+	for i := 1; i <= count; i++ {
+		recordRef := core.RecordRef{byte(i)}
+		result1 = append(result1, nodenetwork.NewNode(recordRef, core.StaticRoleVirtual, nil, "127.0.0.1:3000", ""))
+		result2 = append(result2, certificate.NewBootstrapNode(nil, "", "127.0.0.1:3000", recordRef.String()))
+	}
+
+	return result1, result2
+}
