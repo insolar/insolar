3ede2162295bcb132af804eab9e943ff6a2029ac
egorikas <egorgrishechko@gmail.com>
2019-02-25 13:01:23 +0300

    INS-1574 move pulsar data to another folder

--- configuration/pulsar.go
+++ configuration/pulsar.go
@@ -73,7 +73,7 @@ func NewPulsar() Pulsar {
 		ReceivingVectorTimeout: 1000,
 
 		Neighbours: []PulsarNodeAddress{},
-		Storage:    Storage{DataDirectory: "./data/pulsar"},
+		Storage:    Storage{DataDirectory: "./.artifacts/pulsar"},
 
 		NumberDelta: 10,
 		DistributionTransport: Transport{
