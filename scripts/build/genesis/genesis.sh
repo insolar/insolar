17278478698b882d6cefea18ad8fe7aec1299f7c
vany <ivan.serezhkin@insolar.io>
2019-02-26 21:09:32 +0300

    INS-1652: Dockerfile

--- scripts/build/genesis/genesis.sh
+++ scripts/build/genesis/genesis.sh
@@ -4,32 +4,27 @@ echo ==============================
 echo Making genesis
 echo ==============================
 
-pushd `dirname $0`
-INSOLAR=`git rev-parse --show-toplevel`
-popd
+SCRIPTPATH=`dirname $0`
+INSOLAR=`git -C $SCRIPTPATH rev-parse --show-toplevel`
 
-BASE_DIR=$INSOLAR/scripts/insolard
-
-dirs="data keys certs bin log"
+dirs="data certs bin log"
 for d in $dirs; do rm -rf $d; done
 for d in $dirs; do mkdir $d; done
 rm -f insolar.cfg.yaml
 
-#pushd $INSOLAR
-#make build
-#popd
+make -C $INSOLAR build
+
+cp $INSOLAR/bin/insgorund bin/
 
 ROOT_MEMBER_KEY=keys/root_member.key.json
 BOOTSTRAP_KEY=keys/bootstrap.key.json
-[ -f $ROOT_MEMBER_KEY ] || $INSOLAR/bin/insolar -c gen_keys > $ROOT_MEMBER_KEY
-[ -f $BOOTSTRAP_KEY ] || $INSOLAR/bin/insolar -c gen_keys > $BOOTSTRAP_KEY
-
+[[ -d keys ]] || mkdir keys
+[[ -f $ROOT_MEMBER_KEY ]] || $INSOLAR/bin/insolar -c gen_keys > $ROOT_MEMBER_KEY
+[[ -f $BOOTSTRAP_KEY ]] || $INSOLAR/bin/insolar -c gen_keys > $BOOTSTRAP_KEY
 
 echo Creating node config
 cp $INSOLAR/scripts/build/genesis/insolar.yaml insolar.cfg.yaml
 echo "keyspath: $BOOTSTRAP_KEY" >> insolar.cfg.yaml
-
-
 
 $INSOLAR/bin/insolard  \
                 --config insolar.cfg.yaml \
