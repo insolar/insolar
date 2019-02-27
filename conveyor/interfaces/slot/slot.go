b40073f7d82ea971d896deaf7ffefcad371b7095
Ivan Shibitov <ivan.shibitov@insolar.io>
2019-02-27 19:50:57 +0300

    INS-1508: add slot interface

---
+++ conveyor/interfaces/slot/slot.go
@@ -0,0 +1,49 @@
+/*
+ *    Copyright 2019 Insolar Technologies
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package slot
+
+type reactivateMode interface{}
+
+//go:generate minimock -i github.com/insolar/insolar/conveyor/interfaces.SlotElementHelper -o ./ -s _mock.go
+type SlotElementHelper interface {
+	SlotElementRestrictedHelper
+	informParent(payload interface{}) bool
+	deactivateTill(reactivateOn reactivateMode)
+	sendTask(adapterID uint32, taskPayload interface{}, respHandlerID uint32) error
+	// joinSequence( sequenceKey map-key,sequenceOrder uint64 )
+	// isSequenceHead() bool
+}
+
+//go:generate minimock -i github.com/insolar/insolar/conveyor/interfaces.SlotElementRestrictedHelper -o ./ -s _mock.go
+type SlotElementRestrictedHelper interface {
+	SlotElementReadOnly
+
+	GetParentElementID() uint32
+	GetInputEvent() interface{}
+	GetPayload() interface{}
+
+	Reactivate()
+	LeaveSequence()
+}
+
+//go:generate minimock -i github.com/insolar/insolar/conveyor/interfaces.SlotElementReadOnly -o ./ -s _mock.go
+type SlotElementReadOnly interface {
+	GetElementID() uint32
+	GetNodeID() uint32
+	GetType() int
+	GetState() int
+}
