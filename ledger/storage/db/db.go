48ef26cfbd624b9c1a0553bd2d7b0c7d1677e1b3
Andrey Romancev <andrey@romancev.com>
2019-02-28 18:15:08 +0300

    INS-1660: Add DB implementations.

--- ledger/storage/db/db.go
+++ ledger/storage/db/db.go
@@ -2,9 +2,18 @@ package db
 
 type Scope byte
 
+func (s Scope) Bytes() []byte {
+	return []byte{byte(s)}
+}
+
 type Key interface {
 	Scope() Scope
 	Key() []byte
+}
+
+type DB interface {
+	Get(Key) ([]byte, error)
+	Set(Key, []byte) error
 }
 
 const (
