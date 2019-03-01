dfc10c7f4d2a9fc230066c9d8bfd61e676cd9281
Ilya Markin <amferiuss@gmail.com>
2019-03-01 18:47:49 +0300

    INS-1711: new blob storage draft

---
+++ ledger/storage/blob/blob.go
@@ -0,0 +1,35 @@
+/*
+ *    Copyright 2019 Insolar Technologies
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package blob
+
+import (
+	"context"
+
+	"github.com/insolar/insolar/core"
+)
+
+// Accessor provides // TODO
+//go:generate minimock -i github.com/insolar/insolar/ledger/storage/blob.Accessor -o ./ -s _mock.go
+type Accessor interface {
+	Get(ctx context.Context, jetID core.JetID, id *core.RecordID) ([]byte, error)
+}
+
+// Modifier provides // TODO
+//go:generate minimock -i github.com/insolar/insolar/ledger/storage/blob.Modifier -o ./ -s _mock.go
+type Modifier interface {
+	Set(ctx context.Context, jetID core.JetID, pulseNumber core.PulseNumber, blob []byte) (*core.RecordID, error)
+}
