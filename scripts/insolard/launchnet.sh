57b2dac5f58d6408f7001ab9db8ce02e08553719
egorikas <egorgrishechko@gmail.com>
2019-03-15 13:15:15 +0300

    INS-2032 fix tests

--- scripts/insolard/launchnet.sh
+++ scripts/insolard/launchnet.sh
@@ -9,6 +9,7 @@ PULSARD=$BIN_DIR/pulsard
 PULSEWATCHER=$BIN_DIR/pulsewatcher
 CONTRACT_STORAGE=contractstorage
 LEDGER_DIR=data
+LEDGER_NEW_DIR=new-data
 CONFIGS_DIR=configs
 BASE_DIR=scripts/insolard
 KEYS_FILE=$BASE_DIR/$CONFIGS_DIR/bootstrap_keys.json
@@ -97,6 +98,7 @@ clear_dirs()
     echo "clear_dirs() starts ..."
     rm -rfv $CONTRACT_STORAGE/*
     rm -rfv $LEDGER_DIR/*
+    rm -rfv $LEDGER_NEW_DIR/*
     rm -rfv $DISCOVERY_NODES_DATA/*
     rm -rfv $GENERATED_CONFIGS_DIR/*
     rm -rfv $INSGORUND_DATA/*
@@ -109,6 +111,7 @@ create_required_dirs()
     echo "create_required_dirs() starts ..."
     mkdir -vp $CONTRACT_STORAGE
     mkdir -vp $LEDGER_DIR
+    mkdir -vp $LEDGER_NEW_DIR
     mkdir -vp $DISCOVERY_NODES_DATA/certs
     mkdir -vp $GENERATED_CONFIGS_DIR
     mkdir -vp $INSGORUND_DATA
@@ -117,6 +120,7 @@ create_required_dirs()
     for node in "${DISCOVERY_NODES[@]}"
     do
         mkdir -vp $node/data
+        mkdir -vp $node/new-data
     done
 
     mkdir -p scripts/insolard/$CONFIGS_DIR
@@ -235,6 +239,7 @@ copy_data()
     for node in "${DISCOVERY_NODES[@]}"
     do
         cp -v $LEDGER_DIR/* $node/data
+        cp -v $LEDGER_NEW_DIR/* $node/new-data
     done
     echo "copy_data() end."
 }
