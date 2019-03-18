b88a8d96b26fe7502fe71009ba8f167247f49a21
Alexander Orlovsky <nordicdyno@gmail.com>
2019-03-18 21:44:55 +0300

    INS-1658: improve fuzzify of StringFromBytes test

--- gen/string_test.go
+++ gen/string_test.go
@@ -19,12 +19,14 @@ package gen
 import (
 	"testing"
 
+	"github.com/google/gofuzz"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGen_StringFromBytes(t *testing.T) {
-	symbolsString := "1aBDEhwqidwqkdoij39u"
-	symbols := []byte(symbolsString)
+	symbolFuzzer := fuzz.New().NilChance(0).NumElements(1, 100)
+	var symbols []byte
+	symbolFuzzer.Fuzz(&symbols)
 	for i := 0; i < 100; i++ {
 		s := StringFromBytes(symbols, i)
 		require.GreaterOrEqualf(t, i, len(s), "string length should not be greater than `maxcount`")
