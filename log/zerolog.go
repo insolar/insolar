42c0bcea8eaecf2ebc8da3a49c3ffb1b5ff5c21b
Ivan Shibitov <ivan.shibitov@insolar.io>
2019-03-19 11:58:36 +0300

    INS-2044: return zerolog

--- log/zerolog.go
+++ log/zerolog.go
@@ -45,9 +45,7 @@ func newZerologAdapter(cfg configuration.Log) (*zerologAdapter, error) {
 	}
 
 	zerolog.CallerSkipFrameCount = 3
-	//return &zerologAdapter{logger: zerolog.New(output).Level(zerolog.InfoLevel).With().Timestamp().Caller().Logger()}, nil
-	//return &zerologAdapter{logger: zerolog.New(output).Level(zerolog.InfoLevel).With().Timestamp().Logger()}, nil
-	return &zerologAdapter{logger: zerolog.New(output).Level(zerolog.InfoLevel).With().Logger()}, nil
+	return &zerologAdapter{logger: zerolog.New(output).Level(zerolog.InfoLevel).With().Timestamp().Caller().Logger()}, nil
 }
 
 // WithFields return copy of adapter with predefined fields.
