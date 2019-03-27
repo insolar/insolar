7e0d11f0b9621dbcc64bdce1156eb9f431185056
egorikas <egorgrishechko@gmail.com>
2019-03-27 19:37:42 +0300

    INS-2015 adding TestJetIndex_For

--- ledger/storage/db/jetindex_test.go
+++ ledger/storage/db/jetindex_test.go
@@ -19,8 +19,10 @@ package db
 import (
 	"testing"
 
+	"github.com/insolar/insolar/insolar"
 	"github.com/insolar/insolar/insolar/gen"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestIndex_Add(t *testing.T) {
@@ -43,4 +45,34 @@ func TestJetIndex_Delete(t *testing.T) {
 	idx.storage[jetID][id] = struct{}{}
 	idx.Delete(id, jetID)
 	assert.Nil(t, idx.storage[jetID])
+}
+
+func TestJetIndex_For(t *testing.T) {
+	t.Parallel()
+
+	idx := NewJetIndex()
+	id := insolar.NewID(insolar.PulseNumber(4), []byte{1})
+	sID := insolar.NewID(insolar.PulseNumber(4), []byte{2})
+	tID := insolar.NewID(insolar.PulseNumber(4), []byte{3})
+	jetID := gen.JetID()
+	idx.Add(*id, jetID)
+	idx.Add(*sID, jetID)
+	idx.Add(*tID, jetID)
+
+	for i := 0; i < 100; i++ {
+		id := gen.ID()
+		if id.Pulse() != insolar.PulseNumber(4) {
+			idx.Add(gen.ID(), gen.JetID())
+		}
+	}
+
+	res := idx.For(jetID, insolar.PulseNumber(4))
+
+	require.Equal(t, 3, len(res))
+	_, ok := res[*id]
+	require.Equal(t, true, ok)
+	_, ok = res[*sID]
+	require.Equal(t, true, ok)
+	_, ok = res[*tID]
+	require.Equal(t, true, ok)
 }
