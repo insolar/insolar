d404c8704fff0ad622e5d1c9ce57a40886b52da4
Ivan Shibitov <ivan.shibitov@insolar.io>
2019-03-29 13:30:13 +0300

    INS-2172: add generated init

---
+++ conveyor/generator/state_machines/initial/initial_generated.go
@@ -0,0 +1,95 @@
+/*
+*    Copyright 2019 Insolar Technologies
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
+
+package initial
+
+import (
+	"context"
+	"errors"
+
+	"github.com/insolar/insolar/conveyor/statemachine"
+	"github.com/insolar/insolar/conveyor/fsm"
+)
+
+func RawInitialPresentFactory() *statemachine.StateMachine {
+	return &statemachine.StateMachine{
+		ID: 3,
+		States: []statemachine.State{
+			{
+				
+				Transition: func(element fsm.SlotElementHelper) (interface{}, fsm.ElementState, error) {
+    		        aInput, ok := element.GetInputEvent().(interface {})
+            		if !ok { return nil, 0, errors.New("wrong input event type") }
+            		ctx := context.TODO()
+				    state, payload := ParseInputEvent(ctx, element, aInput, element.GetPayload())
+                    return payload, state, nil
+					
+				},
+				
+			},{
+				
+				
+				
+			},
+		},
+	}
+}
+
+func RawInitialPastFactory() *statemachine.StateMachine {
+	return &statemachine.StateMachine{
+		ID: 3,
+		States: []statemachine.State{
+			{
+				Transition: func(element fsm.SlotElementHelper) (interface{}, fsm.ElementState, error) {
+    		        aInput, ok := element.GetInputEvent().(interface {})
+            		if !ok { return nil, 0, errors.New("wrong input event type") }
+            		ctx := context.TODO()
+				    state, payload := ParseInputEvent(ctx, element, aInput, element.GetPayload())
+                    return payload, state, nil
+					
+				},
+				
+			},{
+				
+				
+			},
+		},
+	}
+}
+
+func RawInitialFutureFactory() *statemachine.StateMachine {
+	return &statemachine.StateMachine{
+		ID: 3,
+		States: []statemachine.State{
+			{
+				
+				Transition: func(element fsm.SlotElementHelper) (interface{}, fsm.ElementState, error) {
+    		        aInput, ok := element.GetInputEvent().(interface {})
+            		if !ok { return nil, 0, errors.New("wrong input event type") }
+            		ctx := context.TODO()
+				    state, payload := ParseInputEvent(ctx, element, aInput, element.GetPayload())
+                    return payload, state, nil
+					
+				},
+				
+			},{
+				
+				
+				
+			},
+		},
+	}
+}
