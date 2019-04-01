4c50780feff7232298d2637411f15d99f544b92d
anton.alekseev <anton.alexeev@insolar.io>
2019-04-01 14:22:52 +0300

    INS-1552 imports

--- network/merkle/errors_test.go
+++ network/merkle/errors_test.go
@@ -56,15 +56,16 @@ import (
 	"encoding/hex"
 	"testing"
 
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
 	"github.com/insolar/insolar/component"
 	"github.com/insolar/insolar/insolar"
 	"github.com/insolar/insolar/platformpolicy"
 	"github.com/insolar/insolar/pulsar/pulsartestutils"
 	"github.com/insolar/insolar/testutils"
 	"github.com/insolar/insolar/testutils/nodekeeper"
-	"github.com/pkg/errors"
-	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
 type calculatorErrorSuite struct {
