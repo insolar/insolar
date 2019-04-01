64b415e53939384485b53bd68bd069a099b50503
Andrey Bronin <andrey.bronin@insolar.io>
2019-04-01 18:42:34 +0300

    INS-2216: update stringer

--- insolar/messagetype_string.go
+++ insolar/messagetype_string.go
@@ -4,6 +4,42 @@ package insolar
 
 import "strconv"
 
+func _() {
+	// An "invalid array index" compiler error signifies that the constant values have changed.
+	// Re-run the stringer command to generate them again.
+	var x [1]struct{}
+	_ = x[TypeCallMethod-0]
+	_ = x[TypeCallConstructor-1]
+	_ = x[TypeReturnResults-2]
+	_ = x[TypeExecutorResults-3]
+	_ = x[TypeValidateCaseBind-4]
+	_ = x[TypeValidationResults-5]
+	_ = x[TypePendingFinished-6]
+	_ = x[TypeStillExecuting-7]
+	_ = x[TypeGetCode-8]
+	_ = x[TypeGetObject-9]
+	_ = x[TypeGetDelegate-10]
+	_ = x[TypeGetChildren-11]
+	_ = x[TypeUpdateObject-12]
+	_ = x[TypeRegisterChild-13]
+	_ = x[TypeJetDrop-14]
+	_ = x[TypeSetRecord-15]
+	_ = x[TypeValidateRecord-16]
+	_ = x[TypeSetBlob-17]
+	_ = x[TypeGetObjectIndex-18]
+	_ = x[TypeGetPendingRequests-19]
+	_ = x[TypeHotRecords-20]
+	_ = x[TypeGetJet-21]
+	_ = x[TypeAbandonedRequestsNotification-22]
+	_ = x[TypeGetRequest-23]
+	_ = x[TypeGetPendingRequestID-24]
+	_ = x[TypeValidationCheck-25]
+	_ = x[TypeHeavyStartStop-26]
+	_ = x[TypeHeavyPayload-27]
+	_ = x[TypeBootstrapRequest-28]
+	_ = x[TypeNodeSignRequest-29]
+}
+
 const _MessageType_name = "TypeCallMethodTypeCallConstructorTypeReturnResultsTypeExecutorResultsTypeValidateCaseBindTypeValidationResultsTypePendingFinishedTypeStillExecutingTypeGetCodeTypeGetObjectTypeGetDelegateTypeGetChildrenTypeUpdateObjectTypeRegisterChildTypeJetDropTypeSetRecordTypeValidateRecordTypeSetBlobTypeGetObjectIndexTypeGetPendingRequestsTypeHotRecordsTypeGetJetTypeAbandonedRequestsNotificationTypeGetRequestTypeGetPendingRequestIDTypeValidationCheckTypeHeavyStartStopTypeHeavyPayloadTypeBootstrapRequestTypeNodeSignRequest"
 
 var _MessageType_index = [...]uint16{0, 14, 33, 50, 69, 89, 110, 129, 147, 158, 171, 186, 201, 217, 234, 245, 258, 276, 287, 305, 327, 341, 351, 384, 398, 421, 440, 458, 474, 494, 513}
