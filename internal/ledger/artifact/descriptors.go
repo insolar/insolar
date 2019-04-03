7ca3bf214e9aba378a06ae3e03329f1619eaba65
Alexander Orlovsky <nordicdyno@gmail.com>
2019-04-03 19:41:05 +0300

    INS-1979: add artifact.ObjectDescriptor for smooth migration from artifacts client

---
+++ internal/ledger/artifact/descriptors.go
@@ -0,0 +1,113 @@
+package artifact
+
+import (
+	"errors"
+
+	"github.com/insolar/insolar/insolar"
+)
+
+// RefIterator is used for iteration over affined children(parts) of container.
+type RefIterator interface {
+	Next() (*insolar.Reference, error)
+	HasNext() bool
+}
+
+//go:generate minimock -i github.com/insolar/insolar/internal/ledger/artifact.ObjectDescriptor -o ./ -s _mock.go
+
+// ObjectDescriptor represents meta info required to fetch all object data.
+type ObjectDescriptor interface {
+	// HeadRef returns head reference to represented object record.
+	HeadRef() *insolar.Reference
+
+	// StateID returns reference to object state record.
+	StateID() *insolar.ID
+
+	// Memory fetches object memory from storage.
+	Memory() []byte
+
+	// IsPrototype determines if the object is a prototype.
+	IsPrototype() bool
+
+	// Code returns code reference.
+	Code() (*insolar.Reference, error)
+
+	// Prototype returns prototype reference.
+	Prototype() (*insolar.Reference, error)
+
+	// Children returns object's children references.
+	Children(pulse *insolar.PulseNumber) (RefIterator, error)
+
+	// ChildPointer returns the latest child for this object.
+	ChildPointer() *insolar.ID
+
+	// Parent returns object's parent.
+	Parent() *insolar.Reference
+}
+
+// objectDescriptor represents meta info required to fetch all object data.
+type objectDescriptor struct {
+	head         insolar.Reference
+	state        insolar.ID
+	prototype    *insolar.Reference
+	isPrototype  bool
+	childPointer *insolar.ID // can be nil.
+	memory       []byte
+	parent       insolar.Reference
+}
+
+// IsPrototype determines if the object is a prototype.
+func (d *objectDescriptor) IsPrototype() bool {
+	return d.isPrototype
+}
+
+// Code returns code reference.
+func (d *objectDescriptor) Code() (*insolar.Reference, error) {
+	if !d.IsPrototype() {
+		return nil, errors.New("object is not a prototype")
+	}
+	if d.prototype == nil {
+		return nil, errors.New("object has no code")
+	}
+	return d.prototype, nil
+}
+
+// Prototype returns prototype reference.
+func (d *objectDescriptor) Prototype() (*insolar.Reference, error) {
+	if d.IsPrototype() {
+		return nil, errors.New("object is not an instance")
+	}
+	if d.prototype == nil {
+		return nil, errors.New("object has no prototype")
+	}
+	return d.prototype, nil
+}
+
+// HeadRef returns reference to represented object record.
+func (d *objectDescriptor) HeadRef() *insolar.Reference {
+	return &d.head
+}
+
+// StateID returns reference to object state record.
+func (d *objectDescriptor) StateID() *insolar.ID {
+	return &d.state
+}
+
+// ChildPointer returns the latest child for this object.
+func (d *objectDescriptor) ChildPointer() *insolar.ID {
+	return d.childPointer
+}
+
+// Memory fetches latest memory of the object known to storage.
+func (d *objectDescriptor) Memory() []byte {
+	return d.memory
+}
+
+// Children returns object's children references.
+func (d *objectDescriptor) Children(pulse *insolar.PulseNumber) (RefIterator, error) {
+	panic("Children method not implemented for artifact.Descriptor")
+}
+
+// Parent returns object's parent.
+func (d *objectDescriptor) Parent() *insolar.Reference {
+	return &d.parent
+}
