3bfb6c235fad4d500fde63c078fc0cee9abc9a0c
Andrey Bronin <andrey.bronin@insolar.io>
2019-04-08 19:17:14 +0300

    INS-2215: transport2 draft

---
+++ network/transport/transport2_test.go
@@ -0,0 +1,23 @@
+package transport
+
+import (
+	"context"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransport(t *testing.T) {
+	tcp := TcpTransport{listenAddress: "127.0.0.1:8080"}
+	ctx := context.Background()
+	err := tcp.Start(ctx)
+	defer tcp.Stop(ctx)
+	assert.NoError(t, err)
+
+	_, err = http.Get("http://127.0.0.1:8080")
+	assert.NoError(t, err)
+
+	<-time.After(12 * time.Hour)
+}
