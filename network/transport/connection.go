f2882721e1df6af0e77bdf7bdf940bc28136fbef
Andrey Bronin <andrey.bronin@insolar.io>
2019-04-08 18:14:52 +0300

    INS-2215: connectionFactory in separate file

---
+++ network/transport/connection.go
@@ -0,0 +1,38 @@
+package transport
+
+import (
+	"context"
+	"net"
+
+	"github.com/pkg/errors"
+
+	"github.com/insolar/insolar/instrumentation/inslogger"
+)
+
+type tcpConnectionFactory struct{}
+
+func (*tcpConnectionFactory) CreateConnection(ctx context.Context, address net.Addr) (net.Conn, error) {
+	logger := inslogger.FromContext(ctx)
+	tcpAddress, ok := address.(*net.TCPAddr)
+	if !ok {
+		return nil, errors.New("[ createConnection ] Failed to get tcp address")
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddress)
+	if err != nil {
+		logger.Errorf("[ createConnection ] Failed to open connection to %s: %s", address, err.Error())
+		return nil, errors.Wrap(err, "[ createConnection ] Failed to open connection")
+	}
+
+	err = conn.SetKeepAlive(true)
+	if err != nil {
+		logger.Error("[ createConnection ] Failed to set keep alive")
+	}
+
+	err = conn.SetNoDelay(true)
+	if err != nil {
+		logger.Error("[ createConnection ] Failed to set connection no delay: ", err.Error())
+	}
+
+	return conn, nil
+}
--- network/transport/tcp.go
+++ network/transport/tcp.go
@@ -220,31 +220,3 @@ func (t *tcpTransport) handleAcceptedConnection(conn net.Conn) {
 		}
 	}
 }
-
-type tcpConnectionFactory struct{}
-
-func (*tcpConnectionFactory) CreateConnection(ctx context.Context, address net.Addr) (net.Conn, error) {
-	logger := inslogger.FromContext(ctx)
-	tcpAddress, ok := address.(*net.TCPAddr)
-	if !ok {
-		return nil, errors.New("[ createConnection ] Failed to get tcp address")
-	}
-
-	conn, err := net.DialTCP("tcp", nil, tcpAddress)
-	if err != nil {
-		logger.Errorf("[ createConnection ] Failed to open connection to %s: %s", address, err.Error())
-		return nil, errors.Wrap(err, "[ createConnection ] Failed to open connection")
-	}
-
-	err = conn.SetKeepAlive(true)
-	if err != nil {
-		logger.Error("[ createConnection ] Failed to set keep alive")
-	}
-
-	err = conn.SetNoDelay(true)
-	if err != nil {
-		logger.Error("[ createConnection ] Failed to set connection no delay: ", err.Error())
-	}
-
-	return conn, nil
-}
