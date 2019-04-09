7904c7fa9d2f52248e3bc6683234abadf4a5afd3
Alexander Alexeev <alexander.alexeev@insolar.io>
2019-04-09 20:04:14 +0300

    INS-2219: change loglevel tests (work in progress)

--- functest/change_loglevel_test.go
+++ functest/change_loglevel_test.go
@@ -19,15 +19,28 @@
 package functest
 
 import (
+	"io/ioutil"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestChangeLogLevel(t *testing.T) {
+func TestChangeLogLevelOk(t *testing.T) {
 	url := HOST_DEBUG + "/debug/loglevel?level=debug"
-	getResp, err := http.Get(url)
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
 	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, getResp.StatusCode)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+	resBody, err := ioutil.ReadAll(resp.Body)
+	require.Nil(t, err)
+	require.Equal(t, string(resBody), "New log level: 'debug'")
+}
+
+func TestChangeLogLevelFail(t *testing.T) {
+	url := HOST_DEBUG + "/debug/loglevel?level=ololo"
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	require.NoError(t, err)
+	require.NotEqual(t, http.StatusOK, resp.StatusCode)
 }
