06add8904ee985569d264ae3d07edef778323b68
hirama <crrrr@mail.ru>
2019-04-09 14:43:01 +0300

    secp256k1 curve

--- platformpolicy/customcrypto/secp256k1/curve.go
+++ platformpolicy/customcrypto/secp256k1/curve.go
@@ -5,76 +5,33 @@ import (
 	"math/big"
 )
 
-// A Curve represents a short-form Weierstrass curve with a=-3.
-// See https://www.hyperelliptic.org/EFD/g1p/auto-shortw.html
-type Curve interface {
-	// Params returns the parameters for the curve.
-	Params() *CurveParams
-	// IsOnCurve reports whether the given (x,y) lies on the curve.
-	IsOnCurve(x, y *big.Int) bool
-	// Add returns the sum of (x1,y1) and (x2,y2)
-	Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int)
-	// Double returns 2*(x,y)
-	Double(x1, y1 *big.Int) (x, y *big.Int)
-	// ScalarMult returns k*(Bx,By) where k is a number in big-endian form.
-	ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int)
-	// ScalarBaseMult returns k*G, where G is the base point of the group
-	// and k is an integer in big-endian form.
-	ScalarBaseMult(k []byte) (x, y *big.Int)
+type BitCurve struct {
+	P       *big.Int // the order of the underlying field
+	N       *big.Int // the order of the base point
+	B       *big.Int // the constant of the BitCurve equation
+	Gx, Gy  *big.Int // (x,y) of the base point
+	BitSize int      // the size of the underlying field
 }
 
-type CurveParams struct {
-	P       *big.Int
-	N       *big.Int
-	B       *big.Int
-	Gx, Gy  *big.Int
-	BitSize int
-	Name    string
-}
-
-var (
-	secp256k1 *CurveParams
-	p, _      = new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
-	n, _      = new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16)
-	b, _      = new(big.Int).SetString("0000000000000000000000000000000000000000000000000000000000000007", 16)
-	gx, _     = new(big.Int).SetString("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16)
-	gy, _     = new(big.Int).SetString("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16)
-	bitSize   = 256
-	name      = "secp256k1"
-)
-
-func Secp256k1() elliptic.Curve {
-	return &CurveParams{
-		P:       p,
-		N:       n,
-		B:       b,
-		Gx:      gx,
-		Gy:      gy,
-		BitSize: bitSize,
-		Name:    name,
-	}
-}
-
-func (curve *CurveParams) Params() *elliptic.CurveParams {
+func (curve *BitCurve) Params() *elliptic.CurveParams {
 	return &elliptic.CurveParams{
-		P:       p,
-		N:       n,
-		B:       b,
-		Gx:      gx,
-		Gy:      gy,
-		BitSize: bitSize,
-		Name:    name,
+		P:       curve.P,
+		N:       curve.N,
+		B:       curve.B,
+		Gx:      curve.Gx,
+		Gy:      curve.Gy,
+		BitSize: curve.BitSize,
 	}
 }
 
-func (curve *CurveParams) IsOnCurve(x, y *big.Int) bool {
+func (curve *BitCurve) IsOnCurve(x, y *big.Int) bool {
 	y2 := new(big.Int).Exp(y, big.NewInt(2), nil)
 	x3 := new(big.Int).Exp(x, big.NewInt(3), nil)
 	ans := new(big.Int).Mod(y2.Sub(y2, x3.Add(x3, curve.B)), curve.P)
 	return ans.Cmp(big.NewInt(0)) == 0
 }
 
-func (curve *CurveParams) addJacobian(x1, y1, z1, x2, y2, z2 *big.Int) (*big.Int, *big.Int, *big.Int) {
+func (curve *BitCurve) addJacobian(x1, y1, z1, x2, y2, z2 *big.Int) (*big.Int, *big.Int, *big.Int) {
 	x3, y3, z3 := new(big.Int), new(big.Int), new(big.Int)
 	if z1.Sign() == 0 {
 		x3.Set(x2)
@@ -142,7 +99,7 @@ func (curve *CurveParams) addJacobian(x1, y1, z1, x2, y2, z2 *big.Int) (*big.Int
 	return x3, y3, z3
 }
 
-func (curve *CurveParams) affineFromJacobian(x, y, z *big.Int) (xOut, yOut *big.Int) {
+func (curve *BitCurve) affineFromJacobian(x, y, z *big.Int) (xOut, yOut *big.Int) {
 	if z.Sign() == 0 {
 		return new(big.Int), new(big.Int)
 	}
@@ -164,13 +121,13 @@ func zForAffine(x, y *big.Int) *big.Int {
 	return z
 }
 
-func (curve *CurveParams) Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int) {
+func (curve *BitCurve) Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int) {
 	z1 := zForAffine(x1, y1)
 	z2 := zForAffine(x2, y2)
 	return curve.affineFromJacobian(curve.addJacobian(x1, y1, z1, x2, y2, z2))
 }
 
-func (curve *CurveParams) doubleJacobian(x, y, z *big.Int) (*big.Int, *big.Int, *big.Int) {
+func (curve *BitCurve) doubleJacobian(x, y, z *big.Int) (*big.Int, *big.Int, *big.Int) {
 	a := new(big.Int).Mul(x, x)
 	b := new(big.Int).Mul(y, y)
 	c := new(big.Int).Mul(b, b)
@@ -194,12 +151,12 @@ func (curve *CurveParams) doubleJacobian(x, y, z *big.Int) (*big.Int, *big.Int, 
 	return x3, y3, z3
 }
 
-func (curve *CurveParams) Double(x1, y1 *big.Int) (x, y *big.Int) {
+func (curve *BitCurve) Double(x1, y1 *big.Int) (x, y *big.Int) {
 	z1 := zForAffine(x1, y1)
 	return curve.affineFromJacobian(curve.doubleJacobian(x1, y1, z1))
 }
 
-func (curve *CurveParams) ScalarMult(Bx, By *big.Int, k []byte) (x, y *big.Int) {
+func (curve *BitCurve) ScalarMult(Bx, By *big.Int, k []byte) (x, y *big.Int) {
 	Bz := new(big.Int).SetInt64(1)
 	x, y, z := new(big.Int), new(big.Int), new(big.Int)
 	for _, byte := range k {
@@ -214,6 +171,24 @@ func (curve *CurveParams) ScalarMult(Bx, By *big.Int, k []byte) (x, y *big.Int) 
 	return curve.affineFromJacobian(x, y, z)
 }
 
-func (curve *CurveParams) ScalarBaseMult(k []byte) (x, y *big.Int) {
+func (curve *BitCurve) ScalarBaseMult(k []byte) (x, y *big.Int) {
 	return curve.ScalarMult(curve.Gx, curve.Gy, k)
+}
+
+var theCurve = new(BitCurve)
+
+func init() {
+	// See SEC 2 section 2.7.1
+	// curve parameters taken from:
+	// http://www.secg.org/sec2-v2.pdf
+	theCurve.P, _ = new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 0)
+	theCurve.N, _ = new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 0)
+	theCurve.B, _ = new(big.Int).SetString("0000000000000000000000000000000000000000000000000000000000000007", 0)
+	theCurve.Gx, _ = new(big.Int).SetString("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 0)
+	theCurve.Gy, _ = new(big.Int).SetString("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 0)
+	theCurve.BitSize = 256
+}
+
+func Secp256k1() *BitCurve {
+	return theCurve
 }
