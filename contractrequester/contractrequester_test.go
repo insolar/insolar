fce6f78da037147286d2bf1543d4c4560a89fb58
anton.alekseev <anton.alexeev@insolar.io>
2019-04-10 18:27:18 +0300

    INS-1465 fix method call

--- contractrequester/contractrequester_test.go
+++ contractrequester/contractrequester_test.go
@@ -152,7 +152,7 @@ func TestCallMethodCanceled(t *testing.T) {
 		return &reply.RegisterRequest{}, nil
 	}
 
-	_, err = cr.CallMethod(ctx, msg, false, &ref, method, insolar.Arguments{}, &prototypeRef)
+	_, err = cr.CallMethod(ctx, msg, false, false, &ref, method, insolar.Arguments{}, &prototypeRef)
 	require.Error(t, err)
 	assert.Contains(t, "canceled", err.Error())
 
