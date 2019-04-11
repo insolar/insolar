6aa1f2aac992d737b4ed47e55515e91e3c6f4822
Andrey Romancev <andrey@romancev.com>
2019-04-11 18:14:23 +0300

    INS-2193: Add common components.

---
+++ server/internal/logger.go
@@ -0,0 +1,36 @@
+package internal
+
+import (
+	"context"
+
+	"github.com/insolar/insolar/configuration"
+	"github.com/insolar/insolar/insolar"
+	"github.com/insolar/insolar/instrumentation/inslogger"
+	"github.com/insolar/insolar/log"
+)
+
+func Logger(
+	ctx context.Context,
+	cfg configuration.Log,
+	traceID, nodeRef, nodeRole string,
+) (context.Context, insolar.Logger) {
+	inslog, err := log.NewLog(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	if newInslog, err := inslog.WithLevel(cfg.Level); err != nil {
+		inslog.Error(err.Error())
+	} else {
+		inslog = newInslog
+	}
+
+	ctx = inslogger.SetLogger(ctx, inslog)
+	ctx, inslog = inslogger.WithTraceField(ctx, traceID)
+	ctx, inslog = inslogger.WithField(ctx, "nodeid", nodeRef)
+	ctx, inslog = inslogger.WithField(ctx, "role", nodeRole)
+	ctx = inslogger.SetLogger(ctx, inslog)
+	log.SetGlobalLogger(inslog)
+
+	return ctx, inslog
+}
