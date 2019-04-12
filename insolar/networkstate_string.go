32e891d4c046c7b0cd1543cb15c49dfa64c8d50a
vany <ivan.serezhkin@insolar.io>
2019-04-12 13:48:31 +0300

    INS-2092: merge

--- insolar/networkstate_string.go
+++ insolar/networkstate_string.go
@@ -4,6 +4,17 @@ package insolar
 
 import "strconv"
 
+func _() {
+	// An "invalid array index" compiler error signifies that the constant values have changed.
+	// Re-run the stringer command to generate them again.
+	var x [1]struct{}
+	_ = x[NoNetworkState-0]
+	_ = x[VoidNetworkState-1]
+	_ = x[JetlessNetworkState-2]
+	_ = x[AuthorizationNetworkState-3]
+	_ = x[CompleteNetworkState-4]
+}
+
 const _NetworkState_name = "NoNetworkStateVoidNetworkStateJetlessNetworkStateAuthorizationNetworkStateCompleteNetworkState"
 
 var _NetworkState_index = [...]uint8{0, 14, 30, 49, 74, 94}
