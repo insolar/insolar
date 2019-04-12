f9ac85e008dbb1b5cc62575f28e4832ac531ea2b
Alexander Alexeev <alexander.alexeev@insolar.io>
2019-04-12 18:14:58 +0300

    Additional comments to Flow (work in progress...)

--- insolar/flow/flow.go
+++ insolar/flow/flow.go
@@ -23,8 +23,11 @@ import (
 )
 
 // Handle is a one-function synchronous process that can call routines to do long processing.
-//
 // IMPORTANT: Asynchronous code is NOT ALLOWED here.
+// To create a new Handle of a given message use handler.NewHandler procedure.
+// After creating a Handle you can register it in MessageBus like this:
+// `h.Bus.MustRegister(insolar.TypeGetObject, createdHandle.WrapBusHandle)`
+// You can find an example in insolar/ladger/artifactmanager/handler.go
 type Handle func(context.Context, Flow) error
 
 // MakeHandle is a function that constructs new Handle.
