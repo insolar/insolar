e4e2cfb886610f17fdeafaa31e99d1427675d928
Ruslan Zakirov <ruslan.zakirov@gmail.com>
2019-04-17 22:29:56 +0300

    INS-2343: fix race, change of communicators while distributing pulse
    set pulse on pause and continue

--- network/servicenetwork/integr_pulsar.go
+++ network/servicenetwork/integr_pulsar.go
@@ -54,6 +54,7 @@ package servicenetwork
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -72,6 +73,8 @@ import (
 
 type TestPulsar interface {
 	Start(ctx context.Context, bootstrapHosts []string) error
+	Pause()
+	Continue()
 	component.Stopper
 }
 
@@ -101,6 +104,10 @@ type testPulsar struct {
 	distributor   insolar.PulseDistributor
 	generator     entropygenerator.EntropyGenerator
 	cm            *component.Manager
+
+	activityMutex sync.Mutex
+	active        bool
+	distributing  bool
 
 	pulseTimeMs  int32
 	reqTimeoutMs int32
@@ -133,8 +140,36 @@ func (tp *testPulsar) Start(ctx context.Context, bootstrapHosts []string) error 
 		return errors.Wrap(err, "Failed to start test pulsar components")
 	}
 
+	tp.active = true
 	go tp.distribute(ctx)
 	return nil
+}
+
+func (tp *testPulsar) Pause() {
+	tp.activityMutex.Lock()
+	tp.active = false
+	tp.activityMutex.Unlock()
+
+	for {
+		tp.activityMutex.Lock()
+		if !tp.distributing {
+			tp.activityMutex.Unlock()
+			return
+		}
+		tp.activityMutex.Unlock()
+	}
+}
+
+func (tp *testPulsar) Continue() {
+	for {
+		tp.activityMutex.Lock()
+		if !tp.distributing {
+			tp.active = true
+			tp.activityMutex.Unlock()
+			return
+		}
+		tp.activityMutex.Unlock()
+	}
 }
 
 func (tp *testPulsar) distribute(ctx context.Context) {
@@ -160,8 +195,26 @@ func (tp *testPulsar) distribute(ctx context.Context) {
 	for {
 		select {
 		case <-time.After(time.Duration(tp.pulseTimeMs) * time.Millisecond):
-			go tp.distributor.Distribute(ctx, pulse)
-			pulse = tp.incrementPulse(pulse)
+			tp.activityMutex.Lock()
+			if tp.active {
+				if tp.distributing {
+					tp.activityMutex.Unlock()
+					log.Fatal("Time for a new Pulse came while still distributing")
+				}
+
+				tp.distributing = true
+				tp.activityMutex.Unlock()
+				go func(pulse insolar.Pulse) {
+					tp.distributor.Distribute(ctx, pulse)
+
+					tp.activityMutex.Lock()
+					tp.distributing = false
+					tp.activityMutex.Unlock()
+				}(pulse)
+				pulse = tp.incrementPulse(pulse)
+			} else {
+				tp.activityMutex.Unlock()
+			}
 		case <-tp.cancellationToken:
 			return
 		}
