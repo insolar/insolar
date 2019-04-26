e5482a198b187daeb85aaefe6c5cdfddaa91ec5c
vany <ivan.serezhkin@insolar.io>
2019-04-26 19:01:50 +0300

    INS-2092: mock certs

--- network/gateway/complete.go
+++ network/gateway/complete.go
@@ -52,7 +52,12 @@ package gateway
 
 import (
 	"context"
+	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/insolar/insolar/testutils"
 
 	"github.com/insolar/insolar/certificate"
 
@@ -181,4 +186,95 @@ func (g *Complete) signCertHandler(ctx context.Context, p insolar.Parcel) (insol
 	return &reply.NodeSign{
 		Sign: sign,
 	}, nil
+}
+
+func mockCryptographyService(t *testing.T, ok bool) insolar.CryptographyService {
+	cs := testutils.NewCryptographyServiceMock(t)
+	cs.SignFunc = func(data []byte) (*insolar.Signature, error) {
+		if ok {
+			sig := insolar.SignatureFromBytes([]byte("test_sig"))
+			return &sig, nil
+		}
+		return nil, errors.New("test_error")
+	}
+	return cs
+}
+
+func mockCertificateManager(t *testing.T, certNodeRef *insolar.Reference, discoveryNodeRef *insolar.Reference, unsignCertOk bool) *testutils.CertificateManagerMock {
+	cm := testutils.NewCertificateManagerMock(t)
+	cm.GetCertificateFunc = func() insolar.Certificate {
+		return &certificate.Certificate{
+			AuthorizationCertificate: certificate.AuthorizationCertificate{
+				PublicKey: "test_public_key",
+				Reference: certNodeRef.String(),
+				Role:      "virtual",
+			},
+			MajorityRule: 0,
+			BootstrapNodes: []certificate.BootstrapNode{
+				{
+					NodeRef:     discoveryNodeRef.String(),
+					PublicKey:   "test_discovery_public_key",
+					Host:        "test_discovery_host",
+					NetworkSign: []byte("test_network_sign"),
+				},
+			},
+		}
+	}
+	cm.NewUnsignedCertificateFunc = func(key string, role string, nodeRef string) (insolar.Certificate, error) {
+		require.Equal(t, "test_node_public_key", key)
+		require.Equal(t, "virtual", role)
+
+		if unsignCertOk {
+			return &certificate.Certificate{
+				AuthorizationCertificate: certificate.AuthorizationCertificate{
+					PublicKey: key,
+					Reference: nodeRef,
+					Role:      role,
+				},
+				RootDomainReference: "test_root_domain_ref",
+				MajorityRule:        0,
+				PulsarPublicKeys:    []string{},
+				BootstrapNodes: []certificate.BootstrapNode{
+					{
+						PublicKey:   "test_discovery_public_key",
+						Host:        "test_discovery_host",
+						NetworkSign: []byte("test_network_sign"),
+						NodeRef:     discoveryNodeRef.String(),
+					},
+				},
+			}, nil
+		}
+		return nil, errors.New("test_error")
+	}
+	return cm
+}
+
+func mockMessageBus(t *testing.T, ok bool, ref *insolar.Reference, discovery *insolar.Reference) *testutils.MessageBusMock {
+	mb := testutils.NewMessageBusMock(t)
+	mb.MustRegisterFunc = func(p insolar.MessageType, handler insolar.MessageHandler) {
+		require.Equal(t, p, insolar.TypeNodeSignRequest)
+	}
+	mb.SendFunc = func(p context.Context, msg insolar.Message, options *insolar.MessageSendOptions) (insolar.Reply, error) {
+		require.Equal(t, ref, msg.(*message.NodeSignPayload).NodeRef)
+		require.Equal(t, discovery, options.Receiver)
+		if ok {
+			return &reply.NodeSign{
+				Sign: []byte("test_sig"),
+			}, nil
+		}
+		return nil, errors.New("test_error")
+	}
+	return mb
+}
+
+func mockReply(t *testing.T) []byte {
+	node, err := insolar.MarshalArgs(struct {
+		PublicKey string
+		Role      insolar.StaticRole
+	}{
+		PublicKey: "test_node_public_key",
+		Role:      insolar.StaticRoleVirtual,
+	}, nil)
+	require.NoError(t, err)
+	return []byte(node)
 }
