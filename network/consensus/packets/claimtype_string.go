325c1e19ba3354e030503c539227ee56fd2ffa33
YakovZverev <yakov.zverev@insolar.io>
2019-05-15 18:47:56 +0300

    make generate

--- network/consensus/packets/claimtype_string.go
+++ network/consensus/packets/claimtype_string.go
@@ -11,11 +11,12 @@ func _() {
 	_ = x[TypeNodeJoinClaim-1]
 	_ = x[TypeNodeAnnounceClaim-2]
 	_ = x[TypeNodeLeaveClaim-3]
+	_ = x[TypeChangeNetworkClaim-4]
 }
 
-const _ClaimType_name = "TypeNodeJoinClaimTypeNodeAnnounceClaimTypeNodeLeaveClaim"
+const _ClaimType_name = "TypeNodeJoinClaimTypeNodeAnnounceClaimTypeNodeLeaveClaimTypeChangeNetworkClaim"
 
-var _ClaimType_index = [...]uint8{0, 17, 38, 56}
+var _ClaimType_index = [...]uint8{0, 17, 38, 56, 78}
 
 func (i ClaimType) String() string {
 	i -= 1
