c4cdccc955a57c5aebe5f34d303696179fa39a73
Alexander Orlovsky <nordicdyno@gmail.com>
2019-05-17 18:25:44 +0300

    INS-2300: add platformpolicy.MustPublicKeyToString helper function

--- platformpolicy/utils.go
+++ platformpolicy/utils.go
@@ -20,15 +20,19 @@ import (
 	"crypto"
 )
 
+// MustNormalizePublicKey parses public key in PEM format, returns normalized (stable) public key value.
+// Panics on error.
 func MustNormalizePublicKey(b []byte) string {
 	ks := NewKeyProcessor()
 	pubKey, err := ks.ImportPublicKeyPEM(b)
 	if err != nil {
 		panic(err)
 	}
-	return string(MustPublicKeyToBytes(pubKey))
+	return MustPublicKeyToString(pubKey)
 }
 
+// MustPublicKeyToBytes returns byte representation of public key.
+// Panics on error.
 func MustPublicKeyToBytes(key crypto.PublicKey) []byte {
 	ks := NewKeyProcessor()
 	b, err := ks.ExportPublicKeyPEM(key)
@@ -36,4 +40,10 @@ func MustPublicKeyToBytes(key crypto.PublicKey) []byte {
 		panic(err)
 	}
 	return b
+}
+
+// MustPublicKeyToString returns string representation of public key.
+// Panics on error.
+func MustPublicKeyToString(key crypto.PublicKey) string {
+	return string(MustPublicKeyToBytes(key))
 }
