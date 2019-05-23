32cd59ef9cb88ef1337c7efd1b1c60b2767a9af1
Maria Zubareva <maria.zubareva@insolar.io>
2019-05-23 13:40:52 +0300

    NOISSUE: 'invalid child record' retry in functests

--- functest/utils_test.go
+++ functest/utils_test.go
@@ -245,6 +245,13 @@ func signedRequest(user *user, method string, params ...interface{}) (interface{
 			continue
 		}
 
+		if strings.Contains(resp.Error, "invalid child record") {
+			fmt.Printf("Invalid child record, retry. Attempt: %d/%d\n(error - %s)\n", currentInterNum, sendRetryCount, resp.Error)
+			fmt.Printf("Method: %s\n", method)
+			time.Sleep(time.Second)
+			continue
+		}
+
 		break
 	}
 
