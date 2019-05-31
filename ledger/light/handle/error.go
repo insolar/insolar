14be87fec1a6ec14dc1db3534307b8c37a93a075
Andrey Romancev <andrey@romancev.com>
2019-05-31 15:18:25 +0300

    INS-2472: Better errors.

--- ledger/light/handle/error.go
+++ ledger/light/handle/error.go
@@ -24,6 +24,7 @@ import (
 	"github.com/insolar/insolar/insolar/flow"
 	"github.com/insolar/insolar/insolar/payload"
 	"github.com/insolar/insolar/instrumentation/inslogger"
+	"github.com/pkg/errors"
 )
 
 type Error struct {
@@ -39,12 +40,12 @@ func NewError(msg *message.Message) *Error {
 func (s *Error) Present(ctx context.Context, f flow.Flow) error {
 	pl, err := payload.UnmarshalFromMeta(s.message.Payload)
 	if err != nil {
-		inslogger.FromContext(ctx).Error("failed to unmarshal error")
+		inslogger.FromContext(ctx).Error(errors.Wrap(err, "failed to unmarshal error"))
 		return nil
 	}
 	p, ok := pl.(*payload.Error)
 	if !ok {
-		inslogger.FromContext(ctx).Error("failed to unmarshal error")
+		inslogger.FromContext(ctx).Errorf("unexpected error type %T", pl)
 		return nil
 	}
 
