fa74e7cc84570f4f84d464d4c5765a9389b6f35a
anton.alekseev <anton.alexeev@insolar.io>
2019-06-06 15:32:10 +0300

    INS-2576+INS-2566 need to inspect more

--- functest/logicrunner_test.go
+++ functest/logicrunner_test.go
@@ -284,6 +284,7 @@ func (r *Two) Hello(s string) (string, error) {
 	require.Equal(t, "Hello you too, ins. 1288 times!", resp)
 }
 
+// TODO i suspect race here, place sleep 5 sec into two.Hello and you get it
 func TestBasicNotificationCallError(t *testing.T) {
 	var contractOneCode = `
 package main
