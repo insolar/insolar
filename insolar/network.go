ad2ce25d76dfb84aa81d07c107a2e89c2b507660
Andrey Bronin <andrey.bronin@insolar.io>
2019-06-12 23:34:44 +0300

    MessageBusLocker interface removed

--- insolar/network.go
+++ insolar/network.go
@@ -75,13 +75,3 @@ const (
 	// CompleteNetworkState state means network is ok and ready for proper work
 	CompleteNetworkState
 )
-
-// TODO This Interface seems to duplicate MBLocker
-//go:generate minimock -i github.com/insolar/insolar/insolar.GlobalInsolarLock -o ../testutils -s _mock.go
-
-// GlobalInsolarLock is lock of all incoming and outcoming network calls.
-// It's not intended to be used in multiple threads. And main use of it is `Set` method of `PulseManager`.
-type GlobalInsolarLock interface {
-	Acquire(ctx context.Context)
-	Release(ctx context.Context)
-}
