2274360233a8b100b483d0c20b502345b4924bae
egor <egor.pol9kov@gmail.com>
2019-06-17 16:30:13 +0300

    WLT-471: fixed proxy

--- application/proxy/member/member.go
+++ application/proxy/member/member.go
@@ -40,7 +40,7 @@ type Params struct {
 
 // PrototypeReference to prototype of this contract
 // error checking hides in generator
-var PrototypeReference, _ = insolar.NewReferenceFromBase58("1111Ty7XVmFHr7ZMLtEyFgbiTRvd7xrxLYHooY5F3j.11111111111111111111111111111111")
+var PrototypeReference, _ = insolar.NewReferenceFromBase58("111172bzSsys44AK9U3kyjzgv4feg5DqpmkBFH7ui6.11111111111111111111111111111111")
 
 // Member holds proxy type
 type Member struct {
