4efba02a6417a7e18b73021fb875a9dfa0b52f58
egor <egor.pol9kov@gmail.com>
2019-06-18 10:46:58 +0300

    WLT-471: fixed ref in sendRequest

--- functest/utils_test.go
+++ functest/utils_test.go
@@ -203,7 +203,7 @@ func signedRequest(user *user, method string, params map[string]interface{}) (in
 			fmt.Printf("Network timeout, retry. Attempt: %d/%d\n", currentIterNum, sendRetryCount)
 			fmt.Printf("Method: %s\n", method)
 			time.Sleep(time.Second)
-			resp.Error = requester.Error{Message: netErr.Error()}
+			resp.Error = &requester.Error{Message: netErr.Error()}
 			continue
 		} else if err != nil {
 			return nil, err
