887009131005788cc15cfdf90b1827be0417907a
Alexander Alexeev <alexander.alexeev@insolar.io>
2019-06-20 15:25:27 +0300

    Revert "Revert "Merge pull request #1922 from insolar/INS-2746-refactor-jetinfo""
    This reverts commit fc8a4e70653e5fe66ea3f8da646833df0218db66.

--- ledger/drop/drop.go
+++ ledger/drop/drop.go
@@ -64,7 +64,7 @@ type Drop struct {
 	// JetID represents data about JetID of the current jet.Drop.
 	JetID insolar.JetID
 
-	// SplitIntent indicates that current jet was split.
+	// Split indicates that current jet was split.
 	Split bool
 }
 
