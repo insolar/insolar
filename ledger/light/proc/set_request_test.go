49cf2df4c019214f1e918f9fdcd3dd106d14410b
Ilya Markin <amferiuss@gmail.com>
2019-06-27 16:56:23 +0300

    INS-2753: remove test for pendings in set-request

--- ledger/light/proc/set_request_test.go
+++ ledger/light/proc/set_request_test.go
@@ -29,69 +29,8 @@ import (
 	"github.com/insolar/insolar/ledger/light/hot"
 	"github.com/insolar/insolar/ledger/light/recentstorage"
 	"github.com/insolar/insolar/ledger/object"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestSetRequest_ProceedErrTooManyPendings(t *testing.T) {
-	t.Parallel()
-
-	ctx := flow.TestContextWithPulse(
-		inslogger.TestContext(t),
-		insolar.GenesisPulse.PulseNumber+10,
-	)
-
-	writeAccessor := hot.NewWriteAccessorMock(t)
-	writeAccessor.BeginMock.Return(func() {}, nil)
-
-	sender := bus.NewSenderMock(t)
-	sender.ReplyMock.Return()
-
-	records := object.NewRecordModifierMock(t)
-	records.SetMock.Return(nil)
-
-	pending := recentstorage.NewPendingStorageMock(t)
-	pending.AddPendingRequestMock.Return()
-	provider := recentstorage.NewProviderMock(t)
-	provider.GetPendingStorageMock.Return(pending)
-	provider.CountMock.Return(recentstorage.PendingRequestsLimit + 1)
-
-	ref := gen.Reference()
-	jetID := gen.JetID()
-	id := gen.ID()
-
-	virtual := record.Virtual{
-		Union: &record.Virtual_Request{
-			Request: &record.Request{
-				Object:   &ref,
-				CallType: record.CTMethod,
-			},
-		},
-	}
-	virtualBuf, err := virtual.Marshal()
-	require.NoError(t, err)
-
-	request := payload.SetRequest{
-		Request: virtualBuf,
-	}
-	requestBuf, err := request.Marshal()
-	require.NoError(t, err)
-
-	msg := payload.Meta{
-		Payload: requestBuf,
-	}
-
-	// Pendings limit reached.
-	setRequestProc := NewSetRequest(msg, virtual, id, jetID)
-	setRequestProc.dep.writer = writeAccessor
-	setRequestProc.dep.sender = sender
-	setRequestProc.dep.recentStorage = provider
-	setRequestProc.dep.records = records
-
-	err = setRequestProc.Proceed(ctx)
-	require.Error(t, err)
-	assert.Equal(t, insolar.ErrTooManyPendingRequests, err)
-}
 
 func TestSetRequest_Proceed(t *testing.T) {
 	t.Parallel()
