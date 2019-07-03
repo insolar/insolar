1cfcd142f3441c8757ca8eb1c64137962e80d3ed
Andrey Bronin <andrey.bronin@insolar.io>
2019-07-03 19:15:09 +0300

    deserializers

--- network/consensus/serialization/deserialize.go
+++ network/consensus/serialization/deserialize.go
@@ -111,9 +111,56 @@ func (p *GlobulaConsensusProtocolV2Packet) Deserialize(data io.Reader) error {
 }
 
 func (b *PacketBody) Deserialize(data io.Reader, header *UnifiedProtocolPacketHeader) error {
+	// todo: check packet type
+	if header.GetFlag(0) {
+		var pulsarData EmbeddedPulsarData
+		err := pulsarData.Deserialize(data)
+		if err != nil {
+			return errors.Wrap(err, "[ PacketBody.Deserialize ] Can't deserialize EmbeddedPulsarData")
+		}
+	}
+
+	// todo: check packet type
+	var announcement MembershipAnnouncement
+	err := announcement.Deserialize(data)
+	if err != nil {
+		return errors.Wrap(err, "[ PacketBody.Deserialize ] Can't read Announcement")
+	}
+	b.Announcement = &announcement
+
 	panic("implement me")
 }
 
-func (b *EmbeddedPulsarData) Deserialize(data io.Reader) error {
+func (p *EmbeddedPulsarData) Deserialize(data io.Reader) error {
+	err := p.Header.Deserialize(data)
+	if err != nil {
+		return errors.Wrap(err, "[ EmbeddedPulsarData.Deserialize ] Can't deserialize Header")
+	}
+
+	// todo: PulsarPulsePacketExt
+
+	err = binary.Read(data, defaultByteOrder, &p.PulsarSignature)
+	if err != nil {
+		return errors.Wrap(err, "[ UnifiedProtocolPacketHeader.Deserialize ] Can't read ReceiverID")
+	}
+
+	panic("implement me")
+}
+
+func (m *MembershipAnnouncement) Deserialize(data io.Reader) error {
+	err := binary.Read(data, defaultByteOrder, &m.CurrentRank)
+	if err != nil {
+		return errors.Wrap(err, "[ MembershipAnnouncement.Deserialize ] Can't read CurrentRank")
+	}
+
+	if m.CurrentRank != 0 {
+		err := binary.Read(data, defaultByteOrder, &m.RequestedPower)
+		if err != nil {
+			return errors.Wrap(err, "[ MembershipAnnouncement.Deserialize ] Can't read RequestedPower")
+		}
+		// todo: Member
+		// todo: AnnounceSignature
+	}
+
 	panic("implement me")
 }
