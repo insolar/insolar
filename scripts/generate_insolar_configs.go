a8f0479776d5e08479481c75f3a69f201349f370
Alexander Orlovsky <nordicdyno@gmail.com>
2019-07-12 20:21:02 +0300

    INS-2851: frequently split jets in launchent
    split jets on every pulse until depth limit if records count greater than 10

--- scripts/generate_insolar_configs.go
+++ scripts/generate_insolar_configs.go
@@ -144,6 +144,11 @@ func main() {
 			gorundPorts = append(gorundPorts, []string{strconv.Itoa(rpcListenPort - 1), strconv.Itoa(rpcListenPort)})
 		}
 
+		if node.Role == "light_material" {
+			conf.Ledger.JetSplit.ThresholdRecordsCount = 10
+			conf.Ledger.JetSplit.ThresholdOverflowCount = 0
+		}
+
 		conf.APIRunner.Address = fmt.Sprintf(defaultHost+":191%02d", nodeIndex)
 		conf.Metrics.ListenAddress = fmt.Sprintf(defaultHost+":80%02d", nodeIndex)
 		conf.Introspection.Addr = fmt.Sprintf(defaultHost+":555%02d", nodeIndex)
