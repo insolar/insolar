87e6d9cb667bdbe3259f1a803b29f5848c2ae347
anton.alekseev <anton.alexeev@insolar.io>
2019-07-12 17:33:48 +0300

    INS-2965 formating

--- api/contract.go
+++ api/contract.go
@@ -165,11 +165,11 @@ type CallMethodArgs struct {
 
 // CallMethodReply is reply that Contract.CallMethod returns
 type CallMethodReply struct {
-	Reply           reply.CallMethod  `json:"Reply"`
-	ExtractedReply  interface{}       `json:"ExtractedReply"`
-	ExtractedError  string            `json:"ExtractedError"`
-	Error *foundation.Error `json:"FoundationError"`
-	TraceID         string            `json:"TraceID"`
+	Reply          reply.CallMethod  `json:"Reply"`
+	ExtractedReply interface{}       `json:"ExtractedReply"`
+	ExtractedError string            `json:"ExtractedError"`
+	Error          *foundation.Error `json:"FoundationError"`
+	TraceID        string            `json:"TraceID"`
 }
 
 // CallConstructor make an object from its prototype
