6785ac6e282c44bfd3b074be10c099455109b7ff
Ivan Shibitov <ivan.shibitov@insolar.io>
2019-07-15 14:49:41 +0300

    INS-2970: call finalizer in pulseManager

--- ledger/heavy/pulsemanager/pulsemanager.go
+++ ledger/heavy/pulsemanager/pulsemanager.go
@@ -26,6 +26,7 @@ import (
 	"github.com/insolar/insolar/insolar/pulse"
 	"github.com/insolar/insolar/instrumentation/inslogger"
 	"github.com/insolar/insolar/instrumentation/instracer"
+	"github.com/insolar/insolar/ledger/heavy/executor"
 
 	"github.com/pkg/errors"
 	"go.opencensus.io/trace"
@@ -38,15 +39,16 @@ type ActiveListSwapper interface {
 
 // PulseManager implements insolar.PulseManager.
 type PulseManager struct {
-	Bus               insolar.MessageBus        `inject:""`
-	NodeNet           insolar.NodeNetwork       `inject:""`
-	GIL               insolar.GlobalInsolarLock `inject:""`
-	ActiveListSwapper ActiveListSwapper         `inject:""`
-	NodeSetter        node.Modifier             `inject:""`
-	Nodes             node.Accessor             `inject:""`
-	PulseAppender     pulse.Appender            `inject:""`
-	PulseAccessor     pulse.Accessor            `inject:""`
-	JetModifier       jet.Modifier              `inject:""`
+	Bus                insolar.MessageBus          `inject:""`
+	NodeNet            insolar.NodeNetwork         `inject:""`
+	GIL                insolar.GlobalInsolarLock   `inject:""`
+	ActiveListSwapper  ActiveListSwapper           `inject:""`
+	NodeSetter         node.Modifier               `inject:""`
+	Nodes              node.Accessor               `inject:""`
+	PulseAppender      pulse.Appender              `inject:""`
+	PulseAccessor      pulse.Accessor              `inject:""`
+	FinalizationKeeper executor.FinalizationKeeper `inject:""`
+	JetModifier        jet.Modifier                `inject:""`
 
 	currentPulse insolar.Pulse
 
@@ -80,7 +82,12 @@ func (m *PulseManager) Set(ctx context.Context, newPulse insolar.Pulse) error {
 	)
 	defer span.End()
 
-	err := m.setUnderGilSection(ctx, newPulse)
+	err := m.FinalizationKeeper.OnPulse(ctx, newPulse.PulseNumber)
+	if err != nil {
+		return errors.Wrap(err, "got error calling FinalizationKeeper.OnPulse")
+	}
+
+	err = m.setUnderGilSection(ctx, newPulse)
 	if err != nil {
 		return err
 	}
