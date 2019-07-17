25b86a9216ff8de1b49c41f155407cefa1968fc9
Ilya Markin <ilyamarkin@MacBook-Pro-Lubov.local>
2019-07-17 21:45:13 +0300

    INS-2995: GetPendings handler and procedure

---
+++ ledger/light/proc/get_pendings.go
@@ -0,0 +1,82 @@
+//
+// Copyright 2019 Insolar Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package proc
+
+import (
+	"context"
+
+	"github.com/insolar/insolar/insolar"
+	"github.com/insolar/insolar/insolar/bus"
+	"github.com/insolar/insolar/insolar/flow"
+	"github.com/insolar/insolar/insolar/payload"
+	"github.com/insolar/insolar/ledger/light/executor"
+	"github.com/pkg/errors"
+)
+
+type GetPendings struct {
+	message  payload.Meta
+	objectID insolar.ID
+
+	dep struct {
+		filaments executor.FilamentCalculator
+		sender    bus.Sender
+	}
+}
+
+func NewGetPendings(msg payload.Meta, objectID insolar.ID) *GetPendings {
+	return &GetPendings{
+		message:  msg,
+		objectID: objectID,
+	}
+}
+
+func (gp *GetPendings) Dep(
+	f executor.FilamentCalculator,
+	s bus.Sender,
+) {
+	gp.dep.filaments = f
+	gp.dep.sender = s
+}
+
+func (gp *GetPendings) Proceed(ctx context.Context) error {
+	ids, err := gp.dep.filaments.PendingRequests(ctx, flow.Pulse(ctx), gp.objectID)
+	if err != nil {
+		return errors.Wrap(err, "failed to calculate pending")
+	}
+	if len(ids) == 0 {
+		msg, err := payload.NewMessage(&payload.Error{
+			Code: payload.CodeNoPendings,
+			Text: insolar.ErrNoPendingRequest.Error(),
+		})
+		if err != nil {
+			return errors.Wrap(err, "failed to create reply")
+		}
+		go gp.dep.sender.Reply(ctx, gp.message, msg)
+		return nil
+	}
+
+	if len(ids) > 100 {
+		ids = ids[:100]
+	}
+
+	msg, err := payload.NewMessage(&payload.IDs{
+		IDs: ids,
+	})
+
+	go gp.dep.sender.Reply(ctx, gp.message, msg)
+	return nil
+}
