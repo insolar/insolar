11c38d2b8ab187968819ed7b9250d6bd33b2cd25
egorikas <egorgrishechko@gmail.com>
2019-07-17 16:53:58 +0300

    INS-2865 make generate

--- ledger/light/executor/filament_cleaner_mock.go
+++ ledger/light/executor/filament_cleaner_mock.go
@@ -10,12 +10,12 @@ import (
 	"time"
 
 	"github.com/gojuno/minimock"
-	"github.com/insolar/insolar/insolar"
+	insolar "github.com/insolar/insolar/insolar"
 
 	testify_assert "github.com/stretchr/testify/assert"
 )
 
-// FilamentCleanerMock implements github.com/insolar/insolar/ledger/light/executor.FilamentCleaner
+//FilamentCleanerMock implements github.com/insolar/insolar/ledger/light/executor.FilamentCleaner
 type FilamentCleanerMock struct {
 	t minimock.Tester
 
@@ -25,7 +25,7 @@ type FilamentCleanerMock struct {
 	ClearMock       mFilamentCleanerMockClear
 }
 
-// NewFilamentCleanerMock returns a mock for github.com/insolar/insolar/ledger/light/executor.FilamentCleaner
+//NewFilamentCleanerMock returns a mock for github.com/insolar/insolar/ledger/light/executor.FilamentCleaner
 func NewFilamentCleanerMock(t minimock.Tester) *FilamentCleanerMock {
 	m := &FilamentCleanerMock{t: t}
 
@@ -52,7 +52,7 @@ type FilamentCleanerMockClearInput struct {
 	p insolar.ID
 }
 
-// Expect specifies that invocation of FilamentCleaner.Clear is expected from 1 to Infinity times
+//Expect specifies that invocation of FilamentCleaner.Clear is expected from 1 to Infinity times
 func (m *mFilamentCleanerMockClear) Expect(p insolar.ID) *mFilamentCleanerMockClear {
 	m.mock.ClearFunc = nil
 	m.expectationSeries = nil
@@ -64,7 +64,7 @@ func (m *mFilamentCleanerMockClear) Expect(p insolar.ID) *mFilamentCleanerMockCl
 	return m
 }
 
-// Return specifies results of invocation of FilamentCleaner.Clear
+//Return specifies results of invocation of FilamentCleaner.Clear
 func (m *mFilamentCleanerMockClear) Return() *FilamentCleanerMock {
 	m.mock.ClearFunc = nil
 	m.expectationSeries = nil
@@ -76,7 +76,7 @@ func (m *mFilamentCleanerMockClear) Return() *FilamentCleanerMock {
 	return m.mock
 }
 
-// ExpectOnce specifies that invocation of FilamentCleaner.Clear is expected once
+//ExpectOnce specifies that invocation of FilamentCleaner.Clear is expected once
 func (m *mFilamentCleanerMockClear) ExpectOnce(p insolar.ID) *FilamentCleanerMockClearExpectation {
 	m.mock.ClearFunc = nil
 	m.mainExpectation = nil
@@ -87,7 +87,7 @@ func (m *mFilamentCleanerMockClear) ExpectOnce(p insolar.ID) *FilamentCleanerMoc
 	return expectation
 }
 
-// Set uses given function f as a mock of FilamentCleaner.Clear method
+//Set uses given function f as a mock of FilamentCleaner.Clear method
 func (m *mFilamentCleanerMockClear) Set(f func(p insolar.ID)) *FilamentCleanerMock {
 	m.mainExpectation = nil
 	m.expectationSeries = nil
@@ -96,7 +96,7 @@ func (m *mFilamentCleanerMockClear) Set(f func(p insolar.ID)) *FilamentCleanerMo
 	return m.mock
 }
 
-// Clear implements github.com/insolar/insolar/ledger/light/executor.FilamentCleaner interface
+//Clear implements github.com/insolar/insolar/ledger/light/executor.FilamentCleaner interface
 func (m *FilamentCleanerMock) Clear(p insolar.ID) {
 	counter := atomic.AddUint64(&m.ClearPreCounter, 1)
 	defer atomic.AddUint64(&m.ClearCounter, 1)
@@ -131,17 +131,17 @@ func (m *FilamentCleanerMock) Clear(p insolar.ID) {
 	m.ClearFunc(p)
 }
 
-// ClearMinimockCounter returns a count of FilamentCleanerMock.ClearFunc invocations
+//ClearMinimockCounter returns a count of FilamentCleanerMock.ClearFunc invocations
 func (m *FilamentCleanerMock) ClearMinimockCounter() uint64 {
 	return atomic.LoadUint64(&m.ClearCounter)
 }
 
-// ClearMinimockPreCounter returns the value of FilamentCleanerMock.Clear invocations
+//ClearMinimockPreCounter returns the value of FilamentCleanerMock.Clear invocations
 func (m *FilamentCleanerMock) ClearMinimockPreCounter() uint64 {
 	return atomic.LoadUint64(&m.ClearPreCounter)
 }
 
-// ClearFinished returns true if mock invocations count is ok
+//ClearFinished returns true if mock invocations count is ok
 func (m *FilamentCleanerMock) ClearFinished() bool {
 	// if expectation series were set then invocations count should be equal to expectations count
 	if len(m.ClearMock.expectationSeries) > 0 {
@@ -161,8 +161,8 @@ func (m *FilamentCleanerMock) ClearFinished() bool {
 	return true
 }
 
-// ValidateCallCounters checks that all mocked methods of the interface have been called at least once
-// Deprecated: please use MinimockFinish method or use Finish method of minimock.Controller
+//ValidateCallCounters checks that all mocked methods of the interface have been called at least once
+//Deprecated: please use MinimockFinish method or use Finish method of minimock.Controller
 func (m *FilamentCleanerMock) ValidateCallCounters() {
 
 	if !m.ClearFinished() {
@@ -171,19 +171,19 @@ func (m *FilamentCleanerMock) ValidateCallCounters() {
 
 }
 
-// CheckMocksCalled checks that all mocked methods of the interface have been called at least once
-// Deprecated: please use MinimockFinish method or use Finish method of minimock.Controller
+//CheckMocksCalled checks that all mocked methods of the interface have been called at least once
+//Deprecated: please use MinimockFinish method or use Finish method of minimock.Controller
 func (m *FilamentCleanerMock) CheckMocksCalled() {
 	m.Finish()
 }
 
-// Finish checks that all mocked methods of the interface have been called at least once
-// Deprecated: please use MinimockFinish or use Finish method of minimock.Controller
+//Finish checks that all mocked methods of the interface have been called at least once
+//Deprecated: please use MinimockFinish or use Finish method of minimock.Controller
 func (m *FilamentCleanerMock) Finish() {
 	m.MinimockFinish()
 }
 
-// MinimockFinish checks that all mocked methods of the interface have been called at least once
+//MinimockFinish checks that all mocked methods of the interface have been called at least once
 func (m *FilamentCleanerMock) MinimockFinish() {
 
 	if !m.ClearFinished() {
@@ -192,14 +192,14 @@ func (m *FilamentCleanerMock) MinimockFinish() {
 
 }
 
-// Wait waits for all mocked methods to be called at least once
-// Deprecated: please use MinimockWait or use Wait method of minimock.Controller
+//Wait waits for all mocked methods to be called at least once
+//Deprecated: please use MinimockWait or use Wait method of minimock.Controller
 func (m *FilamentCleanerMock) Wait(timeout time.Duration) {
 	m.MinimockWait(timeout)
 }
 
-// MinimockWait waits for all mocked methods to be called at least once
-// this method is called by minimock.Controller
+//MinimockWait waits for all mocked methods to be called at least once
+//this method is called by minimock.Controller
 func (m *FilamentCleanerMock) MinimockWait(timeout time.Duration) {
 	timeoutCh := time.After(timeout)
 	for {
@@ -225,8 +225,8 @@ func (m *FilamentCleanerMock) MinimockWait(timeout time.Duration) {
 	}
 }
 
-// AllMocksCalled returns true if all mocked methods were called before the execution of AllMocksCalled,
-// it can be used with assert/require, i.e. assert.True(mock.AllMocksCalled())
+//AllMocksCalled returns true if all mocked methods were called before the execution of AllMocksCalled,
+//it can be used with assert/require, i.e. assert.True(mock.AllMocksCalled())
 func (m *FilamentCleanerMock) AllMocksCalled() bool {
 
 	if !m.ClearFinished() {
