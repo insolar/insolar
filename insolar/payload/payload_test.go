33b6e4b2527ed193c593e04f00e4a7f61677ab1b
Ilya Markin <amferiuss@gmail.com>
2019-07-18 10:39:34 +0300

    INS-2995: change client-test and fix some utilities code

--- insolar/payload/payload_test.go
+++ insolar/payload/payload_test.go
@@ -53,6 +53,7 @@ func TestMarshalUnmarshal(t *testing.T) {
 		{tp: payload.TypeMeta, pl: &payload.Meta{}},
 		{tp: payload.TypeError, pl: &payload.Error{}},
 		{tp: payload.TypeID, pl: &payload.ID{}},
+		{tp: payload.TypeIDs, pl: &payload.IDs{}},
 		{tp: payload.TypeState, pl: &payload.State{}},
 		{tp: payload.TypeGetObject, pl: &payload.GetObject{}},
 		{tp: payload.TypePassState, pl: &payload.PassState{}},
