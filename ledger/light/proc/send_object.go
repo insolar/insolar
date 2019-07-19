f313ba691dce89b971ab4725e29e2dd0e50421bb
egorikas <egorgrishechko@gmail.com>
2019-07-19 15:41:49 +0300

    INS-2979 add checking for lasteststate

--- ledger/light/proc/send_object.go
+++ ledger/light/proc/send_object.go
@@ -34,7 +34,7 @@ import (
 type SendObject struct {
 	message  payload.Meta
 	objectID insolar.ID
-	index    record.Lifeline
+	lifeline record.Lifeline
 
 	Dep struct {
 		Coordinator    jet.Coordinator
@@ -49,11 +49,11 @@ type SendObject struct {
 func NewSendObject(
 	msg payload.Meta,
 	id insolar.ID,
-	idx record.Lifeline,
+	lifeline record.Lifeline,
 ) *SendObject {
 	return &SendObject{
 		message:  msg,
-		index:    idx,
+		lifeline: lifeline,
 		objectID: id,
 	}
 }
@@ -135,9 +135,13 @@ func (p *SendObject) Proceed(ctx context.Context) error {
 		return nil
 	}
 
+	if p.lifeline.LatestState == nil {
+		return ErrNotActivated
+	}
+
 	logger := inslogger.FromContext(ctx)
 	{
-		buf, err := p.index.Marshal()
+		buf, err := p.lifeline.Marshal()
 		if err != nil {
 			return errors.Wrap(err, "failed to marshal index")
 		}
@@ -152,14 +156,14 @@ func (p *SendObject) Proceed(ctx context.Context) error {
 		logger.Info("sending index")
 	}
 
-	rec, err := p.Dep.RecordAccessor.ForID(ctx, *p.index.LatestState)
+	rec, err := p.Dep.RecordAccessor.ForID(ctx, *p.lifeline.LatestState)
 	switch err {
 	case nil:
 		logger.Info("sending state")
 		return sendState(rec)
 	case object.ErrNotFound:
 		logger.Info("state not found (sending pass)")
-		return sendPassState(*p.index.LatestState)
+		return sendPassState(*p.lifeline.LatestState)
 	default:
 		return errors.Wrap(err, "failed to fetch record")
 	}
