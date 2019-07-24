466e6282b2920a8b92123bf48b0e2792a8e54287
SergeyRadist <14805670+SergeyRadist@users.noreply.github.com>
2019-07-24 19:42:56 +0300

    INS-3014 Light Replication test - replication test in integration tests

--- ledger/light/integration/light_test.go
+++ ledger/light/integration/light_test.go
@@ -22,10 +22,6 @@ import (
 	"math/rand"
 	"sync"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
