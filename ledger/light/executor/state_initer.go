eea84050a9260a90375ea4c76ba767848d528e9b
Ilya Markin <amferiuss@gmail.com>
2019-07-25 14:25:47 +0300

    INS-3030: StateIniter implementation

--- ledger/light/executor/state_initer.go
+++ ledger/light/executor/state_initer.go
@@ -18,12 +18,106 @@ package executor
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/insolar/insolar/insolar"
+	"github.com/insolar/insolar/insolar/bus"
+	"github.com/insolar/insolar/insolar/jet"
+	"github.com/insolar/insolar/insolar/payload"
+	"github.com/insolar/insolar/ledger/drop"
+	"github.com/insolar/insolar/ledger/light/hot"
+	"github.com/pkg/errors"
 )
 
 //go:generate minimock -i github.com/insolar/insolar/ledger/light/executor.StateIniter -o ./ -s _mock.go
 
 type StateIniter interface {
 	PrepareState(ctx context.Context, pulse insolar.PulseNumber) error
+}
+
+// NewStateIniter creates StateIniterDefault with all required components.
+func NewStateIniter(
+	jetModifier jet.Modifier,
+	jetReleaser hot.JetReleaser,
+	drops drop.Modifier,
+	coordinator jet.Coordinator,
+	sender bus.Sender,
+) *StateIniterDefault {
+	return &StateIniterDefault{
+		jetModifier: jetModifier,
+		jetReleaser: jetReleaser,
+		drops:       drops,
+		coordinator: coordinator,
+		sender:      sender,
+	}
+}
+
+// StateIniterDefault implements StateIniter.
+type StateIniterDefault struct {
+	jetModifier jet.Modifier
+	jetReleaser hot.JetReleaser
+	drops       drop.Modifier
+	coordinator jet.Coordinator
+	sender      bus.Sender
+}
+
+func (s *StateIniterDefault) PrepareState(ctx context.Context, pulse insolar.PulseNumber) error {
+	if pulse < insolar.FirstPulseNumber {
+		return errors.Errorf("invalid pulse %s for light state initialization ", pulse)
+	}
+
+	heavyRef, err := s.coordinator.Heavy(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get heavy ref from coordinator")
+	}
+
+	msg, err := payload.NewMessage(&payload.GetInitialState{
+		Pulse: pulse,
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to create GetInitialState message")
+	}
+
+	reps, done := s.sender.SendTarget(ctx, msg, *heavyRef)
+	defer done()
+
+	res, ok := <-reps
+	if !ok {
+		return errors.New("no reply for light state initialization")
+	}
+
+	pl, err := payload.UnmarshalFromMeta(res.Payload)
+	if err != nil {
+		return errors.Wrap(err, "failed to unmarshal reply")
+	}
+	initialState, ok := pl.(*payload.InitialState)
+	if !ok {
+		return fmt.Errorf("unexpected reply %T", pl)
+	}
+
+	jets := initialState.JetIDs
+	err = s.jetModifier.Update(ctx, pulse, true, jets...)
+	if err != nil {
+		return errors.Wrap(err, "failed to update jets")
+	}
+
+	for _, jetID := range jets {
+		err = s.jetReleaser.Unlock(ctx, insolar.ID(jetID))
+		if err != nil {
+			return errors.Wrap(err, "failed to unlock jet")
+		}
+	}
+
+	for _, buf := range initialState.Drops {
+		d, err := drop.Decode(buf)
+		if err != nil {
+			return errors.Wrap(err, "failed to decode drop")
+		}
+		err = s.drops.Set(ctx, *d)
+		if err != nil {
+			return errors.Wrap(err, "failed to set drop")
+		}
+	}
+
+	return nil
 }
