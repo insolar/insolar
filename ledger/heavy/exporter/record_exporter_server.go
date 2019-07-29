6e5a70a00f445ea6dc6990d4a805a9cc8658a016
egorikas <egorgrishechko@gmail.com>
2019-07-29 14:40:13 +0300

    INS-2960 iterator completed

--- ledger/heavy/exporter/record_exporter_server.go
+++ ledger/heavy/exporter/record_exporter_server.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/insolar/insolar/insolar"
 	"github.com/insolar/insolar/insolar/pulse"
-	"github.com/insolar/insolar/instrumentation/inslogger"
 	"github.com/insolar/insolar/ledger/heavy/executor"
 	"github.com/insolar/insolar/ledger/object"
 )
@@ -31,6 +30,50 @@ type RecordServer struct {
 	pulseCalculator pulse.Calculator
 	recordIndex     object.RecordPositionAccessor
 	recordAccessor  object.RecordAccessor
+	jetKeeper       executor.JetKeeper
+}
+
+func NewRecordServer(
+	pulseCalculator pulse.Calculator,
+	recordIndex object.RecordPositionAccessor,
+	recordAccessor object.RecordAccessor,
+	jetKeeper executor.JetKeeper,
+) *RecordServer {
+	return &RecordServer{pulseCalculator: pulseCalculator, recordIndex: recordIndex, recordAccessor: recordAccessor, jetKeeper: jetKeeper}
+}
+
+func (r *RecordServer) Export(ctx context.Context, getRecords *GetRecords) (*Records, error) {
+	if getRecords.Count == 0 {
+		return nil, errors.New("count can't be 0")
+	}
+
+	if getRecords.PulseNumber != 0 {
+		topPulse := r.jetKeeper.TopSyncPulse()
+		if topPulse < getRecords.PulseNumber {
+			return nil, errors.New("trying to get a non-finalized pulse data")
+		}
+	}
+
+	iter := newRecordIterator(
+		getRecords.PulseNumber,
+		getRecords.RecordNumber,
+		getRecords.Count,
+		r.recordIndex,
+		r.recordAccessor,
+		r.jetKeeper,
+		r.pulseCalculator,
+	)
+
+	result := &Records{}
+	for iter.HasNext(ctx) {
+		record, err := iter.Next(ctx)
+		if err != nil {
+			return nil, err
+		}
+		result.Records = append(result.Records, *record)
+	}
+
+	return result, nil
 }
 
 type recordIterator struct {
@@ -46,11 +89,23 @@ type recordIterator struct {
 	pulseCalculator pulse.Calculator
 }
 
-func newRecordIterator(pn insolar.PulseNumber, lastPosition uint32, takeCount uint32) *recordIterator {
+func newRecordIterator(
+	pn insolar.PulseNumber,
+	lastPosition uint32,
+	takeCount uint32,
+	recordIndex object.RecordPositionAccessor,
+	recordAccessor object.RecordAccessor,
+	jetKeeper executor.JetKeeper,
+	pulseCalculator pulse.Calculator,
+) *recordIterator {
 	return &recordIterator{
 		needToRead:      takeCount,
 		currentPosition: lastPosition,
 		currentPulse:    pn,
+		recordIndex:     recordIndex,
+		recordAccessor:  recordAccessor,
+		jetKeeper:       jetKeeper,
+		pulseCalculator: pulseCalculator,
 	}
 }
 
@@ -125,56 +180,4 @@ func (r *recordIterator) setNextPulse(ctx context.Context) error {
 	r.currentPosition = 1
 
 	return nil
-}
-
-func (r *RecordServer) Export(ctx context.Context, getRecords *GetRecords) (*Records, error) {
-	if getRecords.Count == 0 {
-		return nil, errors.New("count can't be 0")
-	}
-
-	if getRecords.PulseNumber == 0 {
-		return r.fetchSince(ctx, insolar.FirstPulseNumber, getRecords.RecordNumber, getRecords.Count)
-	}
-}
-
-func (r *RecordServer) fetchSince(ctx context.Context, pn insolar.PulseNumber, lastPosition uint32, takeCount uint32) (*Records, error) {
-	var ids []object.OrderedID
-	var err error
-	currentPN := pn
-
-	for uint32(len(ids)) < takeCount {
-		takeDelta := takeCount - uint32(len(ids))
-
-		ids, err = r.recordIndex.IDs(currentPN, lastPosition, takeDelta)
-		if err != nil {
-			return nil, err
-		}
-
-		if uint32(len(ids)) != takeCount {
-			nextPulse, err := r.pulseCalculator.Forwards(ctx, currentPN, 1)
-			if err != nil {
-				inslogger.FromContext(ctx).Error(err)
-				return r.loadRecords(ctx, ids)
-			}
-		}
-
-	}
-}
-
-func (r *RecordServer) loadRecords(ctx context.Context, ids []object.OrderedID) (*Records, error) {
-	result := &Records{}
-	for _, id := range ids {
-		rec, err := r.recordAccessor.ForID(ctx, id.ID)
-		if err != nil {
-			return nil, err
-		}
-		result.Records = append(result.Records, Record{
-			PulseNumber:  id.ID.Pulse(),
-			RecordNumber: id.Position,
-			RecordID:     id.ID,
-			Record:       rec,
-		})
-	}
-
-	return result, nil
 }
