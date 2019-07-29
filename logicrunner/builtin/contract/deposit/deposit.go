766e1d650e566144ff9614d28ffa04818be74b6d
egor <egor.pol9kov@gmail.com>
2019-07-29 14:57:36 +0300

    WLT-695: added safemath to deposit

--- logicrunner/builtin/contract/deposit/deposit.go
+++ logicrunner/builtin/contract/deposit/deposit.go
@@ -21,7 +21,7 @@ import (
 	"math/big"
 
 	"github.com/insolar/insolar/insolar"
-	"github.com/insolar/insolar/logicrunner/builtin/contract/wallet/safemath"
+	"github.com/insolar/insolar/logicrunner/builtin/contract/deposit/safemath"
 	"github.com/insolar/insolar/logicrunner/builtin/proxy/wallet"
 
 	"github.com/insolar/insolar/logicrunner/goplugin/foundation"
