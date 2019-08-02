ce1ae61935a9d7b8c74596dfd5b143da65a9d9a7
Tsovak Sahakyan <tsovak36@gmail.com>
2019-08-02 11:34:41 +0300

    [INS-3081] use test data

--- functest/logicrunner_test.go
+++ functest/logicrunner_test.go
@@ -21,6 +21,7 @@ package functest
 import (
 	"fmt"
 	"github.com/insolar/insolar/api"
+	"io/ioutil"
 	"strconv"
 	"sync"
 	"testing"
@@ -1257,52 +1258,6 @@ func (c *One) Get() (int, error) {
 	require.Equal(t, float64(12), result.ExtractedReply)
 }
 
-var contractOneCode = `
-package main
-
-import (
-	"github.com/insolar/insolar/logicrunner/builtin/foundation"
-	"time"
-)
-
-type One struct {
-	foundation.BaseContract
-	Number int
-}
-
-func New() (*One, error) {
-	return &One{Number: 0}, nil
-}
-
-func NewWithNumber(num int) (*One, error) {
-	return &One{Number: num}, nil
-}
-
-var INSATTR_GetAndIncrement_API = true
-func (c *One) GetAndIncrement() (int, error) {
-	time.Sleep(200 * time.Millisecond)
-	c.Number++
-	return c.Number, nil
-}
-
-var INSATTR_GetAndDecrement_API = true
-func (c *One) GetAndDecrement() (int, error) {
-	time.Sleep(200 * time.Millisecond)
-	c.Number--
-	return c.Number, nil
-}
-
-var INSATTR_Get_API = true
-func (c *One) Get() (int, error) {
-	return c.Number, nil
-}
-
-var INSATTR_DoNothing_API = true
-func (r *One) DoNothing() (error) {
-	return nil
-}
-`
-
 func TestMultiplyNoWaitCall(t *testing.T) {
 	var counter = strconv.Itoa(time.Now().Nanosecond())
 
@@ -1356,6 +1311,9 @@ func (r *Two) DoNothing() (error) {
 }
 
 `
+	data, err := ioutil.ReadFile("test-data/simple_contract_with_sleep.go")
+	require.Nil(t, err)
+	var contractOneCode = string(data)
 
 	contractOneRef := uploadContract(t, "first_contract"+counter, contractOneCode)
 	firstObjRef := callConstructor(t, contractOneRef, "NewWithNumber", 100)
@@ -1400,7 +1358,11 @@ func (r *Two) NoWaitGet(OneRef insolar.Reference) (int, error) {
 	return c.Get()
 }
 `
+	data, err := ioutil.ReadFile("test-data/simple_contract_with_sleep.go")
+	require.Nil(t, err)
+	var contractOneCode = string(data)
 	var n = 100
+
 	contractOneRef := uploadContract(t, "first_contract"+counter, contractOneCode)
 	firstObjRef := callConstructor(t, contractOneRef, "NewWithNumber", n)
 
