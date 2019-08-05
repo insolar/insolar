e804f2c26c46571198c1db177780e10e1bdfec4f
Tsovak Sahakyan <tsovak36@gmail.com>
2019-08-05 14:46:48 +0300

    [INS-3081] move code from file to code

--- functest/logicrunner_test.go
+++ functest/logicrunner_test.go
@@ -22,7 +22,6 @@ import (
 	"fmt"
 	"github.com/insolar/insolar/api"
 	"github.com/insolar/insolar/insolar/utils"
-	"io/ioutil"
 	"sync"
 	"testing"
 	"time"
@@ -1259,6 +1258,56 @@ func (c *One) Get() (int, error) {
 }
 
 func TestMultiplyNoWaitCall(t *testing.T) {
+	var contractOneCode = `
+package main
+
+import (
+	"github.com/insolar/insolar/logicrunner/builtin/foundation"
+	"time"
+)
+
+type One struct {
+	foundation.BaseContract
+	Number int
+}
+
+func New() (*One, error) {
+	return &One{Number: 0}, nil
+}
+
+func NewWithNumber(num int) (*One, error) {
+	return &One{Number: num}, nil
+}
+
+var INSATTR_GetAndIncrement_API = true
+
+func (c *One) GetAndIncrement() (int, error) {
+	time.Sleep(200 * time.Millisecond)
+	c.Number++
+	return c.Number, nil
+}
+
+var INSATTR_GetAndDecrement_API = true
+
+func (c *One) GetAndDecrement() (int, error) {
+	time.Sleep(200 * time.Millisecond)
+	c.Number--
+	return c.Number, nil
+}
+
+var INSATTR_Get_API = true
+
+func (c *One) Get() (int, error) {
+	return c.Number, nil
+}
+
+var INSATTR_DoNothing_API = true
+
+func (r *One) DoNothing() error {
+	return nil
+}
+`
+
 	var contractTwoCode = `
 package main
 
@@ -1309,10 +1358,6 @@ func (r *Two) DoNothing() (error) {
 }
 
 `
-	data, err := ioutil.ReadFile("test-data/simple_contract_with_sleep.txt")
-	require.Nil(t, err)
-	var contractOneCode = string(data)
-
 	contractOneRef := uploadContractOnce(t, "first_contract", contractOneCode)
 	firstObjRef := callConstructor(t, contractOneRef, "NewWithNumber", 100)
 	firstResult := callMethod(t, firstObjRef, "GetAndIncrement")
@@ -1327,6 +1372,55 @@ func (r *Two) DoNothing() (error) {
 }
 
 func TestMultiplyNoWaitCallsOnSomeObject(t *testing.T) {
+	var contractOneCode = `
+package main
+
+import (
+	"github.com/insolar/insolar/logicrunner/builtin/foundation"
+	"time"
+)
+
+type One struct {
+	foundation.BaseContract
+	Number int
+}
+
+func New() (*One, error) {
+	return &One{Number: 0}, nil
+}
+
+func NewWithNumber(num int) (*One, error) {
+	return &One{Number: num}, nil
+}
+
+var INSATTR_GetAndIncrement_API = true
+
+func (c *One) GetAndIncrement() (int, error) {
+	time.Sleep(200 * time.Millisecond)
+	c.Number++
+	return c.Number, nil
+}
+
+var INSATTR_GetAndDecrement_API = true
+
+func (c *One) GetAndDecrement() (int, error) {
+	time.Sleep(200 * time.Millisecond)
+	c.Number--
+	return c.Number, nil
+}
+
+var INSATTR_Get_API = true
+
+func (c *One) Get() (int, error) {
+	return c.Number, nil
+}
+
+var INSATTR_DoNothing_API = true
+
+func (r *One) DoNothing() error {
+	return nil
+}
+`
 	var contractTwoCode = `
 package main
 
@@ -1354,9 +1448,6 @@ func (r *Two) NoWaitGet(OneRef insolar.Reference) (int, error) {
 	return c.Get()
 }
 `
-	data, err := ioutil.ReadFile("test-data/simple_contract_with_sleep.txt")
-	require.Nil(t, err)
-	var contractOneCode = string(data)
 	var n = 100
 
 	contractOneRef := uploadContractOnce(t, "simple_contract_with_sleep", contractOneCode)
