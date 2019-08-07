e27890daf2205494284b1133bd9285859c4446e4
Andrey Romancev <andrey@romancev.com>
2019-08-07 17:35:12 +0300

    INS-2849: Remove GIL and old message bus from pulse manager.

--- ledger/light/integration/server_test.go
+++ ledger/light/integration/server_test.go
@@ -40,7 +40,6 @@ import (
 	"github.com/insolar/insolar/insolar/node"
 	"github.com/insolar/insolar/insolar/payload"
 	"github.com/insolar/insolar/insolar/pulse"
-	"github.com/insolar/insolar/insolar/reply"
 	"github.com/insolar/insolar/instrumentation/inslogger"
 	"github.com/insolar/insolar/keystore"
 	"github.com/insolar/insolar/ledger/drop"
@@ -185,12 +184,10 @@ func NewServer(
 
 	// Communication.
 	var (
-		Bus                        insolar.MessageBus
 		ServerBus, ClientBus       *bus.Bus
 		ServerPubSub, ClientPubSub message.PubSub
 	)
 	{
-		Bus = &stub{}
 		ServerPubSub = gochannel.NewGoChannel(gochannel.Config{}, logger)
 		ClientPubSub = gochannel.NewGoChannel(gochannel.Config{}, logger)
 		ServerBus = bus.NewBus(cfg.Bus, ServerPubSub, Pulses, Coordinator, CryptoScheme)
@@ -306,27 +303,19 @@ func NewServer(
 			},
 		)
 
-		pm := executor.NewPulseManager(
+		PulseManager = executor.NewPulseManager(
+			NodeNetwork,
+			FlowDispatcher,
+			Nodes,
+			Pulses,
+			Pulses,
+			hotWaitReleaser,
 			jetSplitter,
 			lthSyncer,
-			writeController,
 			hotSender,
+			writeController,
 			stateIniter,
 		)
-		pm.Bus = Bus
-		pm.NodeNet = NodeNetwork
-		pm.JetReleaser = hotWaitReleaser
-		pm.JetModifier = Jets
-		pm.NodeSetter = Nodes
-		pm.Nodes = Nodes
-		pm.PulseAccessor = Pulses
-		pm.PulseCalculator = Pulses
-		pm.PulseAppender = Pulses
-		pm.GIL = &stub{}
-		pm.NodeNet = NodeNetwork
-		pm.Dispatcher = FlowDispatcher
-
-		PulseManager = pm
 	}
 
 	// Start routers with handlers.
@@ -544,29 +533,4 @@ func (n *nodeNetMock) GetWorkingNodes() []insolar.NetworkNode {
 
 func (n *nodeNetMock) GetWorkingNodesByRole(role insolar.DynamicRole) []insolar.Reference {
 	panic("implement me")
-}
-
-type stub struct{}
-
-func (*stub) Send(context.Context, insolar.Message, *insolar.MessageSendOptions) (insolar.Reply, error) {
-	return &reply.OK{}, nil
-}
-
-func (*stub) Register(p insolar.MessageType, handler insolar.MessageHandler) error {
-	return nil
-}
-
-func (*stub) MustRegister(p insolar.MessageType, handler insolar.MessageHandler) {
-}
-
-func (*stub) OnPulse(context.Context, insolar.Pulse) error {
-	return nil
-}
-
-func (*stub) Acquire(ctx context.Context) {}
-
-func (*stub) Release(ctx context.Context) {}
-
-func (*stub) MoveSyncToActive(ctx context.Context, number insolar.PulseNumber) error {
-	return nil
 }
