6900f984d2e4fe5221601df65f255814d98dd35e
Dmitry Panchenko <dmitry.panchenko@insolar.io>
2019-08-07 17:56:36 +0300

    INS-3100: fix joiner bootstrap pulse

--- network/gateway/joiner_bootstrap.go
+++ network/gateway/joiner_bootstrap.go
@@ -60,6 +60,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/insolar/insolar/insolar"
+	"github.com/insolar/insolar/insolar/pulse"
 	"github.com/insolar/insolar/instrumentation/inslogger"
 	"github.com/insolar/insolar/network"
 	"github.com/insolar/insolar/network/hostnetwork/host"
@@ -75,24 +76,26 @@ type JoinerBootstrap struct {
 	*Base
 }
 
-func (g *JoinerBootstrap) Run(ctx context.Context, pulse insolar.Pulse) {
+func (g *JoinerBootstrap) Run(ctx context.Context, p insolar.Pulse) {
 	logger := inslogger.FromContext(ctx)
 	permit, err := g.authorize(ctx)
 	if err != nil {
 		logger.Error(err.Error())
-		g.Gatewayer.SwitchState(ctx, insolar.NoNetworkState, pulse)
+		g.Gatewayer.SwitchState(ctx, insolar.NoNetworkState, p)
 		return
 	}
 
-	resp, err := g.BootstrapRequester.Bootstrap(ctx, permit, *g.originCandidate, &pulse)
+	resp, err := g.BootstrapRequester.Bootstrap(ctx, permit, *g.originCandidate, &p)
 	if err != nil {
 		logger.Error(err.Error())
-		g.Gatewayer.SwitchState(ctx, insolar.NoNetworkState, pulse)
+		g.Gatewayer.SwitchState(ctx, insolar.NoNetworkState, p)
 		return
 	}
 
+	responsePulse := pulse.FromProto(&resp.Pulse)
+
 	g.bootstrapETA = time.Second * time.Duration(resp.ETASeconds)
-	g.Gatewayer.SwitchState(ctx, insolar.WaitConsensus, pulse)
+	g.Gatewayer.SwitchState(ctx, insolar.WaitConsensus, *responsePulse)
 }
 
 func (g *JoinerBootstrap) GetState() insolar.NetworkState {
