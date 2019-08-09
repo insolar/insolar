7ddc87b803e001413acd75bc18cd638d782187b3
Ivan Shibitov <ivan.shibitov@insolar.io>
2019-08-09 18:14:46 +0300

    INS-3039: addd test fot pulse finalization

--- ledger/heavy/executor/finalize_pulse.go
+++ ledger/heavy/executor/finalize_pulse.go
@@ -1,1 +1,76 @@
+///
+// Copyright 2019 Insolar Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///
+
 package executor
+
+import (
+	"context"
+
+	"github.com/insolar/insolar/insolar"
+	"github.com/insolar/insolar/insolar/pulse"
+	"github.com/insolar/insolar/instrumentation/inslogger"
+)
+
+// FinalizePulse starts backup process if needed
+func FinalizePulse(ctx context.Context, pulses pulse.Calculator, backuper BackupMaker, jetKeeper JetKeeper, newPulse insolar.PulseNumber) {
+	logger := inslogger.FromContext(ctx)
+	if !jetKeeper.HasAllJetConfirms(ctx, newPulse) {
+		logger.Debug("not all jets confirmed. Do nothing. Pulse: ", newPulse)
+		return
+	}
+
+	nextTop, err := pulses.Forwards(ctx, jetKeeper.TopSyncPulse(), 1)
+	if err != nil {
+		logger.Warn("Can't get next pulse for topSynk: ", jetKeeper.TopSyncPulse())
+		return
+	}
+
+	if !nextTop.PulseNumber.Equal(newPulse) {
+		logger.Infof("Try to finalize not sequential pulse. Skip it. newTop: %d, target: %d", nextTop.PulseNumber, newPulse)
+		return
+	}
+
+	go func() {
+		logger.Debug("FinalizePulse starts")
+		bkpError := backuper.MakeBackup(ctx, newPulse)
+		if bkpError != nil && bkpError != ErrAlreadyDone && bkpError != ErrBackupDisabled {
+			panic("Can't do backup: " + bkpError.Error())
+		}
+
+		if bkpError == ErrAlreadyDone {
+			logger.Info("Pulse already backuped: ", newPulse, bkpError)
+			return
+		}
+
+		err := jetKeeper.AddBackupConfirmation(ctx, newPulse)
+		if err != nil {
+			panic("Can't add backup confirmation: " + err.Error())
+		}
+
+		inslogger.FromContext(ctx).Infof("Pulse %d completely finalized ( drops + hots + backup )", newPulse)
+
+		nextTop, err := pulses.Forwards(ctx, jetKeeper.TopSyncPulse(), 1)
+		if err != nil && err != pulse.ErrNotFound {
+			panic("pulses.Forwards topSynk: " + jetKeeper.TopSyncPulse().String())
+		}
+		if err == pulse.ErrNotFound {
+			logger.Info("Stop propagating of backups")
+			return
+		}
+		logger.Info("Propagating finalization to next pulse: ", nextTop.PulseNumber)
+		go FinalizePulse(ctx, pulses, backuper, jetKeeper, nextTop.PulseNumber)
+	}()
+}
--- ledger/heavy/executor/finalize_pulse_test.go
+++ ledger/heavy/executor/finalize_pulse_test.go
@@ -1,1 +1,107 @@
-package executor
+///
+// Copyright 2019 Insolar Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///
+
+package executor_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gojuno/minimock"
+	"github.com/insolar/insolar/insolar"
+	"github.com/insolar/insolar/instrumentation/inslogger"
+	"github.com/insolar/insolar/ledger/heavy/executor"
+	"github.com/insolar/insolar/testutils/network"
+	"github.com/pkg/errors"
+)
+
+func TestFinalizePulse_HappyPath(t *testing.T) {
+	ctx := inslogger.TestContext(t)
+	mc := minimock.NewController(t)
+	defer mc.Finish()
+	defer mc.Wait(time.Minute)
+
+	testPulse := insolar.PulseNumber(insolar.FirstPulseNumber)
+	targetPulse := testPulse + 1
+
+	pc := network.NewPulseCalculatorMock(mc)
+	pc.ForwardsMock.Return(insolar.Pulse{PulseNumber: targetPulse}, nil)
+
+	bkp := executor.NewBackupMakerMock(mc)
+	bkp.MakeBackupMock.Return(nil)
+
+	jk := executor.NewJetKeeperMock(mc)
+	jk.HasAllJetConfirmsMock.Return(true)
+	jk.TopSyncPulseMock.Return(testPulse)
+	jk.AddBackupConfirmationMock.Return(nil)
+
+	executor.FinalizePulse(ctx, pc, bkp, jk, targetPulse)
+}
+
+func TestFinalizePulse_JetIsNotConfirmed(t *testing.T) {
+	ctx := inslogger.TestContext(t)
+	mc := minimock.NewController(t)
+	defer mc.Finish()
+	defer mc.Wait(time.Minute)
+
+	testPulse := insolar.PulseNumber(insolar.FirstPulseNumber)
+
+	jk := executor.NewJetKeeperMock(mc)
+	jk.HasAllJetConfirmsMock.Return(false)
+
+	executor.FinalizePulse(ctx, nil, nil, jk, testPulse)
+}
+
+func TestFinalizePulse_CantGteNextPulse(t *testing.T) {
+	ctx := inslogger.TestContext(t)
+	mc := minimock.NewController(t)
+	defer mc.Finish()
+	defer mc.Wait(time.Minute)
+
+	testPulse := insolar.PulseNumber(insolar.FirstPulseNumber)
+
+	jk := executor.NewJetKeeperMock(mc)
+	jk.HasAllJetConfirmsMock.Return(true)
+	jk.TopSyncPulseMock.Return(testPulse)
+
+	pc := network.NewPulseCalculatorMock(mc)
+	pc.ForwardsMock.Return(insolar.Pulse{}, errors.New("Test"))
+
+	executor.FinalizePulse(ctx, pc, nil, jk, testPulse)
+}
+
+func TestFinalizePulse_BackupError(t *testing.T) {
+	ctx := inslogger.TestContext(t)
+	mc := minimock.NewController(t)
+	defer mc.Finish()
+	defer mc.Wait(time.Minute)
+
+	testPulse := insolar.PulseNumber(insolar.FirstPulseNumber)
+	targetPulse := testPulse + 1
+
+	jk := executor.NewJetKeeperMock(mc)
+	jk.HasAllJetConfirmsMock.Return(true)
+	jk.TopSyncPulseMock.Return(targetPulse)
+
+	pc := network.NewPulseCalculatorMock(mc)
+	pc.ForwardsMock.Return(insolar.Pulse{PulseNumber: targetPulse}, nil)
+
+	bkp := executor.NewBackupMakerMock(mc)
+	bkp.MakeBackupMock.Return(executor.ErrAlreadyDone)
+
+	executor.FinalizePulse(ctx, pc, bkp, jk, targetPulse)
+
+}
