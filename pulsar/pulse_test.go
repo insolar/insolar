d60eed044cc15f62d0de962f86fe6345bb622974
egorikas <egorgrishechko@gmail.com>
2019-08-13 10:48:16 +0300

    revert TestNewPulse

--- pulsar/pulse_test.go
+++ pulsar/pulse_test.go
@@ -21,16 +21,17 @@ import (
 
 	"github.com/insolar/insolar/configuration"
 	"github.com/insolar/insolar/insolar"
-	"github.com/insolar/insolar/pulsar/entropygenerator"
+	"github.com/insolar/insolar/pulsar/pulsartestutils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewPulse(t *testing.T) {
-	generator := &entropygenerator.StandardEntropyGenerator{}
+	generator := &pulsartestutils.MockEntropyGenerator{}
 	previousPulse := insolar.PulseNumber(876)
 	expectedPulse := previousPulse + insolar.PulseNumber(configuration.NewPulsar().NumberDelta)
 
 	result := NewPulse(configuration.NewPulsar().NumberDelta, previousPulse, generator)
 
+	require.Equal(t, result.Entropy[:], pulsartestutils.MockEntropy[:])
 	require.Equal(t, result.PulseNumber, insolar.PulseNumber(expectedPulse))
 }
