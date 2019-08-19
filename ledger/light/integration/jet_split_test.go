8778337b9879814e0ae1db221cda2b1e907c4b01
SergeyRadist <14805670+SergeyRadist@users.noreply.github.com>
2019-08-19 16:45:25 +0300

    #INS-3044 LightInitialState message handling test

--- ledger/light/integration/jet_split_test.go
+++ ledger/light/integration/jet_split_test.go
@@ -25,7 +25,9 @@ import (
 	"github.com/insolar/insolar/insolar"
 	"github.com/insolar/insolar/insolar/jet"
 	"github.com/insolar/insolar/insolar/payload"
+	"github.com/insolar/insolar/insolar/pulse"
 	"github.com/insolar/insolar/instrumentation/inslogger"
+	"github.com/insolar/insolar/ledger/drop"
 )
 
 func Test_JetSplitEveryPulse(t *testing.T) {
@@ -243,6 +245,165 @@ func Test_JetSplitsWhenOverflows(t *testing.T) {
 			// Starting to send messages
 			if i >= splitOnPulse {
 				splittingJets = sendMessages(jetTree)
+			}
+
+			hotObjectsReceived := make(map[insolar.JetID]struct{})
+			hotObjectsConfirmReceived := make(map[insolar.JetID]struct{})
+
+			// collecting HO and HCO
+			for range expectedJets {
+				hotObjectsReceived[<-hotObjects] = struct{}{}
+				hotObjectsConfirmReceived[<-hotObjectConfirm] = struct{}{}
+			}
+
+			for _, expectedJetId := range expectedJets {
+
+				_, ok := hotObjectsReceived[expectedJetId]
+				require.True(t, ok, "No expected jetId %s in hotObjectsReceived", expectedJetId.DebugString())
+
+				_, ok = hotObjectsConfirmReceived[expectedJetId]
+				require.True(t, ok, "No expected jetId %s in hotObjectsConfirmReceived", expectedJetId.DebugString())
+			}
+
+			// collecting Replication
+			replicationObjectsReceived := make(map[insolar.JetID]struct{})
+			for range previousPulseJets {
+				replicationObjectsReceived[<-replication] = struct{}{}
+			}
+
+			for _, expectedJetId := range previousPulseJets {
+				_, ok := replicationObjectsReceived[expectedJetId]
+				require.True(t, ok, "No expected jetId %s in replicationObjectsReceived", expectedJetId.DebugString())
+			}
+
+		}
+	}
+}
+
+func Test_LightStartsFromInitialState(t *testing.T) {
+	t.Parallel()
+
+	var hotObjects = make(chan insolar.JetID)
+	var replication = make(chan insolar.JetID)
+	var hotObjectConfirm = make(chan insolar.JetID)
+
+	var pulsesQuantity = 10
+	var recordsOnPulse = 30
+	var splitOnPulse = 3
+	var initialSplits = 3
+	var jetTree = jet.NewTree(true)
+
+	ctx := inslogger.TestContext(t)
+	cfg := DefaultLightConfig()
+	cfg.Ledger.JetSplit.DepthLimit = 5
+	cfg.Ledger.JetSplit.ThresholdOverflowCount = 0
+	cfg.Ledger.JetSplit.ThresholdRecordsCount = 2
+
+	splitJetTree := func(jets []insolar.JetID, jetTree *jet.Tree, depthLimit uint8) []insolar.JetID {
+		for _, jetID := range jets {
+			if jetID.Depth() < depthLimit {
+				_, _, _ = jetTree.Split(jetID)
+			}
+		}
+		return jetTree.LeafIDs()
+	}
+
+	createDrops := func(jets []insolar.JetID) [][]byte {
+		var drops [][]byte
+		for _, jetID := range jets {
+			drops = append(drops, drop.MustEncode(&drop.Drop{JetID: jetID, Pulse: insolar.FirstPulseNumber}))
+		}
+		return drops
+	}
+
+	// Creating initial jet tree.
+	initialJets := []insolar.JetID{insolar.ZeroJetID}
+	for d := 0; d <= initialSplits; d++ {
+		initialJets = splitJetTree(initialJets, jetTree, cfg.Ledger.JetSplit.DepthLimit)
+	}
+
+	s, err := NewServer(ctx, cfg, func(meta payload.Meta, pl payload.Payload) []payload.Payload {
+		switch p := pl.(type) {
+		case *payload.Replication:
+			replication <- p.JetID
+
+		case *payload.HotObjects:
+			hotObjects <- p.JetID
+
+		case *payload.GotHotConfirmation:
+			hotObjectConfirm <- p.JetID
+		}
+
+		if meta.Receiver == NodeHeavy() {
+			switch pl.(type) {
+			case *payload.Replication, *payload.GotHotConfirmation:
+				return nil
+			case *payload.GetIndex:
+				return []payload.Payload{&payload.Error{Code: payload.CodeNotFound}}
+			case *payload.GetLightInitialState:
+				return []payload.Payload{
+					&payload.LightInitialState{
+						NetworkStart: true,
+						JetIDs:       initialJets,
+						Pulse: pulse.PulseProto{
+							PulseNumber: insolar.FirstPulseNumber,
+						},
+						Drops: createDrops(initialJets),
+					},
+				}
+			}
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	defer s.Stop()
+
+	calculateExpectedJetsByTree := func(splittingJets map[insolar.JetID]int, jetTree *jet.Tree, depthLimit uint8, thresholdRecordsCount int) []insolar.JetID {
+
+		for jetID, val := range splittingJets {
+			if val >= thresholdRecordsCount && jetID.Depth() < depthLimit {
+				_, _, _ = jetTree.Split(jetID)
+			}
+		}
+		return jetTree.LeafIDs()
+	}
+
+	sendMessages := func(jetTree *jet.Tree) map[insolar.JetID]int {
+		updatedJets := make(map[insolar.JetID]int)
+		// Save code.
+		for i := 0; i < recordsOnPulse; i++ {
+			{
+				p, _ := CallSetCode(ctx, s)
+				RequireNotError(p)
+				jetID, _ := jetTree.Find(p.(*payload.ID).ID)
+				updatedJets[jetID]++
+			}
+		}
+		return updatedJets
+	}
+
+	// First pulse goes in storage then interrupts.
+	s.SetPulse(ctx)
+
+	{
+		expectedJets := initialJets
+		updatedJets := make(map[insolar.JetID]int)
+
+		for i := 0; i < pulsesQuantity; i++ {
+
+			s.SetPulse(ctx)
+
+			// Saving previous for Replication check
+			previousPulseJets := expectedJets
+
+			// Starting to split test jet tree
+			if i > splitOnPulse {
+				expectedJets = calculateExpectedJetsByTree(updatedJets, jetTree, cfg.Ledger.JetSplit.DepthLimit, cfg.Ledger.JetSplit.ThresholdRecordsCount)
+			}
+
+			// Starting to send messages
+			if i >= splitOnPulse {
+				updatedJets = sendMessages(jetTree)
 			}
 
 			hotObjectsReceived := make(map[insolar.JetID]struct{})
