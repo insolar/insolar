72ecbdc0629f9208a40e1c4900e7c831128136d3
maximshalavin <shalavinms@gmail.com>
2019-09-03 16:59:00 +0300

    WLT-951: merge conflict

--- logicrunner/builtin/contract/migrationdaemon/migrationdaemon.go
+++ logicrunner/builtin/contract/migrationdaemon/migrationdaemon.go
@@ -119,7 +119,7 @@ func (md *MigrationDaemon) depositMigration(txHash string, migrationAddress stri
 	// If deposit doesn't exist - create new deposit
 	if !isFound {
 		vestingParams, _ := migrationAdminContract.GetDepositParameters()
-		dHolder := deposit.New(caller, txHash, amount.String(), vestingParams)
+		dHolder := deposit.New(caller, txHash, amount.String(), vestingParams.Lockup, vestingParams.Vesting, vestingParams.VestingStep)
 		txDeposit, err := dHolder.AsChild(*tokenHolderRef)
 		if err != nil {
 			return nil, fmt.Errorf("failed to save as child: %s", err.Error())
