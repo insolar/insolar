4abaee70934ec122cc68678de20bf9278d631f2f
egorikas <egorgrishechko@gmail.com>
2019-09-04 19:21:55 +0300

    INS-3448 add last generated pulse metric

--- pulsar/pulsar.go
+++ pulsar/pulsar.go
@@ -139,7 +139,7 @@ func (p *Pulsar) Send(ctx context.Context, pulseNumber insolar.PulseNumber) erro
 	p.lastPNMutex.Unlock()
 	logger.Infof("set latest pulse: %v", pulseForSending.PulseNumber)
 
-	stats.Record(ctx, statPulseGenerated.M(1))
+	stats.Record(ctx, statPulseGenerated.M(1), statCurrentPulse.M(int64(pulseNumber)))
 	return nil
 }
 
