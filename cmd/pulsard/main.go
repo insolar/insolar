1b6fdb161a2617df57375a37069cabb43ac450b0
egorikas <egorgrishechko@gmail.com>
2019-09-04 11:50:15 +0300

    INS-3443 tunning of pulsar's config. tuning of config-generator tool

--- cmd/pulsard/main.go
+++ cmd/pulsard/main.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/spf13/cobra"
 	jww "github.com/spf13/jwalterweatherman"
+	"github.com/spf13/viper"
 
 	"github.com/insolar/insolar/component"
 	"github.com/insolar/insolar/configuration"
@@ -68,24 +69,29 @@ func main() {
 	params := parseInputParams()
 
 	jww.SetStdoutThreshold(jww.LevelDebug)
-	cfgHolder := configuration.NewHolder()
 	var err error
+
+	vp := viper.New()
+	pCfg := configuration.NewPulsarConfiguration()
 	if len(params.configPath) != 0 {
-		err = cfgHolder.LoadFromFile(params.configPath)
-	} else {
-		err = cfgHolder.Load()
+		vp.SetConfigFile(params.configPath)
 	}
+	err = vp.ReadInConfig()
+	if err != nil {
+		log.Warn("failed to load configuration from file: ", err.Error())
+	}
+	err = vp.Unmarshal(&pCfg)
 	if err != nil {
 		log.Warn("failed to load configuration from file: ", err.Error())
 	}
 
 	traceID := utils.RandTraceID()
 	ctx := context.Background()
-	ctx, inslog := inslogger.InitNodeLogger(ctx, cfgHolder.Configuration.Log, traceID, "", "pulsar")
+	ctx, inslog := inslogger.InitNodeLogger(ctx, pCfg.Log, traceID, "", "pulsar")
 
 	jaegerflush := func() {}
 	if params.traceEnabled {
-		jconf := cfgHolder.Configuration.Tracer.Jaeger
+		jconf := pCfg.Tracer.Jaeger
 		log.Infof("Tracing enabled. Agent endpoint: '%s', collector endpoint: '%s'", jconf.AgentEndpoint, jconf.CollectorEndpoint)
 		jaegerflush = instracer.ShouldRegisterJaeger(
 			ctx,
@@ -97,8 +103,8 @@ func main() {
 	}
 	defer jaegerflush()
 
-	cm, server := initPulsar(ctx, cfgHolder.Configuration)
-	pulseTicker := runPulsar(ctx, server, cfgHolder.Configuration.Pulsar)
+	cm, server := initPulsar(ctx, pCfg)
+	pulseTicker := runPulsar(ctx, server, pCfg.Pulsar)
 
 	defer func() {
 		pulseTicker.Stop()
@@ -115,7 +121,7 @@ func main() {
 	<-gracefulStop
 }
 
-func initPulsar(ctx context.Context, cfg configuration.Configuration) (*component.Manager, *pulsar.Pulsar) {
+func initPulsar(ctx context.Context, cfg configuration.PulsarConfiguration) (*component.Manager, *pulsar.Pulsar) {
 	fmt.Println("Version: ", version.GetFullVersion())
 	fmt.Println("Starts with configuration:\n", configuration.ToString(cfg))
 
