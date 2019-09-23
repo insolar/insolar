e3bbb1f17f668ff9d7fec821f6b9157998cee88f
Andrey Romancev <andrey@romancev.com>
2019-09-23 19:07:04 +0300

    NOISSUE: Change ID string method to use base64.

--- reference/local.go
+++ reference/local.go
@@ -174,7 +174,7 @@ func (p *byteWriter) isFull() bool {
 
 // String implements stringer on ID and returns base58 encoded value
 func (v Local) String() string {
-	repr, err := DefaultEncoder().EncodeRecord(&v)
+	repr, err := NewBase64Encoder(0).EncodeRecord(&v)
 	if err != nil {
 		return ""
 	}
