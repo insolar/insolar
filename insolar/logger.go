7cf52b26c8e0cb1ab3d3910fb4e3d2ab2fb1592f
Red Cyrax <cyraxred@users.noreply.github.com>
2019-09-25 13:58:49 +0300

    Comments and readability fixes.

--- insolar/logger.go
+++ insolar/logger.go
@@ -41,6 +41,194 @@ const (
 
 const LogLevelCount = int(maxLogLevel)
 
+type LogFormat uint8
+
+const (
+	TextFormat LogFormat = iota
+	JSONFormat
+)
+const DefaultLogFormat = TextFormat
+
+type LogOutput uint8
+
+const (
+	StdErrOutput LogOutput = iota
+	SysLogOutput
+	//JournalDOutput
+)
+const DefaultLogOutput = StdErrOutput
+
+const DefaultOutputParallelLimit = 5
+
+//go:generate minimock -i github.com/insolar/insolar/insolar.Logger -o ./ -s _mock.go -g
+
+// Logger is the interface for loggers used in the Insolar components.
+type Logger interface {
+	// Debug logs a message at level Debug.
+	Debug(...interface{})
+	// Debugf formatted logs a message at level Debug.
+	Debugf(string, ...interface{})
+
+	// Info logs a message at level Info.
+	Info(...interface{})
+	// Infof formatted logs a message at level Info.
+	Infof(string, ...interface{})
+
+	// Warn logs a message at level Warn.
+	Warn(...interface{})
+	// Warnf logs a message at level Warn.
+	Warnf(string, ...interface{})
+
+	// Error logs a message at level Error.
+	Error(...interface{})
+	// Errorf logs a message at level Error.
+	Errorf(string, ...interface{})
+
+	// Fatal logs a message at level Fatal and than call os.exit().
+	Fatal(...interface{})
+	// Fatalf formatted logs a message at level Fatal and than call os.exit().
+	Fatalf(string, ...interface{})
+
+	// Panic logs a message at level Panic and than call panic().
+	Panic(...interface{})
+	// Panicf formatted logs a message at level Panic and than call panic().
+	Panicf(string, ...interface{})
+
+	// Event logs a message with the given level.
+	Event(level LogLevel, args ...interface{})
+	// Eventf formats and logs a message with the given level.
+	Eventf(level LogLevel, fmt string, args ...interface{})
+
+	// Is() returns true when a message of the given level will get to output. Considers the global log filter.
+	Is(level LogLevel) bool
+
+	// WithFields return copy of Logger with the given fields added.
+	WithFields(map[string]interface{}) Logger
+	// WithField return copy of Logger with the given field added.
+	WithField(string, interface{}) Logger
+
+	// Provides a builder based on configuration of this logger.
+	Copy() LoggerBuilder
+	// Provides a copy of this logger with a filter set to lvl.
+	Level(lvl LogLevel) Logger
+
+	// DO NOT USE directly. Provides access to an embeddable methods of this logger.
+	Embeddable() EmbeddedLogger
+}
+
+type LoggerBuilder interface {
+
+	// Returns the current output
+	GetOutput() io.Writer
+	// Returns the current log level
+	GetLogLevel() LogLevel
+
+	// SetOutput sets the output destination for the logger.
+	WithOutput(w io.Writer) LoggerBuilder
+	// WithFormat sets logger output format.
+	WithFormat(format LogFormat) LoggerBuilder
+
+	// WithLevel sets log level. Cancels WithDynamicLevel()
+	WithLevel(level LogLevel) LoggerBuilder
+	// WithDynamicLevel sets a dynamic log level. Nil value will panic. Resets WithLevel()
+	WithDynamicLevel(level LogLevelGetter) LoggerBuilder
+
+	// Controls 'func' and 'caller' field computation. See also WithSkipFrameCount().
+	WithCaller(mode CallerFieldMode) LoggerBuilder
+	// WithSkipFrameCount changes skipFrameCount to the absolute value. But the value can be negative, and it is applied to a baseline. Value exceeding int8 will panic
+	WithSkipFrameCount(skipFrameCount int) LoggerBuilder
+
+	// Controls collection of metrics. Required flags are ADDED to the current flags. Include specify LogMetricsResetMode to replace flags.
+	WithMetrics(mode LogMetricsMode) LoggerBuilder
+	//Sets an custom recorder for metric collection.
+	WithMetricsRecorder(recorder LogMetricsRecorder) LoggerBuilder
+
+	// Creates a logger.
+	Build() (Logger, error)
+	// Creates a logger with no write delays.
+	BuildLowLatency() (Logger, error)
+}
+
+/*
+	This interface provides methods with -1 call levels.
+	DO NOT USE directly, otherwise WithCaller() functionality will be broken.
+*/
+type EmbeddedLogger interface {
+	// Event logs a message with the given level. DO NOT USE directly.
+	EmbeddedEvent(level LogLevel, args ...interface{})
+	// Eventf formats and logs a message with the given level. DO NOT USE directly.
+	EmbeddedEventf(level LogLevel, fmt string, args ...interface{})
+}
+
+type GlobalLogAdapterFactory interface {
+	CreateGlobalLogAdapter() GlobalLogAdapter
+}
+
+type GlobalLogAdapter interface {
+	SetGlobalLoggerFilter(level LogLevel)
+	GetGlobalLoggerFilter() LogLevel
+}
+
+type CallerFieldMode uint8
+
+const (
+	NoCallerField CallerFieldMode = iota
+	CallerField
+	CallerFieldWithFuncName
+)
+
+type LogMetricsRecorder interface {
+	RecordLogEvent(level LogLevel)
+	RecordLogWrite(level LogLevel)
+	RecordLogDelay(level LogLevel, d time.Duration)
+}
+
+type LogMetricsMode uint8
+
+const NoLogMetrics LogMetricsMode = 0
+const (
+	// Logger will report every event to metrics
+	LogMetricsEventCount LogMetricsMode = 1 << iota
+	// Logger will report to metrics a write duration (time since an event was created till it was directed to the output)
+	LogMetricsWriteDelayReport
+	// Logger will add a write duration field into to the output
+	LogMetricsWriteDelayField
+	// No effect on logger. Indicates that WithMetrics should replace the mode, instead of adding it.
+	LogMetricsResetMode
+)
+
+type LoggerOutputGetter interface {
+	GetLoggerOutput() LoggerOutput
+}
+
+type LoggerOutput interface {
+	LogLevelWriter
+	LowLatencyWrite(LogLevel, []byte) (int, error)
+	IsLowLatencySupported() bool
+	GetBareOutput() io.Writer
+}
+
+type LogLevelGetter interface {
+	GetLogLevel() LogLevel
+}
+
+type LogObjectWriter interface {
+	AddStructFields(s interface{})
+	AddFieldMap(map[string]interface{})
+	AddField(key string, v interface{})
+	AddRawJSON(key string, b []byte)
+}
+
+type LogObjectMarshaller interface {
+	MarshalLogObject(LogObjectWriter) string
+}
+
+type LogLevelWriter interface {
+	io.WriteCloser
+	LogLevelWrite(LogLevel, []byte) (int, error)
+	Flush() error
+}
+
 func (l LogLevel) Equal(other LogLevel) bool {
 	return l == other
 }
@@ -85,15 +273,6 @@ func ParseLevel(levelStr string) (LogLevel, error) {
 	return NoLevel, fmt.Errorf("unknown Level String: '%s', defaulting to NoLevel", levelStr)
 }
 
-type LogFormat uint8
-
-const (
-	TextFormat LogFormat = iota
-	JSONFormat
-)
-
-const DefaultLogFormat = TextFormat
-
 func ParseFormat(formatStr string, defValue LogFormat) (LogFormat, error) {
 	switch strings.ToLower(formatStr) {
 	case "", "default":
@@ -115,17 +294,6 @@ func (l LogFormat) String() string {
 	}
 	return string(l)
 }
-
-type LogOutput uint8
-
-const (
-	StdErrOutput LogOutput = iota
-	SysLogOutput
-	//JournalDOutput
-)
-
-const DefaultLogOutput = StdErrOutput
-const DefaultOutputParallelLimit = 5
 
 func ParseOutput(outputStr string, defValue LogOutput) (LogOutput, error) {
 	switch strings.ToLower(outputStr) {
@@ -151,172 +319,4 @@ func (l LogOutput) String() string {
 		//	return "journald"
 	}
 	return string(l)
-}
-
-type LogObjectWriter interface {
-	AddStructFields(s interface{})
-	AddFieldMap(map[string]interface{})
-	AddField(key string, v interface{})
-	AddRawJSON(key string, b []byte)
-}
-
-type LogObjectMarshaller interface {
-	MarshalLogObject(LogObjectWriter) string
-}
-
-type LogLevelGetter interface {
-	GetLogLevel() LogLevel
-}
-
-type LogLevelWriter interface {
-	io.Writer
-	io.Closer
-	LogLevelWrite(LogLevel, []byte) (int, error)
-	Flush() error
-}
-
-//go:generate minimock -i github.com/insolar/insolar/insolar.Logger -o ./ -s _mock.go -g
-
-// Logger is the interface for loggers used in the Insolar components.
-type Logger interface {
-	// Debug logs a message at level Debug.
-	Debug(...interface{})
-	// Debugf formatted logs a message at level Debug.
-	Debugf(string, ...interface{})
-
-	// Info logs a message at level Info.
-	Info(...interface{})
-	// Infof formatted logs a message at level Info.
-	Infof(string, ...interface{})
-
-	// Warn logs a message at level Warn.
-	Warn(...interface{})
-	// Warnf logs a message at level Warn.
-	Warnf(string, ...interface{})
-
-	// Error logs a message at level Error.
-	Error(...interface{})
-	// Errorf logs a message at level Error.
-	Errorf(string, ...interface{})
-
-	// Fatal logs a message at level Fatal and than call os.exit().
-	Fatal(...interface{})
-	// Fatalf formatted logs a message at level Fatal and than call os.exit().
-	Fatalf(string, ...interface{})
-
-	// Panic logs a message at level Panic and than call panic().
-	Panic(...interface{})
-	// Panicf formatted logs a message at level Panic and than call panic().
-	Panicf(string, ...interface{})
-
-	// Event logs a message with the given level
-	Event(level LogLevel, args ...interface{})
-	// Eventf formats and logs a message with the given level
-	Eventf(level LogLevel, fmt string, args ...interface{})
-
-	// Is() returns true when a message of the given level will get to output
-	Is(level LogLevel) bool
-
-	// WithFields return copy of Logger with predefined fields.
-	WithFields(map[string]interface{}) Logger
-	// WithField return copy of Logger with predefined single field.
-	WithField(string, interface{}) Logger
-
-	// Provides a builder based on configuration of this logger
-	Copy() LoggerBuilder
-	// Provides a copy of this logger with a filer set to lvl
-	Level(lvl LogLevel) Logger
-
-	// DO NOT USE directly. Provides access to an embeddable methods of this logger.
-	Embeddable() EmbeddedLogger
-}
-
-/*
-	This interface provides methods with -1 call levels.
-	DO NOT USE directly, otherwise WithCaller functionality will be broken.
-*/
-type EmbeddedLogger interface {
-	// Event logs a message with the given level
-	EmbeddedEvent(level LogLevel, args ...interface{})
-	// Eventf formats and logs a message with the given level
-	EmbeddedEventf(level LogLevel, fmt string, args ...interface{})
-}
-
-type GlobalLogAdapterFactory interface {
-	CreateGlobalLogAdapter() GlobalLogAdapter
-}
-
-type GlobalLogAdapter interface {
-	SetGlobalLoggerFilter(level LogLevel)
-	GetGlobalLoggerFilter() LogLevel
-}
-
-type CallerFieldMode uint8
-
-const (
-	NoCallerField CallerFieldMode = iota
-	CallerField
-	CallerFieldWithFuncName
-)
-
-type LogMetricsRecorder interface {
-	RecordLogEvent(level LogLevel)
-	RecordLogWrite(level LogLevel)
-	RecordLogDelay(level LogLevel, d time.Duration)
-}
-
-type LogMetricsMode uint8
-
-const NoLogMetrics LogMetricsMode = 0
-const (
-	// Logger will report every event to metrics
-	LogMetricsEventCount LogMetricsMode = 1 << iota
-	// Logger will report to metrics a write duration (time since an event was created till it was directed to the output)
-	LogMetricsWriteDelayReport
-	// Logger will add a write duration field into to the output
-	LogMetricsWriteDelayField
-	// No effect on logger. Indicates that WithMetrics should replace the mode, instead of adding it.
-	LogMetricsResetMode
-)
-
-type LoggerBuilder interface {
-
-	// Returns the current output
-	GetOutput() io.Writer
-	// Returns the current log level
-	GetLogLevel() LogLevel
-
-	// SetOutput sets the output destination for the logger.
-	WithOutput(w io.Writer) LoggerBuilder
-	// WithLevel sets log level. Disables WithDynamicLevel.
-	WithLevel(level LogLevel) LoggerBuilder
-	// WithDynamicLevel sets a dynamic log level. Nil value will panic
-	WithDynamicLevel(level LogLevelGetter) LoggerBuilder
-	// WithFormat sets logger output format
-	WithFormat(format LogFormat) LoggerBuilder
-	// Controls 'func' and 'caller' field computation.
-	WithCaller(mode CallerFieldMode) LoggerBuilder
-	// Controls collection of metrics. Required flags are ADDED to the current flags. Include specify LogMetricsResetMode to replace flags.
-	WithMetrics(mode LogMetricsMode) LoggerBuilder
-	//Sets an custom recorder for metric collection.
-	WithMetricsRecorder(recorder LogMetricsRecorder) LoggerBuilder
-
-	// WithSkipFrameCount changes skipFrameCount to the absolute value. But the value can be negative, and it is applied to a baseline. Value exceeding int8 will panic
-	WithSkipFrameCount(skipFrameCount int) LoggerBuilder
-
-	// Creates a logger
-	Build() (Logger, error)
-	// Creates a logger with no write delays
-	BuildLowLatency() (Logger, error)
-}
-
-type LoggerOutputGetter interface {
-	GetLoggerOutput() LoggerOutput
-}
-
-type LoggerOutput interface {
-	LogLevelWriter
-	LowLatencyWrite(LogLevel, []byte) (int, error)
-	IsLowLatencySupported() bool
-	GetBareOutput() io.Writer
 }
