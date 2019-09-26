f2fe93fed4e93e8714f40f2a238fd9ede8dd4737
Denis Linnik <denis.linnik@ins.world>
2019-09-26 16:21:53 +0300

    INS-3611: copyright and json decode

--- api/availability.go
+++ api/availability.go
@@ -1,25 +1,24 @@
-/*
- * Copyright 2019 Insolar Technologies GmbH
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+// Copyright 2019 Insolar Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 package api
 
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"sync"
 	"time"
@@ -103,18 +102,12 @@ func (nc *NetworkChecker) updateAvailability(ctx context.Context) {
 		return
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		nc.isAvailable = false
-		logger.Error("[ NetworkChecker ] Can't get keeper status: Can't read body: ", err)
-		return
-	}
-
 	respObj := &keeperResponse{}
-	err = json.Unmarshal(body, respObj)
+	err = json.NewDecoder(resp.Body).Decode(respObj)
+
 	if err != nil {
 		nc.isAvailable = false
-		logger.Error("[ NetworkChecker ] Can't get keeper status: Can't unmarshal body: ", err)
+		logger.Error("[ NetworkChecker ] Can't get keeper status: Can't decode body: ", err)
 		return
 	}
 
