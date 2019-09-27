223751627f3f7d3da7a61beb6c93f0237596e023
Red Cyrax <cyraxred@users.noreply.github.com>
2019-09-27 11:54:31 +0300

    Improved marshalling of unnamed log struct

--- log/logadapter/formatter.go
+++ log/logadapter/formatter.go
@@ -20,7 +20,13 @@ import (
 	"fmt"
 	"github.com/insolar/insolar/insolar"
 	"reflect"
+	"strings"
+	"unsafe"
 )
+
+type LogStringer interface {
+	LogString() string
+}
 
 type FormatFunc func(...interface{}) string
 type FormatfFunc func(string, ...interface{}) string
@@ -49,7 +55,7 @@ func (v MsgFormatConfig) TryLogObject(a ...interface{}) (insolar.LogObjectMarsha
 		default:
 			vt := reflect.ValueOf(v)
 			if vt.Kind() == reflect.Struct && len(vt.Type().Name()) == 0 {
-				return GetInlineLogObjectMarshaller(vt), ""
+				return defaultLogObjectMarshaller{vt}, ""
 			}
 		}
 	}
@@ -57,6 +63,9 @@ func (v MsgFormatConfig) TryLogObject(a ...interface{}) (insolar.LogObjectMarsha
 }
 
 func GetInlineLogObjectMarshaller(v reflect.Value) insolar.LogObjectMarshaller {
+	if v.Kind() != reflect.Struct {
+		panic("illegal value")
+	}
 	return defaultLogObjectMarshaller{v}
 }
 
@@ -64,6 +73,149 @@ type defaultLogObjectMarshaller struct {
 	v reflect.Value
 }
 
-func (v defaultLogObjectMarshaller) MarshalLogObject(insolar.LogObjectWriter) string {
-	return fmt.Sprintf("%+v", v.v)
+func (v defaultLogObjectMarshaller) MarshalLogObject(output insolar.LogObjectWriter) string {
+	return printFields(v.v, output)
+}
+
+func quickTag(prefix string, tag reflect.StructTag) (string, bool) {
+	if len(tag) < len(prefix)+1 {
+		return "", false
+	}
+	if strings.HasPrefix(string(tag), prefix) && strings.HasSuffix(string(tag), `"`) {
+		return string(tag[len(prefix) : len(tag)-1]), true
+	}
+	return "", false
+}
+
+func printFields(sv reflect.Value, output insolar.LogObjectWriter) string {
+	if !sv.CanAddr() {
+		s2 := reflect.New(sv.Type()).Elem()
+		s2.Set(sv)
+		sv = s2
+	}
+
+	hasMsg := false
+	msg := ""
+
+	st := sv.Type()
+	for i := 0; i < sv.NumField(); i++ {
+		fv := sv.Field(i)
+		ft := st.Field(i)
+		if fv.Kind() == reflect.Struct {
+			if ft.Anonymous {
+				s := printFields(fv, output)
+				if !hasMsg {
+					msg = s
+				}
+				continue
+			}
+			//if tag, hasFmt := quickTag(`fmt:"`, ft.Tag); hasFmt {
+			//	zz
+			//}
+			continue
+		}
+		tag, hasFmt := quickTag(`fmt:"`, ft.Tag)
+
+		switch ft.Name {
+		case "", "_":
+			// unreadable field(s)
+		case "msg", "message", "Msg", "Message":
+			hasMsg = true
+			if ok, s, iv := tryReflectStrValue(fv); ok {
+				if !hasFmt {
+					msg = s
+				} else {
+					msg = fmt.Sprintf(tag, s)
+				}
+			} else {
+				if !hasFmt {
+					tag = "%v"
+				}
+				msg = fmt.Sprintf(tag, iv)
+			}
+		default:
+			if ok, iv := tryReflectValue(fv); ok {
+				if hasFmt {
+					output.AddField(ft.Name, fmt.Sprintf(tag, iv))
+					continue
+				}
+
+				if rawTag, ok := quickTag(`raw:"`, ft.Tag); ok {
+					output.AddRawJSON(ft.Name, []byte(fmt.Sprintf(rawTag, iv)))
+				} else {
+					output.AddField(ft.Name, iv)
+				}
+			}
+		}
+	}
+	if hasMsg {
+		return msg
+	}
+	if ok, s := tryMsgValue(toInterface(sv)); ok {
+		return s
+	}
+	return ""
+}
+
+func toInterface(v reflect.Value) interface{} {
+	if !v.CanInterface() {
+		v = reflect.NewAt(v.Type(), unsafe.Pointer(v.UnsafeAddr())).Elem()
+	}
+	return v.Interface()
+}
+
+func tryReflectStrValue(v reflect.Value) (bool, string, interface{}) {
+	if v.Kind() == reflect.String {
+		return true, v.String(), nil
+	}
+	iv := toInterface(v)
+	if ok, s := tryStrValue(iv); ok {
+		return true, s, nil
+	}
+	return false, "", iv
+}
+
+func tryReflectValue(v reflect.Value) (bool, interface{}) {
+	switch k := v.Kind(); k {
+	case reflect.String:
+		return true, v.String()
+	case reflect.Ptr:
+		if v.IsNil() {
+			return true, nil
+		}
+	case reflect.Invalid:
+		return false, nil
+	}
+	iv := toInterface(v)
+	if ok, s := tryStrValue(iv); ok {
+		return true, s
+	}
+	return true, iv
+}
+
+func tryStrValue(v interface{}) (bool, string) {
+	switch vv := v.(type) {
+	case string:
+		return true, vv
+	case *string:
+		if vv == nil {
+			return false, ""
+		}
+		return true, *vv
+	case func() string:
+		return true, vv()
+	default:
+		return tryMsgValue(v)
+	}
+}
+
+func tryMsgValue(v interface{}) (bool, string) {
+	switch vv := v.(type) {
+	case LogStringer:
+		return true, vv.LogString()
+	case fmt.Stringer:
+		return true, vv.String()
+	default:
+		return false, ""
+	}
 }
--- log/logadapter/formatter_test.go
+++ log/logadapter/formatter_test.go
@@ -17,40 +17,124 @@
 package logadapter
 
 import (
+	"fmt"
+	"github.com/insolar/insolar/log"
+	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/insolar/insolar/insolar"
+	"github.com/insolar/insolar/network/consensus/common/args"
 )
 
-//func SpecialSprint(v interface{}) string {
-//	GetDefaultLogMsgFormatter()
-//}
+func formatStrValue(a interface{}) string {
+	v := reflect.ValueOf(a)
+	if ok, s, iv := tryReflectStrValue(v); ok {
+		return s
+	} else {
+		if iv == nil {
+			return fmt.Sprint(nil)
+		}
+		return fmt.Sprintf("%v", iv)
+	}
+}
 
-func TestTryLogObject(t *testing.T) {
-	t.Skipped()
-	//require.NotEqual(t,
-	//	"{test1:text1 test2:123 namedType:{test3:0}}",
-	//	SpecialSprint(struct {
-	//		test1 string
-	//		test2 int
-	//		namedType
-	//	}{"text1", 123, namedType{}}),
-	//	"single anonymous struct must be formatted with fields",
-	//)
-	//
-	//require.Equal(t,
-	//	"{0}",
-	//	SpecialSprint(namedType{}),
-	//	"single named struct must be formatted by default",
-	//)
-	//
-	//require.Equal(t,
-	//	"{text1} {text2}",
-	//	SpecialSprint(struct {
-	//		test1 string
-	//	}{"text1"},
-	//		struct {
-	//			test1 string
-	//		}{"text2"},
-	//	),
-	//	"multiple anonymous struct(s) must be formatted by default",
-	//)
+func formatValue(a interface{}) interface{} {
+	v := reflect.ValueOf(a)
+	if ok, iv := tryReflectValue(v); ok {
+		return iv
+	} else {
+		return "?"
+	}
+}
+
+func TestTryFormatStrValue(t *testing.T) {
+	s := "test"
+	require.Equal(t, "test", formatStrValue(&s))
+	require.Equal(t, "<nil>", formatStrValue((*string)(nil)))
+	require.Equal(t, "test", formatStrValue("test"))
+	require.Equal(t, "test", formatStrValue(args.LazyFmt("%s", "test")))
+	require.Equal(t, "test", formatStrValue(func() string { return "test" }))
+	require.Equal(t, "123", formatStrValue(123))
+	require.Equal(t, "<nil>", formatStrValue(nil))
+}
+
+func TestTryFormatValue(t *testing.T) {
+	s := "test"
+	require.Equal(t, "test", formatValue(&s))
+	require.Equal(t, "<nil>", formatValue((*string)(nil)))
+	require.Equal(t, "test", formatValue("test"))
+	require.Equal(t, "test", formatValue(args.LazyFmt("%s", "test")))
+	require.Equal(t, "test", formatValue(func() string { return "test" }))
+	require.Equal(t, 123, formatValue(123))
+	require.Equal(t, nil, formatValue(nil))
+}
+
+func TestPrintFields(t *testing.T) {
+	s := "test2"
+	require.Equal(t,
+		"f0:  99:string,f1:999:int,f2:test_raw,f3:test2:string,f4:nil,f5:stringer_test:string,f6:func_result:string,msg:message title",
+		printFieldsOut(struct {
+			msg string
+			f0  int `fmt:"%4d"`
+			f1  int
+			f2  string `raw:"%s"`
+			f3  *string
+			f4  *string
+			f5  interface{}
+			f6  func() string
+		}{
+			"message title",
+			99, 999, "test_raw", &s, nil,
+			args.LazyFmt("stringer_test"),
+			func() string { return "func_result" },
+		}))
+
+	log.Debug(struct {
+		msg string
+		f0  int `fmt:"%4d"`
+		f1  int
+		f2  string `raw:"%s"`
+		f3  *string
+		f4  *string
+		f5  interface{}
+		f6  func() string
+	}{
+		"message title",
+		99, 999, "test_raw", &s, nil,
+		args.LazyFmt("stringer_test"),
+		func() string { return "func_result" },
+	})
+}
+
+func printFieldsOut(v interface{}) string {
+	o := output{}
+	msg := printFields(reflect.ValueOf(v), &o)
+	o.buf.WriteString("msg:")
+	o.buf.WriteString(msg)
+	return o.buf.String()
+}
+
+var _ insolar.LogObjectWriter = &output{}
+
+type output struct {
+	buf strings.Builder
+}
+
+func (p *output) AddField(k string, v interface{}) {
+	if v == nil {
+		p.buf.WriteString(fmt.Sprintf("%s:nil,", k))
+	} else {
+		p.buf.WriteString(fmt.Sprintf("%s:%v:%s,", k, v, reflect.TypeOf(v).Name()))
+	}
+}
+
+func (output) AddFieldMap(m map[string]interface{}) {
+	panic("implement me")
+}
+
+func (p *output) AddRawJSON(k string, b []byte) {
+	p.buf.WriteString(fmt.Sprintf("%s:%s,", k, b))
 }
