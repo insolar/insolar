a25a8362cf7665f82d8276d35b3e0ff354feefbb
Sergey Morozov <sergey.morozov@corp.mail.ru>
2019-09-29 19:37:41 +0300

    INS-3515: some logs trimming

--- ledger/light/handle/init.go
+++ ledger/light/handle/init.go
@@ -54,9 +54,9 @@ func (s *Init) Present(ctx context.Context, f flow.Flow) error {
 	err := s.handle(ctx, f)
 	if err != nil {
 		if err == flow.ErrCancelled {
-			logger.Info(errors.Wrap(err, "handling error"))
+			logger.Info("Handling flow cancelled")
 		} else {
-			logger.Error(errors.Wrap(err, "handling error"))
+			logger.Error("Handling error: %s", err.Error())
 		}
 	}
 	return err
