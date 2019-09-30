0eb92b20d6da46a0a389a6df85489096d345ba6b
Alexander Orlovsky <nordicdyno@gmail.com>
2019-09-30 14:19:16 +0300

    NOISSUE: fix build issue with missed coreutils on MacOS X

---
+++ scripts/build/realpath.go
@@ -0,0 +1,20 @@
+//usr/bin/env go run "$0" "$@"; exit "$?"
+// realpath.go - because we don't wand depend on coreutils on MacOS X for building binaries
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+func main() {
+	gotPath := os.Args[1]
+	absPath, err := filepath.Abs(gotPath)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed to get absolute path for '%v': %v\n", gotPath, err)
+		os.Exit(1)
+	}
+	fmt.Println(path.Clean(absPath))
+}
