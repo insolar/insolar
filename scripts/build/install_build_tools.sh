5ea2b882d9775d2d5962e47d7e5efc382e0ed18a
Vany Serezhkin <ivan@serezhkin.com>
2019-09-30 14:43:37 +0300

    INS-3647: WIP1

--- scripts/build/install_build_tools.sh
+++ scripts/build/install_build_tools.sh
@@ -6,6 +6,6 @@
 go clean -modcache
 
 ./scripts/build/fetchdeps github.com/golang/dep/cmd/dep v0.5.3
-./scripts/build/fetchdeps golang.org/x/tools/cmd/stringer 63e6ed9258fa6cbc90aab9b1eef3e0866e89b874
+./scripts/build/fetchdeps golang.org/x/tools/cmd/stringer gopls/v0.1.7
 ./scripts/build/fetchdeps github.com/gojuno/minimock/cmd/minimock v2.1.8
 ./scripts/build/fetchdeps github.com/gogo/protobuf/protoc-gen-gogoslick v1.2.1
