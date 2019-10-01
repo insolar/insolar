70a22bab9dd2a70f97e2ef0aed1abe31958bcffb
Maria Zubareva <maria.zubareva@insolar.io>
2019-10-01 15:16:02 +0300

    NOISSUE: let not self scope ref run

--- logicrunner/handle_calls.go
+++ logicrunner/handle_calls.go
@@ -147,7 +147,7 @@ func (h *HandleCall) handleActual(
 
 	objectRef := request.Object
 
-	if objectRef == nil || !objectRef.IsSelfScope() {
+	if objectRef == nil {
 		return nil, errors.New("can't get object reference")
 	}
 
