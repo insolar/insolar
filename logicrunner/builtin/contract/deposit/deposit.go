2c3b95dfbd04793347ec3460e1d5c806db685660
Andrey Pavlushev <andrey.pavlushev@insolar.io>
2019-10-09 11:12:25 +0300

    fix time UnixNano -> Unix

--- logicrunner/builtin/contract/deposit/deposit.go
+++ logicrunner/builtin/contract/deposit/deposit.go
@@ -54,7 +54,7 @@ type DepositOut struct {
 	Amount                  string                 `json:"amount"`
 	TxHash                  string                 `json:"ethTxHash"`
 	VestingType             foundation.VestingType `json:"vestingType"`
-	MaturePulse             int64                  `json:"matureDate"`
+	MatureDate              int64                  `json:"matureDate"`
 	Lockup                  int64                  `json:"lockup"`
 	Vesting                 int64                  `json:"vesting"`
 	VestingStep             int64                  `json:"vestingStep"`
@@ -67,17 +67,17 @@ type DaemonConfirm struct {
 
 func (d Deposit) toOut() DepositOut {
 	var daemonConfirms = make([]DaemonConfirm, 0, len(d.MigrationDaemonConfirms))
-	var pulseDepositUnHold, matureTime int64
+	var pulseDepositUnHold, matureDate int64
 	for k, v := range d.MigrationDaemonConfirms {
 		daemonConfirms = append(daemonConfirms, DaemonConfirm{Reference: k, Amount: v})
 	}
 	t, err := d.PulseDepositUnHold.AsApproximateTime()
 	if err == nil {
-		pulseDepositUnHold = t.UnixNano()
+		pulseDepositUnHold = t.Unix()
 	}
 	mature, err := d.MaturePulse.AsApproximateTime()
 	if err == nil {
-		matureTime = mature.UnixNano()
+		matureDate = mature.Unix()
 	}
 	return DepositOut{
 		Balance:                 d.Balance,
@@ -87,7 +87,7 @@ func (d Deposit) toOut() DepositOut {
 		Amount:                  d.Amount,
 		TxHash:                  d.TxHash,
 		VestingType:             d.VestingType,
-		MaturePulse:             matureTime,
+		MatureDate:              matureDate,
 		Lockup:                  d.Lockup,
 		Vesting:                 d.Vesting,
 		VestingStep:             d.VestingStep,
