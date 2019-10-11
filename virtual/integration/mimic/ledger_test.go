7a14eea117db4bad36e3c09281858c0ac8e42bd2
Eugene Blikh <bigbes@gmail.com>
2019-10-11 13:53:29 +0300

    INS-3703: add genesis mimic integration
    Basically mimic is just an empty ledger-like structure. It needs to be
    populated. One way to support is to support basic genesis/genesis.go (really
    - bootstrap)

--- virtual/integration/mimic/ledger_test.go
+++ virtual/integration/mimic/ledger_test.go
@@ -1,10 +1,19 @@
 package mimic
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 	"testing"
 
 	"github.com/gojuno/minimock"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/insolar/insolar/insolar"
 	"github.com/insolar/insolar/insolar/pulse"
@@ -15,6 +24,65 @@ import (
 	"github.com/insolar/insolar/platformpolicy"
 )
 
+const (
+	LaunchnetRelativePath = "scripts/insolard/launchnet.sh"
+	GenesisRelativePath   = "launchnet/configs/heavy_genesis.json"
+)
+
+func GenerateBootstrap(t testing.TB, skipBuild bool) (func(), string, error) {
+	artifactsDir, err := ioutil.TempDir("", "mimic")
+	if err != nil {
+		return nil, "", errors.Wrap(err, "failed to create temporary directory")
+	}
+
+	cleanupFunc := func() {
+		err := os.RemoveAll(artifactsDir)
+		if err != nil {
+			t.Logf("[ Error ] Failed to cleanup temporary dir %s: %s", artifactsDir, err.Error())
+		}
+	}
+
+	cmd := exec.Command(LaunchnetRelativePath, "-b")
+	cmd.Dir = insolar.RootModuleDir()
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "INSOLAR_ARTIFACTS_DIR="+artifactsDir)
+	if skipBuild {
+		cmd.Env = append(cmd.Env, "SKIP_BUILD=1")
+	}
+
+	if output, err := cmd.CombinedOutput(); err != nil {
+		cleanupFunc()
+
+		t.Logf("[ Error ] Failed to execute bootstrap: %s", err.Error())
+		t.Logf("[ Error ] Output of bootstrap is:")
+
+		outputString := string(bytes.TrimSpace(output))
+		for _, line := range strings.Split(outputString, "\n") {
+			t.Logf("[ Error ] > %s", line)
+		}
+
+		return nil, "", errors.Wrapf(err, "Failed to execute bootstrap: %s", err.Error())
+	}
+
+	return cleanupFunc, artifactsDir, nil
+}
+
+func ReadGenesisContractsConfig(dirPath string) (*insolar.GenesisContractsConfig, error) {
+	genesisConfigPath := path.Join(dirPath, GenesisRelativePath)
+
+	fh, err := os.Open(genesisConfigPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open genesis config for reading")
+	}
+
+	rv := insolar.GenesisHeavyConfig{}
+	if err := json.NewDecoder(fh).Decode(&rv); err != nil {
+		return nil, errors.Wrap(err, "failed to decode genesis config")
+	}
+
+	return &rv.ContractsConfig, nil
+}
+
 func TestMimicLedger_Genesis(t *testing.T) {
 	ctx := inslogger.TestContext(t)
 	mc := minimock.NewController(t)
@@ -22,13 +90,22 @@ func TestMimicLedger_Genesis(t *testing.T) {
 	pcs := platformpolicy.NewPlatformCryptographyScheme()
 	pulseStorage := pulse.NewStorageMem()
 	dmm := drop.NewModifierMock(mc).SetMock.Return(nil)
-	imm := object.NewIndexModifierMock(mc).SetIndexMock.Return(nil)
+	imm := object.NewIndexModifierMock(mc).
+		SetIndexMock.Return(nil).
+		UpdateLastKnownPulseMock.Return(nil)
 	rmm := object.NewRecordModifierMock(mc).SetMock.Return(nil)
 
 	mimicLedgerInstance := NewMimicLedger(pcs, pulseStorage)
 	mimicStorage := mimicLedgerInstance.(*mimicLedger).storage
 
 	mimicClient := NewClient(mimicStorage)
+
+	cleanup, bootstrapDir, err := GenerateBootstrap(t, true)
+	require.NoError(t, err)
+	defer cleanup()
+
+	genesisContractsConfig, err := ReadGenesisContractsConfig(bootstrapDir)
+	require.NoError(t, err)
 
 	genesisObject := genesis.Genesis{
 		ArtifactManager: mimicClient,
@@ -40,13 +117,10 @@ func TestMimicLedger_Genesis(t *testing.T) {
 			RecordModifier: rmm,
 			IndexModifier:  imm,
 		},
-		DiscoveryNodes: []insolar.DiscoveryNodeRegister{},
-		ContractsConfig: insolar.GenesisContractsConfig{
-			PKShardCount: 10,
-			MAShardCount: 10,
-		},
+		DiscoveryNodes:  []insolar.DiscoveryNodeRegister{},
+		ContractsConfig: *genesisContractsConfig,
 	}
 
-	err := genesisObject.Start(ctx)
+	err = genesisObject.Start(ctx)
 	assert.NoError(t, err)
 }
