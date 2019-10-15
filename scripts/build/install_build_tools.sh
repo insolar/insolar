01134fc5791184e39d415c02549992732f53a3a2
Andrey Bronin <andrey.bronin@insolar.io>
2019-10-15 13:42:21 +0300

    try to fix ci build

--- scripts/build/install_build_tools.sh
+++ scripts/build/install_build_tools.sh
@@ -3,10 +3,9 @@
 # install tools for codegen
 #
 
+export GO111MODULE=on
 go clean -modcache
 
-#./scripts/build/fetchdeps github.com/golang/dep/cmd/dep v0.5.3
 ./scripts/build/fetchdeps golang.org/x/tools/cmd/stringer 63e6ed9258fa6cbc90aab9b1eef3e0866e89b874
 ./scripts/build/fetchdeps github.com/gojuno/minimock/cmd/minimock v3.0.5
-#./scripts/build/fetchdeps github.com/gojuno/minimock/cmd/minimock v2.1.8 # FUCK WHAT???
 ./scripts/build/fetchdeps github.com/gogo/protobuf/protoc-gen-gogoslick v1.2.1
