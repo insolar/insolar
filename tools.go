c241878db1d0c95331daa4432b5b662b1f903531
Andrey Bronin <andrey.bronin@insolar.io>
2019-10-31 16:29:08 +0300

    tools protoc-gen-go path fix

--- tools.go
+++ tools.go
@@ -20,6 +20,6 @@ package tools
 
 import _ "golang.org/x/tools/cmd/stringer"
 import _ "github.com/gogo/protobuf/protoc-gen-gogoslick"
-import _ "github.com/golang/protobuf/tree/master/protoc-gen-go"
+import _ "github.com/golang/protobuf/protoc-gen-go"
 import _ "github.com/dgraph-io/badger/badger"
 import _ "github.com/gojuno/minimock/v3/cmd/minimock"
