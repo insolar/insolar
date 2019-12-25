1ca408557b9ee0d3fc6766c81e2c516f260e5773
Sergey Morozov <sergey.morozov@insolar.io>
2019-12-25 13:01:30 +0300

    INS-4035: add test to generate vesting table

--- application/builtin/contract/deposit/deposit_vesting_test.go
+++ application/builtin/contract/deposit/deposit_vesting_test.go
@@ -17,8 +17,10 @@
 package deposit
 
 import (
+	"fmt"
 	"math"
 	"math/big"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -55,4 +57,45 @@ func TestVestingCoeffs(t *testing.T) {
 		require.True(t, ok)
 		assert.Truef(t, expectedCoeff.Cmp(coeffInt) == 0, "step: %d, expected: %s actual: %s", i, expectedCoeff.String(), coeffInt.String())
 	}
+}
+
+func TestVestingCoeffs_Table(t *testing.T) {
+	denom, ok := new(big.Int).SetString(VestingCoeffDenominator, 10)
+	require.True(t, ok)
+	amount, ok := new(big.Int).SetString("5000000000000000000", 10)
+	require.True(t, ok)
+	xnsDenom, ok := new(big.Int).SetString("10000000000", 10)
+	require.True(t, ok)
+
+	var b strings.Builder
+	fmt.Fprintf(&b, "%4s %25s %25s %25s | %25s %25s %25s\n", "Step", "Coefficient", "Released", "Daily release", "Fraction", "Released XNS", "Daily XNS")
+
+	lastReleased := big.NewInt(0)
+	for i, c := range VestingCoeffs {
+		coeff, ok := new(big.Int).SetString(c, 10)
+		require.True(t, ok)
+		frac := new(big.Float).Quo(
+			new(big.Float).SetInt(coeff),
+			new(big.Float).SetInt(denom),
+		)
+		released := new(big.Int).Quo(
+			new(big.Int).Mul(coeff, amount),
+			denom,
+		)
+		daily := new(big.Int).Sub(released, lastReleased)
+		lastReleased = released
+
+		releasedXNS := new(big.Float).Quo(
+			new(big.Float).SetInt(released),
+			new(big.Float).SetInt(xnsDenom),
+		)
+		dailyXNS := new(big.Float).Quo(
+			new(big.Float).SetInt(daily),
+			new(big.Float).SetInt(xnsDenom),
+		)
+
+		fmt.Fprintf(&b, "%4d %25d %25d %25d | %25.20f %25.10f %25.10f\n", i, coeff, released, daily, frac, releasedXNS, dailyXNS)
+	}
+
+	// fmt.Println(b.String())
 }
