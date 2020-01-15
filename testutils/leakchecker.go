b511de647edfa2122ef2f27eb62654f303f23790
Ilya Ozherelyev <ilya.ozherelyev@insolar.io>
2020-01-15 19:29:43 +0300

    NOISSUE: iterate unit tests collects profile

--- testutils/leakchecker.go
+++ testutils/leakchecker.go
@@ -22,7 +22,10 @@ import (
 
 func LeakTester(t goleak.TestingT) {
 	goleak.VerifyNone(t,
+		// Iterate Unit Tests uses memprofile
+		goleak.IgnoreTopFunction("runtime/pprof.readProfile"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
+		// sometimes stack has full import path
 		goleak.IgnoreTopFunction("github.com/insolar/insolar/vendor/go.opencensus.io/stats/view.(*worker).start"),
 		goleak.IgnoreTopFunction("github.com/insolar/insolar/log/critlog.(*internalBackpressureBuffer).worker"))
 }
