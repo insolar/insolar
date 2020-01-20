81d93de2d59b8963a2b04b3b0af69e919e93e228
Alexander Alexeev <alexander.alexeev@insolar.io>
2020-01-20 16:45:33 +0300

    Work in progress...

--- tests/common/common.go
+++ tests/common/common.go
@@ -1,3 +1,21 @@
+//
+// Copyright 2020 Insolar Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// +build slowtest
+
 package common
 
 import (
@@ -8,8 +26,6 @@ import (
 	"github.com/jackc/pgx/v4"
 	"github.com/ory/dockertest/v3"
 )
-
-// +build slowtest
 
 func StartPostgreSQL() (pgURL string, cleaner func()) {
 	pool, err := dockertest.NewPool("")
