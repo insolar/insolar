91097c0c2e64348fc0e91a3d9d18d1c3bb30a691
Alexander Alexeev <alexander.alexeev@insolar.io>
2020-01-22 15:46:20 +0300

    fix

---
+++ migration/0002_records.sql
@@ -0,0 +1,14 @@
+create type local_ref as (pulse_and_scope bigint, hash bytea);
+
+create table records (
+    id local_ref primary key,
+    position bigint not null,
+    object_id local_ref not null,
+    jet_id local_ref not null,
+    signature bytea not null,
+    polymorph int not null,
+    virtual bytea not null -- serialized to protobuf
+);h
+
+---- create above / drop below ----
+DROP TABLE records;
