045d8c6d6dea63e12e1145d8815c3b0e24367db5
Alexander Alexeev <alexander.alexeev@insolar.io>
2020-01-24 17:39:41 +0300

    fix

--- insolar/jet/dbstore.go
+++ insolar/jet/dbstore.go
@@ -20,10 +20,25 @@ import (
 	"context"
 	"sync"
 
+	"github.com/insolar/insolar/instrumentation/inslogger"
+	"github.com/jackc/pgx/v4"
+
 	"github.com/insolar/insolar/insolar"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
 )
+
+var readTxOptions = pgx.TxOptions{
+	IsoLevel:       pgx.Serializable,
+	AccessMode:     pgx.ReadOnly,
+	DeferrableMode: pgx.NotDeferrable,
+}
+
+var writeTxOptions = pgx.TxOptions{
+	IsoLevel:       pgx.Serializable,
+	AccessMode:     pgx.ReadWrite,
+	DeferrableMode: pgx.NotDeferrable,
+}
 
 type DBStore struct {
 	sync.RWMutex
@@ -97,55 +112,108 @@ func (s *DBStore) Clone(ctx context.Context, from, to insolar.PulseNumber, keepA
 
 // TruncateHead remove all records after lastPulse
 func (s *DBStore) TruncateHead(ctx context.Context, from insolar.PulseNumber) error {
-	panic("AALEKSEEV implement")
-	//s.Lock()
-	//defer s.Unlock()
-	//
-	//it := s.db.NewIterator(pulseKey(from), false)
-	//defer it.Close()
-	//
-	//var hasKeys bool
-	//for it.Next() {
-	//	hasKeys = true
-	//	key := newPulseKey(it.Key())
-	//	err := s.db.Delete(&key)
-	//	if err != nil {
-	//		return errors.Wrapf(err, "can't delete key: %+v", key)
-	//	}
-	//
-	//	inslogger.FromContext(ctx).Debugf("Erased key with pulse number: %s", insolar.PulseNumber(key))
-	//}
-	//
-	//if !hasKeys {
-	//	inslogger.FromContext(ctx).Debugf("No records. Nothing done. Pulse number: %s", from.String())
-	//}
-	//
-	//return nil
+	conn, err := s.pool.Acquire(ctx)
+	if err != nil {
+		return errors.Wrap(err, "Unable to acquire a database connection")
+	}
+	defer conn.Release()
+
+	log := inslogger.FromContext(ctx)
+	for { // retry loop
+		tx, err := conn.BeginTx(ctx, writeTxOptions)
+		if err != nil {
+			return errors.Wrap(err, "Unable to start a write transaction")
+		}
+
+		_, err = tx.Exec(ctx, "DELETE FROM jet_trees WHERE pulse_number > $1", from)
+		if err != nil {
+			_ = tx.Rollback(ctx)
+			return errors.Wrap(err, "Unable to DELETE FROM jet_trees")
+		}
+
+		err = tx.Commit(ctx)
+		if err == nil { // success
+			break
+		}
+
+		log.Infof("TruncateHead - commit failed: %v - retrying transaction", err)
+	}
+
+	return nil
 }
 
 func (s *DBStore) get(pn insolar.PulseNumber) *Tree {
-	panic("AALEKSEEV implement me")
-	//serializedTree, err := s.db.Get(pulseKey(pn))
-	//if err != nil {
-	//	return NewTree(pn == insolar.GenesisPulse.PulseNumber)
-	//}
-	//
-	//recovered := &Tree{}
-	//err = recovered.Unmarshal(serializedTree) // AALEKSEEV TODO just load a blog by given pulse number + truncate head
-	//if err != nil {
-	//	return nil
-	//}
-	//return recovered
+	ctx := context.Background()
+	ErrResult := NewTree(pn == insolar.GenesisPulse.PulseNumber)
+	conn, err := s.pool.Acquire(ctx)
+	if err != nil {
+		return ErrResult
+	}
+	defer conn.Release()
+
+	tx, err := conn.BeginTx(ctx, readTxOptions)
+	if err != nil {
+		return ErrResult
+	}
+
+	row := tx.QueryRow(ctx, "SELECT jet_tree FROM jet_trees WHERE pulse_number = $1", pn)
+	var serializedTree []byte
+	err = row.Scan(&serializedTree)
+
+	if err != nil {
+		_ = tx.Rollback(ctx)
+		return ErrResult
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return ErrResult
+	}
+
+	recovered := &Tree{}
+	err = recovered.Unmarshal(serializedTree)
+	if err != nil {
+		return nil
+	}
+	return recovered
 }
 
 func (s *DBStore) set(pn insolar.PulseNumber, jt *Tree) error {
-	panic("AALEKSEEV implement me")
-	//key := pulseKey(pn)
-	//
-	//serialized, err := jt.Marshal() // AALEKSEEV TODO just save a blob by given pulse number + truncate head
-	//if err != nil {
-	//	return errors.Wrap(err, "failed to serialize jet.Tree")
-	//}
-	//
-	//return s.db.Set(key, serialized)
+	ctx := context.Background()
+	serialized, err := jt.Marshal()
+	if err != nil {
+		return errors.Wrap(err, "failed to serialize jet.Tree")
+	}
+
+	conn, err := s.pool.Acquire(ctx)
+	if err != nil {
+		return errors.Wrap(err, "Unable to acquire a database connection")
+	}
+	defer conn.Release()
+
+	log := inslogger.FromContext(ctx)
+	for { // retry loop
+		tx, err := conn.BeginTx(ctx, writeTxOptions)
+		if err != nil {
+			return errors.Wrap(err, "Unable to start a write transaction")
+		}
+
+		_, err = tx.Exec(ctx, `
+			INSERT INTO jet_trees(pulse_number, jet_tree)
+			VALUES ($1, $2)
+		`, pn, serialized)
+		if err != nil {
+			_ = tx.Rollback(ctx)
+			return errors.Wrap(err, "Unable to INSERT jet_tree")
+		}
+
+		err = tx.Commit(ctx)
+		if err == nil { // success
+			break
+		}
+
+		log.Infof("DBStore.set - commit failed: %v - retrying transaction", err)
+	}
+
+	return nil
 }
