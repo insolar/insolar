9d415014eac09e3dbf04e4525c95f98b012761c8
Alexander Alexeev <alexander.alexeev@insolar.io>
2020-01-27 18:04:57 +0300

    fix

--- insolar/jet/dbstore.go
+++ insolar/jet/dbstore.go
@@ -205,15 +205,9 @@ func (s *DBStore) set(pn insolar.PulseNumber, jt *Tree) error {
 			return errors.Wrap(err, "Unable to start a write transaction")
 		}
 
-		// TODO we use UPSERT here to keep the old behaviour of DBStore
-		// This is not necessary the right behaviour - if someone tries to rewrite
-		// an existing jet tree this might indicate an error. Consider returning
-		// an error here in the future.
 		_, err = tx.Exec(ctx, `
 			INSERT INTO jet_trees(pulse_number, jet_tree)
 			VALUES ($1, $2)
-			ON CONFLICT (pulse_number) DO
-			UPDATE SET jet_tree = EXCLUDED.jet_tree
 		`, pn, serialized)
 		if err != nil {
 			_ = tx.Rollback(ctx)
