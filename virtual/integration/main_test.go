0589db257e532efc923d2ce1abd85a6c8a51bcc0
Maria Zubareva <maria.zubareva@insolar.io>
2020-02-07 16:20:30 +0300

    PENV-34: update tests

--- virtual/integration/main_test.go
+++ virtual/integration/main_test.go
@@ -217,10 +217,11 @@ func NewServer(
 		ClientBus = bus.NewBus(cfg.Bus, IncomingPubSub, Pulses, c, CryptoScheme)
 	}
 
-	logicRunner, err := logicrunner.NewLogicRunner(&cfg.LogicRunner, IncomingPubSub, ClientBus, codeRegistry,
-		codeRefRegistry,
-		codeDescriptors,
-		prototypeDescriptors,
+	logicRunner, err := logicrunner.NewLogicRunner(&cfg.LogicRunner, IncomingPubSub, ClientBus,
+		nil,
+		nil,
+		nil,
+		nil,
 	)
 	checkError(ctx, err, "failed to start LogicRunner")
 
