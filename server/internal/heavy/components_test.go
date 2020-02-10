b67953372b0ddb6122ff40266f04fc84191acf13
Maria Zubareva <maria.zubareva@insolar.io>
2020-02-10 18:33:07 +0300

    PENV-35: update test

--- server/internal/heavy/components_test.go
+++ server/internal/heavy/components_test.go
@@ -49,6 +49,6 @@ func TestComponents(t *testing.T) {
 	cfg.Ledger.Storage.DataDirectory = tmpdir
 	cfg.Exporter.Addr = ":0"
 
-	_, err = newComponents(ctx, cfg, genesis.GenesisHeavyConfig{Skip: true})
+	_, err = newComponents(ctx, cfg, genesis.GenesisHeavyConfig{Skip: true}, nil, "")
 	require.NoError(t, err)
 }
