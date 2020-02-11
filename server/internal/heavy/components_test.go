649fee1687ac5331e68ab63949d9370443d9fd3f
Alexander Alexeev <alexander.alexeev@insolar.io>
2020-02-11 17:18:48 +0300

    Fix tests

--- server/internal/heavy/components_test.go
+++ server/internal/heavy/components_test.go
@@ -49,6 +49,6 @@ func TestComponents(t *testing.T) {
 	cfg.Ledger.Storage.DataDirectory = tmpdir
 	cfg.Exporter.Addr = ":0"
 
-	_, err = newComponents(ctx, cfg, application.GenesisHeavyConfig{Skip: true})
+	_, err = newComponents(ctx, cfg, application.GenesisHeavyConfig{Skip: true}, false)
 	require.NoError(t, err)
 }
