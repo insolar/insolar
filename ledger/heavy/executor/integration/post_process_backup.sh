c964501362321efde92b1680b85c77b8cd293f2b
egorikas <egorgrishechko@gmail.com>
2020-02-12 18:37:39 +0300

    INS-4153 revert post_process_backup.sh

---
+++ ledger/heavy/executor/integration/post_process_backup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+badgerDir=$1
+recoverDB=$2
+
+set -o history
+set -o histexpand
+
+echo "BADGER DB: $badgerDir"
+echo "RECOVER DB: $recoverDB"
+
+# Makes a complete copy of a Badger database directory.
+# Repeat rsync if the MANIFEST and SSTables are updated.
+rsync -avz --delete $badgerDir/ $recoverDB/
+while !! | grep -q "(MANIFEST\|\.sst)$"; do :; done
+
+touch $INSOLAR_CURRENT_BACKUP_DIR/BACKUPED
