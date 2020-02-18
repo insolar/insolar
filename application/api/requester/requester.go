4ae9f67778c126339cfb1637ca93bfb7669082ee
Tsovak <tsovak36@gmail.com>
2020-02-18 13:27:49 +0300

    PENV-56: add x-crypto alias

--- application/api/requester/requester.go
+++ application/api/requester/requester.go
@@ -1,23 +1,13 @@
-// Copyright 2020 Insolar Network Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//  Copyright 2020 Insolar Network Ltd.
+//  All rights reserved.
+//  This material is licensed under the Insolar License version 1.0,
+//  available at https://github.com/insolar/insolar/blob/master/network/LICENSE.md.
 
 package requester
 
 import (
 	"bytes"
 	"context"
-	"crypto"
 	"encoding/asn1"
 	"encoding/base64"
 	"encoding/json"
@@ -31,6 +21,7 @@ import (
 	"strconv"
 	"time"
 
+	crypto "github.com/insolar/x-crypto"
 	"github.com/insolar/x-crypto/ecdsa"
 	"github.com/insolar/x-crypto/rand"
 	"github.com/insolar/x-crypto/sha256"
