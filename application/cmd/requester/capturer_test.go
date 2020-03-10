9a75718394afae987cfee2502232bffcab4d068a
Tsovak <tsovak36@gmail.com>
2020-03-10 17:50:34 +0300

    PENV-105: fixed data race in the tests

--- application/cmd/requester/capturer_test.go
+++ application/cmd/requester/capturer_test.go
@@ -6,7 +6,8 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"io"
 	"os"
 	"time"
 )
@@ -18,27 +19,32 @@ type Capturer struct {
 }
 
 // CaptureStdout captures stdout.
-func CaptureStdout(f func()) (string, error) {
+func CaptureStdout(f func(), d time.Duration) (string, error) {
 	capturer := &Capturer{captureStdout: true}
-	return capturer.capture(f)
+	return capturer.capture(f, d)
 }
 
 // CaptureStderr captures stderr.
-func CaptureStderr(f func()) (string, error) {
+func CaptureStderr(f func(), d time.Duration) (string, error) {
 	capturer := &Capturer{captureStderr: true}
-	return capturer.capture(f)
+	return capturer.capture(f, d)
 }
 
 // CaptureOutput captures stdout and stderr.
-func CaptureOutput(f func()) (string, error) {
+func CaptureOutput(f func(), d time.Duration) (string, error) {
 	capturer := &Capturer{captureStdout: true, captureStderr: true}
-	return capturer.capture(f)
+	return capturer.capture(f, d)
 }
 
-func (capturer *Capturer) capture(fn func()) (string, error) {
+func (capturer *Capturer) capture(fn func(), duration time.Duration) (string, error) {
 	r, w, err := os.Pipe()
 	if err != nil {
-		panic(err.Error())
+		return "", err
+	}
+
+	err = r.SetReadDeadline(time.Now().Add(duration))
+	if err != nil {
+		return "", err
 	}
 
 	if capturer.captureStdout {
@@ -59,20 +65,28 @@ func (capturer *Capturer) capture(fn func()) (string, error) {
 
 	defer w.Close()
 
-	var buf bytes.Buffer
-	var retErr error
-	go func() {
-		defer r.Close()
-		_, e := buf.ReadFrom(r)
-		if e != nil {
-			retErr = err
-			return
-		}
-	}()
-
 	fn()
 
-	// here we need sleep because not of all pipes closed
-	time.Sleep(time.Millisecond * 10)
-	return buf.String(), retErr
+	anon := func() interface{} {
+		defer r.Close()
+		reader := bufio.NewReader(r)
+		line, _, e := reader.ReadLine()
+		if e == io.EOF {
+			return result{string(line), nil}
+		} else if e != nil {
+			return result{"nil", e}
+		}
+		return result{string(line), e}
+	}
+
+	res, err := waitForFunction(anon, duration*2)
+	if err != nil {
+		return "nil", err
+	}
+	return res.(result).str, err
+}
+
+type result struct {
+	str string
+	err error
 }
