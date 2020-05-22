22957d47e531750fe0317808c3887952dca49bc3
Sergey Kudasov <sergey.kudasov@insolar.io>
2020-05-22 16:32:58 +0300

    PENV-251: rename file method to move in docker containers with volumes

--- instrumentation/introspector/_swagger-helper/main.go
+++ instrumentation/introspector/_swagger-helper/main.go
@@ -60,13 +60,41 @@ func main() {
 	}
 	sw.writeString(")\n")
 
-	err = os.Rename(tmpF.Name(), outFileName)
+	err = MoveFile(tmpF.Name(), outFileName)
 	if err != nil {
 		log.Fatalf("failed move file from %v to %v: %s", tmpF.Name(), outFileName, err)
 	}
 
 	cwd, _ := os.Getwd()
 	_, _ = fmt.Fprintf(os.Stderr, "Generated file: %v (%v)", outFileName, cwd)
+}
+
+/*
+   GoLang: os.Rename() give error "invalid cross-device link" for Docker container with Volumes.
+   MoveFile(source, destination) will work moving file between folders
+*/
+func MoveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("Couldn't open dest file: %s", err)
+	}
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+	// The copy was successful, so now delete the original file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed removing original file: %s", err)
+	}
+	return nil
 }
 
 type strictWriter struct {
